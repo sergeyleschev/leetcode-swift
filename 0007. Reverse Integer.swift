f624e89dd4b8a24f351e14fbbb4c4a6bc9328849
Sergey Leschev <sergey.leschev@gmail.com>
2021-05-16 14:16:04 +0300

    7. Reverse Integer

--- 0007. Reverse Integer.swift
+++ 0007. Reverse Integer.swift
@@ -1,7 +1,7 @@
 class Solution {
 
     // 7. Reverse Integer
-    // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+    // Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
     // Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
     // Example 1:
@@ -40,9 +40,7 @@ class Solution {
         }
         res = isNegtive ? -res : res
         
-        if res < -Int(max) || res > Int(max)  - 1 {
-            res = 0
-        }
+        if res < -Int(max) || res > Int(max)  - 1 { res = 0 }
 
         return res
     }
