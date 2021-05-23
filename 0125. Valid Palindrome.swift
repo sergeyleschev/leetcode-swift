6c5fe1c24295f3ec24b5347790041fa84b048274
Sergey Leschev <sergey.leschev@gmail.com>
2021-05-23 10:51:29 +0300

    125. Valid Palindrome

---
+++ 0125. Valid Palindrome.swift
@@ -0,0 +1,48 @@
+class Solution {
+
+    // 125. Valid Palindrome
+    // Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+    // Determines if a string is a palindrome.
+    // - Parameter s: The string.
+    // - Returns: True if the string is a palindrome, otherwise returns false.
+
+    // Example 1:
+    // Input: s = "A man, a plan, a canal: Panama"
+    // Output: true
+    // Explanation: "amanaplanacanalpanama" is a palindrome.
+    
+    // Example 2:
+    // Input: s = "race a car"
+    // Output: false
+    // Explanation: "raceacar" is not a palindrome.
+
+    // Constraints:
+    // 1 <= s.length <= 2 * 105
+    // s consists only of printable ASCII characters.
+
+    // - Complexity:
+    //   - time: O(n), where n is the length of the *s*.
+    //   - space: O(1), only constant space is used.
+
+    func isPalindrome(_ s: String) -> Bool {
+        guard !s.isEmpty else { return true }
+
+        let s = Array(s)
+        var i = 0
+        var j = s.count - 1
+
+        while i < j {
+            if !s[i].isLetter, !s[i].isNumber { i += 1; continue }
+
+            if !s[j].isLetter, !s[j].isNumber { j -= 1; continue }
+
+            guard s[i].lowercased() == s[j].lowercased() else { return false }
+            i += 1
+            j -= 1
+        }
+
+        return true
+    }
+
+}
