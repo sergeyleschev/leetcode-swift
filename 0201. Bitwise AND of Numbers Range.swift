b9c4c474018fde864ec5b94dc7b5cc713d8ee0b2
Sergey Leschev <sergey.leschev@gmail.com>
2021-05-25 08:48:30 +0300

    201. Bitwise AND of Numbers Range

---
+++ 0201. Bitwise AND of Numbers Range.swift
@@ -0,0 +1,34 @@
+class Solution {
+
+    // 201. Bitwise AND of Numbers Range
+    // Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+    // Example 1:
+    // Input: left = 5, right = 7
+    // Output: 4
+
+    // Example 2:
+    // Input: left = 0, right = 0
+    // Output: 0
+
+    // Example 3:
+    // Input: left = 1, right = 2147483647
+    // Output: 0
+
+    // Constraints:
+    // 0 <= left <= right <= 2^31 - 1
+    
+    func rangeBitwiseAnd(_ left: Int, _ right: Int) -> Int {
+        var mask = 0
+
+        for i in 0..<32 {
+            let temp = 1 << (31 - i)
+            if temp & left == temp & right {
+                mask |= temp
+            } else { break }
+        }
+        
+        return right & mask
+    }
+
+}
