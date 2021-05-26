bce92419166ef1df03a34e933b68fe43378a3e7b
Sergey Leschev <sergey.leschev@gmail.com>
2021-05-26 23:50:00 +0300

    242. Valid Anagram

---
+++ 0242. Valid Anagram.swift
@@ -0,0 +1,44 @@
+class Solution {
+
+    // 242. Valid Anagram
+    // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    // Finds if t is an anagram of s.
+
+    // - Parameters:
+    //   - s: A string.
+    //   - t: A string.
+    // - Returns: True if t is an anagram of s, otherwise returns false.
+
+    // Example 1:
+    // Input: s = "anagram", t = "nagaram"
+    // Output: true
+
+    // Example 2:
+    // Input: s = "rat", t = "car"
+    // Output: false
+
+    // Constraints:
+    // 1 <= s.length, t.length <= 5 * 10^4
+    // s and t consist of lowercase English letters.
+
+    // Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+    // - Complexity:
+    //   - time: O(n), where n is the length of s.
+    //   - space: O(n), where n is the length of s.
+    
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        var dict = [Character: Int]()
+
+        for ch in s { dict[ch, default: 0] += 1 }
+        
+        for ch in t {
+            guard let val = dict[ch] else { return false }
+            dict[ch] = val > 1 ? val - 1 : nil
+        }
+        
+        return dict.isEmpty
+    }
+
+}
