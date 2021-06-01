edd23f23c118c063a76419d26b5475a44ed12529
Sergey Leschev <sergey.leschev@gmail.com>
2021-06-01 08:55:06 +0300

    504. Base 7

---
+++ 0504. Base 7.swift
@@ -0,0 +1,31 @@
+class Solution {
+
+    // 504. Base 7
+    // Given an integer num, return a string of its base 7 representation.
+
+    // Example 1:
+    // Input: num = 100
+    // Output: "202"
+
+    // Example 2:
+    // Input: num = -7
+    // Output: "-10"
+
+    // Constraints:
+    // -10^7 <= num <= 10^7
+
+    func convertToBase7(_ num: Int) -> String {
+        if num == 0 { return "0" }
+        var result = ""
+        var n = abs(num)
+        
+        while n > 0 {
+            let remainder = n % 7
+            result = "\(remainder)" + result
+            n /= 7
+        }
+        
+        return num < 0 ? "-" + result : result
+    }
+    
+}
