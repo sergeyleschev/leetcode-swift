b1b8f8e3c841044661b66a5057d0197dbe9530f1
Sergey Leschev <sergey.leschev@gmail.com>
2021-06-25 08:39:02 +0300

    986. Interval List Intersections

---
+++ 0986. Interval List Intersections.swift
@@ -0,0 +1,56 @@
+class Solution {
+
+    // 986. Interval List Intersections
+    // You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+    // Return the intersection of these two interval lists.
+    // A closed interval [a, b] (with a < b) denotes the set of real numbers x with a <= x <= b.
+    // The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+    // Example 1:
+    // Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+    // Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+
+    // Example 2:
+    // Input: firstList = [[1,3],[5,9]], secondList = []
+    // Output: []
+
+    // Example 3:
+    // Input: firstList = [], secondList = [[4,8],[10,12]]
+    // Output: []
+
+    // Example 4:
+    // Input: firstList = [[1,7]], secondList = [[3,10]]
+    // Output: [[3,7]]
+
+    // Constraints:
+    // 0 <= firstList.length, secondList.length <= 1000
+    // firstList.length + secondList.length >= 1
+    // 0 <= starti < endi <= 10^9
+    // endi < starti + 1
+    // 0 <= startj < endj <= 10^9
+    // endj < startj + 1
+
+    // - Complexity:
+    //   - time: O(M + N), where M, N are the length of A and B
+    //   - space: O(M + N), the maximum size of the anwser
+
+    func intervalIntersection(_ A: [[Int]], _ B: [[Int]]) -> [[Int]] {
+        if A.isEmpty || B.isEmpty { return [] }
+        var i = 0
+        var j = 0
+        var res = [[Int]]()
+        
+        while i < A.count && j < B.count {
+            if A[i][1] < B[j][1] {
+                if A[i][1] >= B[j][0] { res.append([max(A[i][0], B[j][0]), min(A[i][1], B[j][1])]) }
+                i += 1
+            } else {
+                if A[i][0] <= B[j][1] { res.append([max(A[i][0], B[j][0]), min(A[i][1], B[j][1])]) }
+                j += 1
+            }
+        }
+
+        return res
+    }
+
+}
