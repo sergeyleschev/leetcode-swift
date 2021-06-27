6a19c82863a34f67940ebe958fc02a12e56e1389
Sergey Leschev <sergey.leschev@gmail.com>
2021-06-27 15:35:56 +0300

    1115. Print FooBar Alternately

---
+++ 1115. Print FooBar Alternately.py
@@ -0,0 +1,49 @@
+# 1115. Print FooBar Alternately
+# Suppose you are given the following code:
+
+# class FooBar {
+#   public void foo() {
+#     for (int i = 0; i < n; i++) {
+#       print("foo");
+#     }
+#   }
+
+#   public void bar() {
+#     for (int i = 0; i < n; i++) {
+#       print("bar");
+#     }
+#   }
+# }
+# The same instance of FooBar will be passed to two different threads. Thread A will call foo() while thread B will call bar(). Modify the given program to output "foobar" n times.
+
+# Example 1:
+# Input: n = 1
+# Output: "foobar"
+# Explanation: There are two threads being fired asynchronously. One of them calls foo(), while the other calls bar(). "foobar" is being output 1 time.
+
+# Example 2:
+# Input: n = 2
+# Output: "foobarfoobar"
+# Explanation: "foobar" is being output 2 times.
+
+from threading import Semaphore, Barrier, Lock
+
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_lock = Lock()
+        self.bar_lock = Lock()
+        self.bar_lock.acquire()
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.foo_lock.acquire()
+            printFoo()
+            self.bar_lock.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.bar_lock.acquire()
+            printBar()
+            self.foo_lock.release()
