9091e3601a8f4b3c988d3daadfcc9714513c4771
Sergey Leschev <sergey.leschev@gmail.com>
2021-06-27 15:35:48 +0300

    1114. Print in Order

---
+++ 1114. Print in Order.py
@@ -0,0 +1,46 @@
+# 1114. Print in Order
+# Suppose we have a class:
+# public class Foo {
+#   public void first() { print("first"); }
+#   public void second() { print("second"); }
+#   public void third() { print("third"); }
+# }
+# The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+# Note:
+# We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seem to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
+
+# Example 1:
+# Input: nums = [1,2,3]
+# Output: "firstsecondthird"
+# Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+
+# Example 2:
+# Input: nums = [1,3,2]
+# Output: "firstsecondthird"
+# Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+
+from threading import Lock
+
+
+class Foo:
+    def __init__(self):
+        self.first_lock = Lock()
+        self.second_lock = Lock()
+        self.first_lock.acquire()
+        self.second_lock.acquire()
+
+    def first(self, printFirst: "Callable[[], None]") -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.first_lock.release()
+
+    def second(self, printSecond: "Callable[[], None]") -> None:
+        with self.first_lock:
+            # printSecond() outputs "second". Do not change or remove this line.
+            printSecond()
+            self.second_lock.release()
+
+    def third(self, printThird: "Callable[[], None]") -> None:
+        with self.second_lock:
+            # printThird() outputs "third". Do not change or remove this line.
+            printThird()
