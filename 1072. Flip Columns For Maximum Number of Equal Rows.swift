be64d926fedcc698ba67b9f95c9f54375cc9d266
Sergey Leschev <sergey.leschev@gmail.com>
2021-06-27 15:31:30 +0300

    1072. Flip Columns For Maximum Number of Equal Rows

---
+++ 1072. Flip Columns For Maximum Number of Equal Rows.swift
@@ -0,0 +1,44 @@
+class Solution {
+
+    // 1072. Flip Columns For Maximum Number of Equal Rows
+    // You are given an m x n binary matrix matrix.
+    // You can choose any number of columns in the matrix and flip every cell in that column (i.e., Change the value of the cell from 0 to 1 or vice versa).
+    // Return the maximum number of rows that have all values equal after some number of flips.
+
+    // Example 1:
+    // Input: matrix = [[0,1],[1,1]]
+    // Output: 1
+    // Explanation: After flipping no values, 1 row has all values equal.
+
+    // Example 2:
+    // Input: matrix = [[0,1],[1,0]]
+    // Output: 2
+    // Explanation: After flipping values in the first column, both rows have equal values.
+
+    // Example 3:
+    // Input: matrix = [[0,0,0],[0,0,1],[1,1,0]]
+    // Output: 2
+    // Explanation: After flipping values in the first two columns, the last two rows have equal values.
+
+    // Constraints:
+    // m == matrix.length
+    // n == matrix[i].length
+    // 1 <= m, n <= 300
+    // matrix[i][j] is either 0 or 1.
+
+    func maxEqualRowsAfterFlips(_ matrix: [[Int]]) -> Int {
+        var map = [String: Int]()
+
+        for row in matrix {
+            let key = row[0]  == 1 ? row.reduce("") { $0 + "\($1)" } : row.reduce("") { $0 + "\(1 ^ $1)" }
+            if let c = map[key] {
+                map.updateValue(c + 1, forKey: key)
+            } else {
+                map.updateValue(1, forKey: key)
+            }
+        }
+        
+        return map.values.max() ?? 0
+    }
+ 
+}
