b8f257513ddceede4224db2eab5298c9713e27c9
Sergey Leschev <sergey.leschev@gmail.com>
2021-06-28 07:58:52 +0300

    1218. Longest Arithmetic Subsequence of Given Difference

---
+++ 1218. Longest Arithmetic Subsequence of Given Difference.swift
@@ -0,0 +1,43 @@
+class Solution {
+
+    // 1218. Longest Arithmetic Subsequence of Given Difference
+    // Given an integer array arr and an integer difference, return the length of the longest subsequence in arr which is an arithmetic sequence such that the difference between adjacent elements in the subsequence equals difference.
+    // A subsequence is a sequence that can be derived from arr by deleting some or no elements without changing the order of the remaining elements.
+
+    // Example 1:
+    // Input: arr = [1,2,3,4], difference = 1
+    // Output: 4
+    // Explanation: The longest arithmetic subsequence is [1,2,3,4].
+
+    // Example 2:
+    // Input: arr = [1,3,5,7], difference = 1
+    // Output: 1
+    // Explanation: The longest arithmetic subsequence is any single element.
+
+    // Example 3:
+    // Input: arr = [1,5,7,8,5,3,4,2,1], difference = -2
+    // Output: 4
+    // Explanation: The longest arithmetic subsequence is [7,5,3,1].
+
+    // Constraints:
+    // 1 <= arr.length <= 10^5
+    // -10^4 <= arr[i], difference <= 10^4
+
+    func longestSubsequence(_ arr: [Int], _ difference: Int) -> Int {
+        var ans = 1
+        var map = [Int: Int]()
+
+        for num in arr {
+            let target = num - difference
+            if let c = map[target] {
+                map[num] = c + 1
+                ans = max(ans, c + 1)
+            } else {
+                map[num] = 1
+            }
+        }
+        
+        return ans
+    }
+
+}
