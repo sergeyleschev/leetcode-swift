65a31c56c70a3de03717b7e499ce58204865eada
Sergey Leschev <sergey.leschev@gmail.com>
2021-07-03 18:58:25 +0300

    1250. Check If It Is a Good Array

---
+++ 1250. Check If It Is a Good Array.swift
@@ -0,0 +1,45 @@
+class Solution {
+
+    // 1250. Check If It Is a Good Array
+    // Given an array nums of positive integers. Your task is to select some subset of nums, multiply each element by an integer and add all these numbers. The array is said to be good if you can obtain a sum of 1 from the array by any possible subset and multiplicand.
+    // Return True if the array is good otherwise return False.
+
+    // Example 1:
+    // Input: nums = [12,5,7,23]
+    // Output: true
+    // Explanation: Pick numbers 5 and 7.
+    // 5*3 + 7*(-2) = 1
+
+    // Example 2:
+    // Input: nums = [29,6,10]
+    // Output: true
+    // Explanation: Pick numbers 29, 6 and 10.
+    // 29*1 + 6*(-3) + 10*(-1) = 1
+
+    // Example 3:
+    // Input: nums = [3,6]
+    // Output: false
+
+    // Constraints:
+    // 1 <= nums.length <= 10^5
+    // 1 <= nums[i] <= 10^9
+
+    func isGoodArray(_ nums: [Int]) -> Bool {
+        guard nums.count > 1 else { return nums.isEmpty ? false : nums[0] == 1 }
+        var ans = nums[0]
+        
+        for num in nums {
+            ans = gcd(ans, num)
+            guard ans != 1 else { return true }
+        }
+
+        return false
+    }
+
+
+    private func gcd(_ a: Int, _ b: Int) -> Int {
+        guard b != 0 else { return a }
+        return gcd(b, a % b)
+    }
+
+}
