26c2eb558007d5d8c90e7a0ba88a62ac04b97547
Sergey Leschev <sergey.leschev@gmail.com>
2021-07-04 23:05:59 +0300

    1268. Search Suggestions System

---
+++ 1268. Search Suggestions System.swift
@@ -0,0 +1,81 @@
+class Solution {
+
+    // 1268. Search Suggestions System
+    // Given an array of strings products and a string searchWord. We want to design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with the searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+    // Return list of lists of the suggested products after each character of searchWord is typed. 
+
+    // Example 1:
+    // Input: products = ["mobile","mouse","moneypot","monitor","mousepad"], searchWord = "mouse"
+    // Output: [
+    // ["mobile","moneypot","monitor"],
+    // ["mobile","moneypot","monitor"],
+    // ["mouse","mousepad"],
+    // ["mouse","mousepad"],
+    // ["mouse","mousepad"]
+    // ]
+    // Explanation: products sorted lexicographically = ["mobile","moneypot","monitor","mouse","mousepad"]
+    // After typing m and mo all products match and we show user ["mobile","moneypot","monitor"]
+    // After typing mou, mous and mouse the system suggests ["mouse","mousepad"]
+
+    // Example 2:
+    // Input: products = ["havana"], searchWord = "havana"
+    // Output: [["havana"],["havana"],["havana"],["havana"],["havana"],["havana"]]
+
+    // Example 3:
+    // Input: products = ["bags","baggage","banner","box","cloths"], searchWord = "bags"
+    // Output: [["baggage","bags","banner"],["baggage","bags","banner"],["baggage","bags"],["bags"]]
+
+    // Example 4:
+    // Input: products = ["havana"], searchWord = "tatiana"
+    // Output: [[],[],[],[],[],[],[]]
+
+    // Constraints:
+    // 1 <= products.length <= 1000
+    // There are no repeated elements in products.
+    // 1 <= Σ products[i].length <= 2 * 10^4
+    // All characters of products[i] are lower-case English letters.
+    // 1 <= searchWord.length <= 1000
+    // All characters of searchWord are lower-case English letters.
+
+    // Approach: Brute Force
+
+    private let maximumSuggestedNumber = 3
+
+
+    func suggestedProducts(_ products: [String], _ searchWord: String) -> [[String]] {
+        let products = products.sorted()
+        var results = [[String]]()
+        var map = [String: Int]()
+        var word = ""
+
+        for (i, product) in products.enumerated() { map[product] = i }
+
+        for c in searchWord {
+            word = word + String(c)
+            var newResult = [String]()
+            var startIndex = 0
+
+            if let prevResult = results.last {
+                for product in prevResult {
+                    if product.hasPrefix(word) {
+                        newResult.append(product)
+                        startIndex = map[product]! + 1
+                    }
+
+                    if newResult.count == maximumSuggestedNumber { break  }
+                }
+            }
+
+            if newResult.count < maximumSuggestedNumber {
+                for i in startIndex..<products.count {
+                    if products[i].hasPrefix(word) { newResult.append(products[i]) }
+                    if newResult.count == maximumSuggestedNumber { break }
+                }
+            }
+            results.append(newResult)
+        }
+        
+        return results
+    }
+
+}
