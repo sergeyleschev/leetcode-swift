2318f532e419322687e0188d94cd12050e355273
Sergey Leschev <sergey.leschev@gmail.com>
2021-07-08 21:36:41 +0300

    1325. Delete Leaves With a Given Value

---
+++ 1325. Delete Leaves With a Given Value.swift
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+
+    // 1325. Delete Leaves With a Given Value
+    // Given a binary tree root and an integer target, delete all the leaf nodes with value target.
+    // Note that once you delete a leaf node with value target, if it's parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you can't).
+
+    // Example 1:
+    // Input: root = [1,2,3,2,null,2,4], target = 2
+    // Output: [1,null,3,null,4]
+    // Explanation: Leaf nodes in green with value (target = 2) are removed (Picture in left). 
+    // After removing, new nodes become leaf nodes with value (target = 2) (Picture in center).
+
+    // Example 2:
+    // Input: root = [1,3,3,3,2], target = 3
+    // Output: [1,3,null,null,2]
+
+    // Example 3:
+    // Input: root = [1,2,null,2,null,2], target = 2
+    // Output: [1]
+    // Explanation: Leaf nodes in green with value (target = 2) are removed at each step.
+
+    // Example 4:
+    // Input: root = [1,1,1], target = 1
+    // Output: []
+
+    // Example 5:
+    // Input: root = [1,2,3], target = 1
+    // Output: [1,2,3]
+
+    // Constraints:
+    // 1 <= target <= 1000
+    // The given binary tree will have between 1 and 3000 nodes.
+    // Each node's value is between [1, 1000].
+
+    func removeLeafNodes(_ root: TreeNode?, _ target: Int) -> TreeNode? { delete(root, target) }
+
+    
+    func delete(_ root: TreeNode?, _ target: Int) -> TreeNode? {
+        guard let root = root else { return nil }
+
+        root.left = delete(root.left, target)
+        root.right = delete(root.right, target)
+        
+        if root.val == target && root.right == nil && root.left == nil { return nil } else { return root }
+    }
+
+}
