50f738ad340aeee82c83d300e623db1aa76b2134
Sergey Leschev <sergey.leschev@gmail.com>
2021-07-11 11:42:57 +0300

    1374. Generate a String With Characters That Have Odd Counts

---
+++ 1374. Generate a String With Characters That Have Odd Counts.swift
@@ -0,0 +1,32 @@
+class Solution {
+
+    // 1374. Generate a String With Characters That Have Odd Counts
+    // Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+    // The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
+
+    // Example 1:
+    // Input: n = 4
+    // Output: "pppz"
+    // Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+
+    // Example 2:
+    // Input: n = 2
+    // Output: "xy"
+    // Explanation: "xy" is a valid string since the characters 'x' and 'y' occur once. Note that there are many other valid strings such as "ag" and "ur".
+
+    // Example 3:
+    // Input: n = 7
+    // Output: "holasss"
+
+    // Constraints:
+    // 1 <= n <= 500
+
+    func generateTheString(_ n: Int) -> String {
+        if n % 2 == 0 {
+            return String(Array(repeating: "a", count: n - 1)) + "b"
+        } else {
+            return String(Array(repeating: "a", count: n))
+        }
+    }
+    
+}
