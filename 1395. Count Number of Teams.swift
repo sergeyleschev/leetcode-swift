c94cb5fc326c14c9740383f0537ab7c29eaf3f52
Sergey Leschev <sergey.leschev@gmail.com>
2021-07-11 22:09:17 +0300

    1395. Count Number of Teams

---
+++ 1395. Count Number of Teams.swift
@@ -0,0 +1,62 @@
+class Solution {
+
+    // 1395. Count Number of Teams
+    // There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+    // You have to form a team of 3 soldiers amongst them under the following rules:
+    // Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+    // A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+    // Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+
+    // Example 1:
+    // Input: rating = [2,5,3,4,1]
+    // Output: 3
+    // Explanation: We can form three teams given the conditions. (2,3,4), (5,4,1), (5,3,1). 
+
+    // Example 2:
+    // Input: rating = [2,1,3]
+    // Output: 0
+    // Explanation: We can't form any team given the conditions.
+
+    // Example 3:
+    // Input: rating = [1,2,3,4]
+    // Output: 4
+
+    // Constraints:
+    // n == rating.length
+    // 3 <= n <= 1000
+    // 1 <= rating[i] <= 10^5
+    // All the integers in rating are unique.
+
+    func numTeams(_ rating: [Int]) -> Int {
+        var teamCount = 0
+
+        for i in 1..<rating.count - 1{
+            let origin = rating[i]
+            var leftSmaller = 0
+            var leftLarger = 0
+            var rightSmaller = 0
+            var rightLarger = 0
+
+            for j in 0..<i {
+                if rating[j] < origin {
+                    leftSmaller += 1
+                } else {
+                    leftLarger += 1
+                }
+            }
+
+            for j in i + 1..<rating.count {
+                if rating[j] < origin {
+                    rightSmaller += 1
+                } else {
+                    rightLarger += 1
+                }
+            }
+
+            teamCount += (leftSmaller * rightLarger) + (leftLarger * rightSmaller)
+        }
+
+        return teamCount
+    }
+
+}
