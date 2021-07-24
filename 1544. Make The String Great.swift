aac3a58ecaec16d20e475b77d096072cf2eb47c7
Sergey Leschev <sergey.leschev@gmail.com>
2021-07-24 18:33:56 +0300

    1544. Make The String Great

---
+++ 1544. Make The String Great.swift
@@ -0,0 +1,51 @@
+class Solution {
+
+    // 1544. Make The String Great
+    // Given a string s of lower and upper case English letters.
+    // A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+    // 0 <= i <= s.length - 2
+    // s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+    // To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+
+    // Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+    // Notice that an empty string is also good.
+
+    // Example 1:
+    // Input: s = "leEeetcode"
+    // Output: "leetcode"
+    // Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+
+    // Example 2:
+    // Input: s = "abBAcC"
+    // Output: ""
+    // Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+    // "abBAcC" --> "aAcC" --> "cC" --> ""
+    // "abBAcC" --> "abBA" --> "aA" --> ""
+
+    // Example 3:
+    // Input: s = "s"
+    // Output: "s"
+
+    // Constraints:
+    // 1 <= s.length <= 100
+    // s contains only lower and upper case English letters.
+
+    func makeGood(_ s: String) -> String {
+        var arrS = s.map{ Int($0.asciiValue!) }
+        var i = 0
+        
+        while i < arrS.count - 1 {
+            if 32 == abs(arrS[i] - arrS[i + 1]) {
+                arrS.remove(at: i)
+                arrS.remove(at: i)
+                i = 0
+                continue
+            }
+            i += 1
+        }
+        
+        return String(arrS.map{Character(UnicodeScalar( $0 )!)})
+    }
+
+}
