9e29503e3db7d2c19e323f35229977b54c7728b5
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-01 08:04:54 +0300

    1656. Design an Ordered Stream

---
+++ 1656. Design an Ordered Stream.swift
@@ -0,0 +1,67 @@
+class OrderedStream {
+
+    // 1656. Design an Ordered Stream
+    // There is a stream of n (idKey, value) pairs arriving in an arbitrary order, where idKey is an integer between 1 and n and value is a string. No two pairs have the same id.
+    // Design a stream that returns the values in increasing order of their IDs by returning a chunk (list) of values after each insertion. The concatenation of all the chunks should result in a list of the sorted values.
+    // Implement the OrderedStream class:
+    // OrderedStream(int n) Constructs the stream to take n values.
+    // String[] insert(int idKey, String value) Inserts the pair (idKey, value) into the stream, then returns the largest possible chunk of currently inserted values that appear next in the order.
+
+    // Example:
+    // Input
+    // ["OrderedStream", "insert", "insert", "insert", "insert", "insert"]
+    // [[5], [3, "ccccc"], [1, "aaaaa"], [2, "bbbbb"], [5, "eeeee"], [4, "ddddd"]]
+    // Output
+    // [null, [], ["aaaaa"], ["bbbbb", "ccccc"], [], ["ddddd", "eeeee"]]
+
+    // Explanation
+    // // Note that the values ordered by ID is ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"].
+    // OrderedStream os = new OrderedStream(5);
+    // os.insert(3, "ccccc"); // Inserts (3, "ccccc"), returns [].
+    // os.insert(1, "aaaaa"); // Inserts (1, "aaaaa"), returns ["aaaaa"].
+    // os.insert(2, "bbbbb"); // Inserts (2, "bbbbb"), returns ["bbbbb", "ccccc"].
+    // os.insert(5, "eeeee"); // Inserts (5, "eeeee"), returns [].
+    // os.insert(4, "ddddd"); // Inserts (4, "ddddd"), returns ["ddddd", "eeeee"].
+    // // Concatentating all the chunks returned:
+    // // [] + ["aaaaa"] + ["bbbbb", "ccccc"] + [] + ["ddddd", "eeeee"] = ["aaaaa", "bbbbb", "ccccc", "ddddd", "eeeee"]
+    // // The resulting order is the same as the order above.
+     
+    // Constraints:
+    // 1 <= n <= 1000
+    // 1 <= id <= n
+    // value.length == 5
+    // value consists only of lowercase letters.
+    // Each call to insert will have a unique id.
+    // Exactly n calls will be made to insert.
+
+    var index = 0
+    var n = 0
+    var array: [String] = []
+    
+
+    init(_ n: Int) {
+        self.n = n
+        self.array = Array(repeating: "", count: n)
+    }
+    
+
+    func insert(_ id: Int, _ value: String) -> [String] {
+        var chunkStrs:[String] = []    
+        self.array[id - 1] = value
+        
+        while self.index < self.n {
+            if self.array[self.index] != ""{
+                chunkStrs.append(self.array[self.index])
+                self.index += 1
+            } else { break }
+        }
+        return chunkStrs
+    }
+
+}
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * let obj = OrderedStream(n)
+ * let ret_1: [String] = obj.insert(idKey, value)
+ */
