c38d2d322fcb07712f3e78b2546da53c9b8b111d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-04 22:13:48 +0300

    1725. Number Of Rectangles That Can Form The Largest Square

---
+++ 1725. Number Of Rectangles That Can Form The Largest Square.swift
@@ -0,0 +1,41 @@
+class Solution {
+
+    // 1725. Number Of Rectangles That Can Form The Largest Square
+    // You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+    // You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+    // Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+
+    // Return the number of rectangles that can make a square with a side length of maxLen.
+
+    // Example 1:
+    // Input: rectangles = [[5,8],[3,9],[5,12],[16,5]]
+    // Output: 3
+    // Explanation: The largest squares you can get from each rectangle are of lengths [5,3,5,5].
+    // The largest possible square is of length 5, and you can get it out of 3 rectangles.
+
+    // Example 2:
+    // Input: rectangles = [[2,3],[3,7],[4,3],[3,7]]
+    // Output: 3
+
+    // Constraints:
+    // 1 <= rectangles.length <= 1000
+    // rectangles[i].length == 2
+    // 1 <= li, wi <= 10^9
+    // li != wi
+
+    func countGoodRectangles(_ rectangles: [[Int]]) -> Int {
+        var maxLen = 0
+        var squares: [Int] = []
+        var ans = 0
+        
+        for edges in rectangles {
+            let edge = min(edges[0], edges[1])
+            if edge >= maxLen { squares.append(edge) }
+            maxLen = max(maxLen, edge)
+        }
+        
+        for square in squares where maxLen == square { ans += 1 }
+        return ans
+    }
+
+}
