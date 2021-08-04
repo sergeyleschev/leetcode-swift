7e188161f2b060a8f5fe14e603e907b3f5c9d0e0
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-04 22:13:14 +0300

    1720. Decode XORed Array

---
+++ 1720. Decode XORed Array.swift
@@ -0,0 +1,31 @@
+class Solution {
+
+    // 1720. Decode XORed Array
+    // There is a hidden integer array arr that consists of n non-negative integers.
+    // It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+    // You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+
+    // Return the original array arr. It can be proved that the answer exists and is unique.
+
+    // Example 1:
+    // Input: encoded = [1,2,3], first = 1
+    // Output: [1,0,2,1]
+    // Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+
+    // Example 2:
+    // Input: encoded = [6,2,7,3], first = 4
+    // Output: [4,2,0,7,4]
+
+    // Constraints:
+    // 2 <= n <= 10^4
+    // encoded.length == n - 1
+    // 0 <= encoded[i] <= 10^5
+    // 0 <= first <= 10^5
+
+    func decode(_ encoded: [Int], _ first: Int) -> [Int] {
+        var ans = [first]
+        for num in encoded { ans.append(ans.last! ^ num) }
+        return ans
+    }
+    
+}
