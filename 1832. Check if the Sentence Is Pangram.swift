e0152282711ff32d7d208211ef99c81f0895a46b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-07 22:12:34 +0300

    1832. Check if the Sentence Is Pangram

---
+++ 1832. Check if the Sentence Is Pangram.swift
@@ -0,0 +1,22 @@
+class Solution {
+
+    // 1832. Check if the Sentence Is Pangram
+    // A pangram is a sentence where every letter of the English alphabet appears at least once.
+    // Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+    // Example 1:
+    // Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+    // Output: true
+    // Explanation: sentence contains at least one of every letter of the English alphabet.
+
+    // Example 2:
+    // Input: sentence = "leetcode"
+    // Output: false
+
+    // Constraints:
+    // 1 <= sentence.length <= 1000
+    // sentence consists of lowercase English letters.
+
+    func checkIfPangram(_ sentence: String) -> Bool { Set<Character>(sentence).count == 26 }
+    
+}
