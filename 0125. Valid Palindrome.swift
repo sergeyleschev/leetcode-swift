569cf5f5bb19487356f30227ba16b99351febbf5
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-09 08:11:11 +0300

    125. Valid Palindrome

--- 0125. Valid Palindrome.swift
+++ 0125. Valid Palindrome.swift
@@ -18,7 +18,7 @@ class Solution {
     // Explanation: "raceacar" is not a palindrome.
 
     // Constraints:
-    // 1 <= s.length <= 2 * 105
+    // 1 <= s.length <= 2 * 10^5
     // s consists only of printable ASCII characters.
 
     // - Complexity:
