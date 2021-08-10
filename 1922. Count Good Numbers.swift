ad786eb8ac959bc14309c92eef1b2123474dba06
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-10 22:30:30 +0300

    1922. Count Good Numbers

---
+++ 1922. Count Good Numbers.swift
@@ -0,0 +1,39 @@
+class Solution {
+
+    // 1922. Count Good Numbers
+    // A digit string is good if the digits (0-indexed) at even indices are even and the digits at odd indices are prime (2, 3, 5, or 7).
+    // For example, "2582" is good because the digits (2 and 8) at even positions are even and the digits (5 and 2) at odd positions are prime. However, "3245" is not good because 3 is at an even index but is not even.
+    // Given an integer n, return the total number of good digit strings of length n. Since the answer may be large, return it modulo 109 + 7.
+    // A digit string is a string consisting of digits 0 through 9 that may contain leading zeros.
+
+    // Example 1:
+    // Input: n = 1
+    // Output: 5
+    // Explanation: The good numbers of length 1 are "0", "2", "4", "6", "8".
+
+    // Example 2:
+    // Input: n = 4
+    // Output: 400
+
+    // Example 3:
+    // Input: n = 50
+    // Output: 564908303
+
+    // Constraints:
+    // 1 <= n <= 10^15
+
+    private let mod = 1_000_000_007
+
+
+    func countGoodNumbers(_ n: Int) -> Int { quickPower(20, n >> 1) * (n % 2 == 0 ? 1 : 5) % mod }
+
+    private func quickPower(_ bottom: Int,_ exponent: Int) -> Int {
+        guard exponent > 1 else { return exponent == 0 ? 1 : bottom }
+        let half = quickPower(bottom, exponent >> 1) % mod
+        let ans = half % mod * half % mod
+        
+        if exponent % 2 == 0 { return ans } 
+        else { return bottom * ans % mod }
+    }
+
+}
