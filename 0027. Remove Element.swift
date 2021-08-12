99d6aafda0fd6b1ba92ce233fda1436018ab72a1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 23:01:14 +0300

    0027. Remove Element

--- 0027. Remove Element.swift
+++ 0027. Remove Element.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 27. Remove Element
     // Given an array nums and a value val, remove all instances of that value in-place and return the new length.
     // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
