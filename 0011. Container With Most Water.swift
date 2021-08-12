8b70ba74d1adfcb8424e99f3d325c9281f297873
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 22:57:33 +0300

    0011. Container With Most Water

--- 0011. Container With Most Water.swift
+++ 0011. Container With Most Water.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State Universitys
+
     // 11. Container With Most Water
     // Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
     // Notice that you may not slant the container.
@@ -38,7 +40,6 @@ class Solution {
     func maxArea(_ height: [Int]) -> Int {
         var start = 0
         var end = height.count - 1
-        
         var ans = 0
         
         while start < end {
