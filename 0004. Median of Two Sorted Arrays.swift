b2b9808f4ef9c300fd00fd3750c289bf94e5b59a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 22:56:02 +0300

    0004. Median of Two Sorted Arrays

--- 0004. Median of Two Sorted Arrays.swift
+++ 0004. Median of Two Sorted Arrays.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 4. Median of Two Sorted Arrays
     // Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
     // The overall run time complexity should be O(log (m+n)).
