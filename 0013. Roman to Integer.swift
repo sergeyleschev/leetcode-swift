a510a280611e396125599e4653765dc9fe851d61
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 22:57:56 +0300

    0013. Roman to Integer

--- 0013. Roman to Integer.swift
+++ 0013. Roman to Integer.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 13. Roman to Integer
 	// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 	// Symbol       Value
