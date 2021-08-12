c0fd730ac2a82f2114d53c1007a6832772153014
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 22:58:22 +0300

    0015. 3Sum

--- 0015. 3Sum.swift
+++ 0015. 3Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State Universitys
+
     // 15. 3Sum
     // Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
     // Notice that the solution set must not contain duplicate triplets.
