94298745a41b92c099dcba26b8ab637417918d0b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 23:01:46 +0300

    0030. Substring with Concatenation of All Words

--- 0030. Substring with Concatenation of All Words.swift
+++ 0030. Substring with Concatenation of All Words.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 30. Substring with Concatenation of All Words
     // You are given a string s and an array of strings words of the same length. Return all starting indices of substring(s) in s that is a concatenation of each word in words exactly once, in any order, and without any intervening characters.
     // You can return the answer in any order.
