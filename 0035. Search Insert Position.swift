1d6a827a34a8b631a05a21ca348cdee264da2894
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 23:03:01 +0300

    0035. Search Insert Position

--- 0035. Search Insert Position.swift
+++ 0035. Search Insert Position.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 35. Search Insert Position
     // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
     // You must write an algorithm with O(log n) runtime complexity.
