cca81339246d60f65fda55ed2c74982e70128e3d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 23:01:25 +0300

    0028. Implement strStr()

--- 0028. Implement strStr().swift
+++ 0028. Implement strStr().swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 28. Implement strStr()
 	// Implement strStr().
 	// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
