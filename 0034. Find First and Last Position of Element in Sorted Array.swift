203c6d131ae25d682252d385bebfad6917539c19
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 23:02:29 +0300

    0034. Find First and Last Position of Element in Sorted Array

--- 0034. Find First and Last Position of Element in Sorted Array.swift
+++ 0034. Find First and Last Position of Element in Sorted Array.swift
@@ -1,4 +1,6 @@
 class Solution {
+
+    // Solution @ Sergey Leschev, Belarusian State University
     
     // 34. Find First and Last Position of Element in Sorted Array
     // Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
