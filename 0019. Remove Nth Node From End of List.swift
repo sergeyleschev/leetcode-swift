9d250fbf375749bfd5047af5f946f89d6e842ed7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 22:59:07 +0300

    0019. Remove Nth Node From End of List

--- 0019. Remove Nth Node From End of List.swift
+++ 0019. Remove Nth Node From End of List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 19. Remove Nth Node From End of List
     // Given the head of a linked list, remove the nth node from the end of the list and return its head.
     // Follow up: Could you do this in one pass?
