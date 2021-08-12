0aa612119a2f2c86d6bc0bcb7f28e722c6e8e5bb
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 23:00:42 +0300

    0024. Swap Nodes in Pairs

--- 0024. Swap Nodes in Pairs.swift
+++ 0024. Swap Nodes in Pairs.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 24. Swap Nodes in Pairs
     // Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
