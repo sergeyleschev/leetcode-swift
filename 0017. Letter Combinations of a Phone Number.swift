3f1f6525a4b16e941e1b294665a6cf2906815bbe
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 22:58:45 +0300

    0017. Letter Combinations of a Phone Number

--- 0017. Letter Combinations of a Phone Number.swift
+++ 0017. Letter Combinations of a Phone Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 17. Letter Combinations of a Phone Number
     // Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
     // A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
