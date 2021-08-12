d191f25ee0144640e7690d9b297cbd4deacbab07
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 23:00:10 +0300

    0021. Merge Two Sorted Lists

--- 0021. Merge Two Sorted Lists.swift
+++ 0021. Merge Two Sorted Lists.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 21. Merge Two Sorted Lists
 	// Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
 
