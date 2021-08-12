913db9ac01a532f16460184ecd4647be324b15e1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-12 22:58:32 +0300

    0016. 3Sum Closest

--- 0016. 3Sum Closest.swift
+++ 0016. 3Sum Closest.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 16. 3Sum Closest
     // Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
     
