b8d103fcf2f8ea2bc708cbe4efed7a9b57be51dc
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:02:39 +0300

    0043. Multiply Strings

--- 0043. Multiply Strings.swift
+++ 0043. Multiply Strings.swift
@@ -1,4 +1,6 @@
 class Solution {
+
+	// Solution @ Sergey Leschev, Belarusian State University
 	
 	// 43. Multiply Strings
 	// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
