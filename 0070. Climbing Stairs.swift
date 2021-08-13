85ed7fb6da6d52a3555bd32c0ccfc45c712a0967
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:08:00 +0300

    70. Climbing Stairs

--- 0070. Climbing Stairs.swift
+++ 0070. Climbing Stairs.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 70. Climbing Stairs
     // You are climbing a staircase. It takes n steps to reach the top.
     // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
