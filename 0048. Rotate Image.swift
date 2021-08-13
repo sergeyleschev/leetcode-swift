6997fa5f6d0815f1a1052f9c755689d6b579d4d0
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:03:49 +0300

    0048. Rotate Image

--- 0048. Rotate Image.swift
+++ 0048. Rotate Image.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 48. Rotate Image
 	// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 	// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
