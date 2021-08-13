44a1b8d2c6d4b2d0557174f9cca07152e7987853
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:07:27 +0300

    66. Plus One

--- 0066. Plus One.swift
+++ 0066. Plus One.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 66. Plus One
     // Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
     // The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
