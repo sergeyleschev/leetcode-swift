1196b79cd02aad182e88474453111fd3d7943cd2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:03:38 +0300

    0047. Permutations II

--- 0047. Permutations II.swift
+++ 0047. Permutations II.swift
@@ -1,5 +1,6 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
 
     // 47. Permutations II
     // Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
