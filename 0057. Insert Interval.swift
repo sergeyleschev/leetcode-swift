3d66a70969c9215da2339f20714e9767fd3a7ed7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:06:01 +0300

    57. Insert Interval

--- 0057. Insert Interval.swift
+++ 0057. Insert Interval.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 57. Insert Interval
     // Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
     // You may assume that the intervals were initially sorted according to their start times.
