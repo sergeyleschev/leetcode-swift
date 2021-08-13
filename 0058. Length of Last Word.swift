befef00820552675bd6b96f08c61ecf2f6e0a02a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:06:11 +0300

    58. Length of Last Word

--- 0058. Length of Last Word.swift
+++ 0058. Length of Last Word.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 58. Length of Last Word
     // Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
     // A word is a maximal substring consisting of non-space characters only.
