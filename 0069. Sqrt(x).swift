65988e51927789dfa5a12e084012d2717c1bb137
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:07:52 +0300

    69. Sqrt(x)

--- 0069. Sqrt(x).swift
+++ 0069. Sqrt(x).swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 69. Sqrt(x)
     // Given a non-negative integer x, compute and return the square root of x.
     // Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
