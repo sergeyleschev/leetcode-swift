06f5feb310736d1e786879667654502b39b5b866
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:05:53 +0300

    56. Merge Intervals

--- 0056. Merge Intervals.swift
+++ 0056. Merge Intervals.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 56. Merge Intervals
     // Given an array of intervals where intervals[i] = [start i, end i], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
