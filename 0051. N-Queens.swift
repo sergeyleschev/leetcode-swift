ab1f8e196def4092970de984922fa6dadc7380f4
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:04:22 +0300

    0051. N-Queens

--- 0051. N-Queens.swift
+++ 0051. N-Queens.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 51. N-Queens
     // The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
     // Given an integer n, return all distinct solutions to the n-queens puzzle.
