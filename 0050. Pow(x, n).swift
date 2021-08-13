8c4691d7286bcf363ab0c9a2be3281491b867c09
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:04:12 +0300

    0050. Pow(x, n)

--- 0050. Pow(x, n).swift
+++ 0050. Pow(x, n).swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 50. Pow(x, n)
     // Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
