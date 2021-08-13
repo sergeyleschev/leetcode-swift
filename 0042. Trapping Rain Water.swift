0bec7345139d617321e2bf5242e07268ae3cdf6b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:02:27 +0300

    0042. Trapping Rain Water

--- 0042. Trapping Rain Water.swift
+++ 0042. Trapping Rain Water.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 42. Trapping Rain Water
     // Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
