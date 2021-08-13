c6dd14645b89c5719646dc7937d8907c54783c26
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:06:30 +0300

    60. Permutation Sequence

--- 0060. Permutation Sequence.swift
+++ 0060. Permutation Sequence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 60. Permutation Sequence
     // The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
     // By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
