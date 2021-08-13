0265f86c83f9f1c9b059dd8ac44b458d1e8b085e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:05:22 +0300

    53. Maximum Subarray

--- 0053. Maximum Subarray.swift
+++ 0053. Maximum Subarray.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 53. Maximum Subarray
     // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
