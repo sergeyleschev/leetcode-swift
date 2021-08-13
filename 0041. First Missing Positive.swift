1e1f6ec268645468e7c6e9fe201753ec27f11169
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:02:17 +0300

    0041. First Missing Positive

--- 0041. First Missing Positive.swift
+++ 0041. First Missing Positive.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 41. First Missing Positive
     // Given an unsorted integer array nums, find the smallest missing positive integer.
     // You must implement an algorithm that runs in O(n) time and uses constant extra space.
