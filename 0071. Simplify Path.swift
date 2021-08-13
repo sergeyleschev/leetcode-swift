573ca6c0e7be1017700e8a845d186b5009447d5a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-13 08:08:09 +0300

    71. Simplify Path

--- 0071. Simplify Path.swift
+++ 0071. Simplify Path.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 71. Simplify Path
 
     // Given a string path, which is an absolute path (starting with a slash '/') to a file or directory in a Unix-style file system, convert it to the simplified canonical path.
