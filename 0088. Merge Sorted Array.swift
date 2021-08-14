ebf24f86fa9771bbd241b02f731a449c13d3da64
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:41:10 +0300

    88. Merge Sorted Array

--- 0088. Merge Sorted Array.swift
+++ 0088. Merge Sorted Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+    
     // 88. Merge Sorted Array
     // Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
     // The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
