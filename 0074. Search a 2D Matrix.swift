25d727b1a8eda5cb990771e0086e3c4601bcace1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:39:05 +0300

    74. Search a 2D Matrix

--- 0074. Search a 2D Matrix.swift
+++ 0074. Search a 2D Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 74. Search a 2D Matrix
     // Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
     // Integers in each row are sorted from left to right.
