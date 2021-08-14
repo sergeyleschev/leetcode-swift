a1e0e2d5e7b150c6692fc07be50b83dd1eaf1d85
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:39:59 +0300

    80. Remove Duplicates from Sorted Array II

--- 0080. Remove Duplicates from Sorted Array II.swift
+++ 0080. Remove Duplicates from Sorted Array II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 80. Remove Duplicates from Sorted Array II
     // Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
     // Do not allocate extra space for another array; you must do this by modifying the input array in-place with O(1) extra memory.
