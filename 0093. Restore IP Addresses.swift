5954bdb94ef4cd2cc9dbec772df8b6d2f3836130
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:42:37 +0300

    93. Restore IP Addresses

--- 0093. Restore IP Addresses.swift
+++ 0093. Restore IP Addresses.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 93. Restore IP Addresses
     // Given a string s containing only digits, return all possible valid IP addresses that can be obtained from s. You can return them in any order.
     // A valid IP address consists of exactly four integers, each integer is between 0 and 255, separated by single dots and cannot have leading zeros. For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses and "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses. 
