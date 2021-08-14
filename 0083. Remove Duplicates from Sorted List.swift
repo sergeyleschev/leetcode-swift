abb791bd0bcf3349a00dd629cfa312380cec01e8
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:40:28 +0300

    83. Remove Duplicates from Sorted List

--- 0083. Remove Duplicates from Sorted List.swift
+++ 0083. Remove Duplicates from Sorted List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 83. Remove Duplicates from Sorted List
     // Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 
