b7b1751f232818b832cb3576dd4c72a779b492c2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:43:52 +0300

    102. Binary Tree Level Order Traversal

--- 0102. Binary Tree Level Order Traversal.swift
+++ 0102. Binary Tree Level Order Traversal.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 102. Binary Tree Level Order Traversal
     // Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
 
