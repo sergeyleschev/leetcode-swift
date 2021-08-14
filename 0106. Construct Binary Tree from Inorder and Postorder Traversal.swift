d442da33741ec8f6291e2cd585812c56edf2c375
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:44:28 +0300

    106. Construct Binary Tree from Inorder and Postorder Traversal

--- 0106. Construct Binary Tree from Inorder and Postorder Traversal.swift
+++ 0106. Construct Binary Tree from Inorder and Postorder Traversal.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 106. Construct Binary Tree from Inorder and Postorder Traversal
     // Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
 
