82c19bfddea742670d9c3bc60c67e60580ab4e0e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:39:41 +0300

    78. Subsets

--- 0078. Subsets.swift
+++ 0078. Subsets.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
 	// 78. Subsets
 	// Given an integer array nums of unique elements, return all possible subsets (the power set).
 	// The solution set must not contain duplicate subsets. Return the solution in any order.
