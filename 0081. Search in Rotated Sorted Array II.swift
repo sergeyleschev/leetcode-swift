9bf9c641f94678a61b2a948bc19640d53332648d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:40:07 +0300

    81. Search in Rotated Sorted Array II

--- 0081. Search in Rotated Sorted Array II.swift
+++ 0081. Search in Rotated Sorted Array II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 81. Search in Rotated Sorted Array II
     // There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
     // Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
