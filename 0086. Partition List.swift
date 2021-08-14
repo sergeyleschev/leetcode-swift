41535a1ec53c233b4e4b63ef8e2e64572151db05
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:40:53 +0300

    86. Partition List

--- 0086. Partition List.swift
+++ 0086. Partition List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 86. Partition List
     // Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
     // You should preserve the original relative order of the nodes in each of the two partitions.
