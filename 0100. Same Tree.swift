eb3e46ec9aa437edfad2454b3a28b3015e799e47
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:43:35 +0300

    100. Same Tree

--- 0100. Same Tree.swift
+++ 0100. Same Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 100. Same Tree
     // Given the roots of two binary trees p and q, write a function to check if they are the same or not.
     // Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
