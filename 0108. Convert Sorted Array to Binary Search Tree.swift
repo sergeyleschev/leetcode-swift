53159ab9f20374aa7b16388ea8f3949d9927c3d3
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:44:49 +0300

    108. Convert Sorted Array to Binary Search Tree

--- 0108. Convert Sorted Array to Binary Search Tree.swift
+++ 0108. Convert Sorted Array to Binary Search Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 108. Convert Sorted Array to Binary Search Tree
     // Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
     // A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
