be8cc86a84ec171353292a0d5eddcaa193b24f56
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:41:30 +0300

    90. Subsets II

--- 0090. Subsets II.swift
+++ 0090. Subsets II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 90. Subsets II
     // Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
     // The solution set must not contain duplicate subsets. Return the solution in any order.
