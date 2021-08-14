dd32f67b0422633c3df875a8764789fc823ae891
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:39:14 +0300

    75. Sort Colors

--- 0075. Sort Colors.swift
+++ 0075. Sort Colors.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 75. Sort Colors
     // Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
     // We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
