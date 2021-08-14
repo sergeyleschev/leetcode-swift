ff9a8b8335674321ad3f3e46c05f28e709bcc1f6
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:43:02 +0300

    96. Unique Binary Search Trees

--- 0096. Unique Binary Search Trees.swift
+++ 0096. Unique Binary Search Trees.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 96. Unique Binary Search Trees
     // Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
 
