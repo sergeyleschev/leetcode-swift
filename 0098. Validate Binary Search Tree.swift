8734627005d2eb9a77761ce1570a1b00490f9caa
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:43:19 +0300

    98. Validate Binary Search Tree

--- 0098. Validate Binary Search Tree.swift
+++ 0098. Validate Binary Search Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 98. Validate Binary Search Tree
     // Given the root of a binary tree, determine if it is a valid binary search tree (BST).
 
