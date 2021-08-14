821291a3d5954734fd309d89817384f8ab382aa1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:39:50 +0300

    79. Word Search

--- 0079. Word Search.swift
+++ 0079. Word Search.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 79. Word Search
     // Given an m x n grid of characters board and a string word, return true if word exists in the grid.
     // The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
