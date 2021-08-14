f60580fda6b4dd5253fad5588244869a075863e2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:44:03 +0300

    103. Binary Tree Zigzag Level Order Traversal

--- 0103. Binary Tree Zigzag Level Order Traversal.swift
+++ 0103. Binary Tree Zigzag Level Order Traversal.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 103. Binary Tree Zigzag Level Order Traversal
     // Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
 
