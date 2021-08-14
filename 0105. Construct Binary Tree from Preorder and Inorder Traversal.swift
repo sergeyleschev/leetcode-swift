3b6befe4d6723658f82125bab9284940b29b9de2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:44:20 +0300

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- 0105. Construct Binary Tree from Preorder and Inorder Traversal.swift
+++ 0105. Construct Binary Tree from Preorder and Inorder Traversal.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 105. Construct Binary Tree from Preorder and Inorder Traversal
     // Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
 
