6af007cdbb405b86b8c962b223771b8592eb50c0
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-14 08:41:48 +0300

    92. Reverse Linked List II

--- 0092. Reverse Linked List II.swift
+++ 0092. Reverse Linked List II.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 92. Reverse Linked List II
     // Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 
