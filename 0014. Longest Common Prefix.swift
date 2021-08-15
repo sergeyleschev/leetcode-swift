ae9d91521a52ee201243469c1013a1acc0e3c7a2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-15 10:21:10 +0300

    14. Longest Common Prefix

--- 0014. Longest Common Prefix.swift
+++ 0014. Longest Common Prefix.swift
@@ -22,7 +22,6 @@ class Solution {
 
     func longestCommonPrefix(_ strs: [String]) -> String {
         guard strs.count > 0 else { return "" }
-        
         let chars_array = strs.map({ Array($0) })
         var string = ""
         var i = 0
@@ -35,9 +34,7 @@ class Solution {
                 } else {
                     if c == nil {
                         c = chars[i]
-                    } else if chars[i] != c! {
-                        return string
-                    }
+                    } else if chars[i] != c! { return string }
                 }  
             }
             string += String(c!)
