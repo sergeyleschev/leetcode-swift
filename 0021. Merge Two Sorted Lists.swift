25ad6d0e25e49e659cfbda7a853c70c6007ede46
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-15 10:23:11 +0300

    21. Merge Two Sorted Lists

--- 0021. Merge Two Sorted Lists.swift
+++ 0021. Merge Two Sorted Lists.swift
@@ -36,13 +36,9 @@ class Solution {
 		if l1 == nil { return l2 }
 		if l2 == nil { return l1 }
         
-		if l1!.val < l2!.val {
-			l1!.next = mergeTwoLists(l1!.next, l2)
-			return l1
-		} else {
-			l2!.next = mergeTwoLists(l2!.next, l1)
-			return l2
-		}
+		if l1!.val < l2!.val 
+			 { l1!.next = mergeTwoLists(l1!.next, l2); return l1 } 
+		else { l2!.next = mergeTwoLists(l2!.next, l1); return l2 }
     }
 
 }
