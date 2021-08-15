332a34065de80f65ba845e6ea981be326716eebf
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-15 10:18:53 +0300

    4. Median of Two Sorted Arrays

--- 0004. Median of Two Sorted Arrays.swift
+++ 0004. Median of Two Sorted Arrays.swift
@@ -39,35 +39,35 @@ class Solution {
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
         let total = nums1.count + nums2.count
         var index = 0
-        var index_1 = 0
-        var index_2 = 0
-        var merged_array: [Int] = []
+        var index1 = 0
+        var index2 = 0
+        var mergedArray: [Int] = []
         var res: Double = 0
 
         while total > 0 && index <= total / 2 {
-            if index_1 < nums1.count && index_2 < nums2.count {
-                if nums1[index_1] < nums2[index_2] {
-                    merged_array.append(nums1[index_1])
-                    index_1 += 1
+            if index1 < nums1.count && index2 < nums2.count {
+                if nums1[index1] < nums2[index2] {
+                    mergedArray.append(nums1[index1])
+                    index1 += 1
                 } else {
-                    merged_array.append(nums2[index_2])
-                    index_2 += 1
+                    mergedArray.append(nums2[index2])
+                    index2 += 1
                 }
-            } else if index_1 < nums1.count && index_2 >= nums2.count {
-                merged_array.append(nums1[index_1])
-                index_1 += 1
+            } else if index1 < nums1.count && index2 >= nums2.count {
+                mergedArray.append(nums1[index1])
+                index1 += 1
             } else {
-                merged_array.append(nums2[index_2])
-                index_2 += 1
+                mergedArray.append(nums2[index2])
+                index2 += 1
             }
             index += 1
         }
 
         if total > 0 {
             if total % 2 == 1 {
-                res = Double(merged_array[total / 2])
+                res = Double(mergedArray[total / 2])
             } else {
-                res = (Double(merged_array[total / 2]) + Double(merged_array[total / 2 - 1])) / 2
+                res = (Double(mergedArray[total / 2]) + Double(mergedArray[total / 2 - 1])) / 2
             }
         }
 
