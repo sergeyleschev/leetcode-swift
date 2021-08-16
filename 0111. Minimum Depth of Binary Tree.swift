ecdcec9354acb99a80dc614dfcd3ce444bbd44df
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-16 08:08:32 +0300

    111. Minimum Depth of Binary Tree

--- 0111. Minimum Depth of Binary Tree.swift
+++ 0111. Minimum Depth of Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 111. Minimum Depth of Binary Tree
     // Given a binary tree, find its minimum depth.
     // The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
