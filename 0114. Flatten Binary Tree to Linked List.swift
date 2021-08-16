b63ba333cc2bc516ae680caad79fc2cb836c9a2b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-16 08:09:34 +0300

    114. Flatten Binary Tree to Linked List

--- 0114. Flatten Binary Tree to Linked List.swift
+++ 0114. Flatten Binary Tree to Linked List.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 114. Flatten Binary Tree to Linked List
     // Given the root of a binary tree, flatten the tree into a "linked list":
     // The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
