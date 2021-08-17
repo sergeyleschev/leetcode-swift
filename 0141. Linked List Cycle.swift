0043fa2bc5df1b780d647a0290fdf615319edc58
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 21:31:44 +0300

    141. Linked List Cycle

--- 0141. Linked List Cycle.swift
+++ 0141. Linked List Cycle.swift
@@ -12,6 +12,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 141. Linked List Cycle
     // Given head, the head of a linked list, determine if the linked list has a cycle in it.
     // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
