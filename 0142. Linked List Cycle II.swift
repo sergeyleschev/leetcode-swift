21c04f2016b7afb4301a6a9b6d042302dcbd4328
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 21:31:54 +0300

    142. Linked List Cycle II

--- 0142. Linked List Cycle II.swift
+++ 0142. Linked List Cycle II.swift
@@ -12,6 +12,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 142. Linked List Cycle II
     // Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
     // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
