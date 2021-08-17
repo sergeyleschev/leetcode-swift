6ca2a719f9aa94e0b8e1e1909904166bdef67858
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:03:57 +0300

    132. Palindrome Partitioning II

--- 0132. Palindrome Partitioning II.swift
+++ 0132. Palindrome Partitioning II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 132. Palindrome Partitioning II
 	// Given a string s, partition s such that every substring of the partition is a palindrome.
 	// Return the minimum cuts needed for a palindrome partitioning of s.
