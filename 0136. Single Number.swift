12200cb02d644878d9b1f0bb5623448ef7ee6920
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:05:11 +0300

    136. Single Number

--- 0136. Single Number.swift
+++ 0136. Single Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 136. Single Number
     // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
     // You must implement a solution with a linear runtime complexity and use only constant extra space.
