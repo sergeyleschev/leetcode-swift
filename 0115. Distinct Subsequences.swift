be1a5d2cf02c0a0630aa3b2e502a5fcf180fe821
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 07:57:28 +0300

    115. Distinct Subsequences

--- 0115. Distinct Subsequences.swift
+++ 0115. Distinct Subsequences.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 115. Distinct Subsequences
     // Given two strings s and t, return the number of distinct subsequences of s which equals t.
     // A string's subsequence is a new string formed from the original string by deleting some (can be none) of the characters without disturbing the remaining characters' relative positions. (i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
