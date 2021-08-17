a0b867b0ee5fb8aee9b77b9d5d91f0c3ea7dc3c9
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:04:27 +0300

    134. Gas Station

--- 0134. Gas Station.swift
+++ 0134. Gas Station.swift
@@ -1,4 +1,6 @@
 class Solution {
+
+    // Solution @ Sergey Leschev, Belarusian State University
     
     // 134. Gas Station
     // There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
