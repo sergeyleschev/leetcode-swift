88acc873d3cbd3dfdde79dc23aaed919a7fb0c7e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:02:09 +0300

    123. Best Time to Buy and Sell Stock III

--- 0123. Best Time to Buy and Sell Stock III.swift
+++ 0123. Best Time to Buy and Sell Stock III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 123. Best Time to Buy and Sell Stock III
     // You are given an array prices where prices[i] is the price of a given stock on the ith day.
     // Find the maximum profit you can achieve. You may complete at most two transactions.
