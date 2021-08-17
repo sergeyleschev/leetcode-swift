ad34a9d16f0480ccf3b292b888e8c2ac6f59e569
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:03:14 +0300

    129. Sum Root to Leaf Numbers

--- 0129. Sum Root to Leaf Numbers.swift
+++ 0129. Sum Root to Leaf Numbers.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 129. Sum Root to Leaf Numbers
     // You are given the root of a binary tree containing digits from 0 to 9 only.
     // Each root-to-leaf path in the tree represents a number.
