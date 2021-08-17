b7336139fb415e0928d1eea2eb1b88e2810e4fd6
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:01:47 +0300

    121. Best Time to Buy and Sell Stock

--- 0121. Best Time to Buy and Sell Stock.swift
+++ 0121. Best Time to Buy and Sell Stock.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State Universitys
+
     // 121. Best Time to Buy and Sell Stock
     // You are given an array prices where prices[i] is the price of a given stock on the ith day.
     // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
