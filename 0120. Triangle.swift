42fabefffe63e4ac45a3976792803cffba67ef42
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:01:37 +0300

    120. Triangle

--- 0120. Triangle.swift
+++ 0120. Triangle.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 120. Triangle
 	// Given a triangle array, return the minimum path sum from top to bottom.
 	// For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
