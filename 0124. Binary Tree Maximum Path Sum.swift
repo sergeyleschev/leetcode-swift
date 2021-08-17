0be3312079d16fc50008188c2d6fc4bd2fa9fb3f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:02:19 +0300

    124. Binary Tree Maximum Path Sum

--- 0124. Binary Tree Maximum Path Sum.swift
+++ 0124. Binary Tree Maximum Path Sum.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 124. Binary Tree Maximum Path Sum
     // A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
     // The path sum of a path is the sum of the node's values in the path.
