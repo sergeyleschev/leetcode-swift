4e70a682844020fced3403cad90020b7d0ec32ba
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 21:33:52 +0300

    145. Binary Tree Postorder Traversal

--- 0145. Binary Tree Postorder Traversal.swift
+++ 0145. Binary Tree Postorder Traversal.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State Universitys
+
     // 145. Binary Tree Postorder Traversal
     // Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
