69dea7641cbe1b790089de297d76cd70a4659bc1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:03:46 +0300

    131. Palindrome Partitioning

--- 0131. Palindrome Partitioning.swift
+++ 0131. Palindrome Partitioning.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 131. Palindrome Partitioning
     // Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
     // A palindrome string is a string that reads the same backward as forward.
