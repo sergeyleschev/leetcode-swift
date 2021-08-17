b81da5b4acd883bee9e148297cb4da44602821b5
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 08:00:15 +0300

    118. Pascal's Triangle

--- 0118. Pascal's Triangle.swift
+++ 0118. Pascal's Triangle.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 118. Pascal's Triangle
     // Given an integer numRows, return the first numRows of Pascal's triangle.
     // In Pascal's triangle, each number is the sum of the two numbers directly above it as shown.
