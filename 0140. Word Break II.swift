a9b1c8ae481e23bdcd598997222b700987a00810
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-17 21:31:34 +0300

    140. Word Break II

--- 0140. Word Break II.swift
+++ 0140. Word Break II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 140. Word Break II
     // Given a string s and a dictionary of strings wordDict, add spaces in s to construct a sentence where each word is a valid dictionary word. Return all such possible sentences in any order.
     // Note that the same word in the dictionary may be reused multiple times in the segmentation.
