9d30d3fcc09f014bf5205b25ba341b4400cb7b29
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:04:04 +0300

    160. Intersection of Two Linked Lists

--- 0160. Intersection of Two Linked Lists.swift
+++ 0160. Intersection of Two Linked Lists.swift
@@ -12,6 +12,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 160. Intersection of Two Linked Lists
     // Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
     // For example, the following two linked lists begin to intersect at node c1:
