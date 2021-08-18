a5f89300189c84a5fa98449beb7ae64d5724f2fe
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:05:41 +0300

    169. Majority Element

--- 0169. Majority Element.swift
+++ 0169. Majority Element.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 169. Majority Element
     // Given an array nums of size n, return the majority element.
     // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
