6f376a16be8ba5c44efd0142c15187c224a77462
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 23:04:13 +0300

    183. Customers Who Never Order

--- 0183. Customers Who Never Order.sql
+++ 0183. Customers Who Never Order.sql
@@ -1,3 +1,5 @@
+-- Solution @ Sergey Leschev, Belarusian State University
+
 -- 183. Customers Who Never Order
 -- Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
 -- Table: Customers.
