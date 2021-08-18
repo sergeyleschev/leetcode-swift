db62307f91519f1203ea4b091d3b4f2df8c851a3
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:04:50 +0300

    166. Fraction to Recurring Decimal

--- 0166. Fraction to Recurring Decimal.swift
+++ 0166. Fraction to Recurring Decimal.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 166. Fraction to Recurring Decimal
     // Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
     // If the fractional part is repeating, enclose the repeating part in parentheses.
