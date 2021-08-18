5efe217825683a0b8418627cf0ce02f623eeaf56
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 22:12:48 +0300

    174. Dungeon Game

--- 0174. Dungeon Game.swift
+++ 0174. Dungeon Game.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 174. Dungeon Game
     // The demons had captured the princess and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of m x n rooms laid out in a 2D grid. Our valiant knight was initially positioned in the top-left room and must fight his way through dungeon to rescue the princess.
     // The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
