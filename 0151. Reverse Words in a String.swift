391e72444ce046c9df4a755564696445ffd9beae
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:03:15 +0300

    151. Reverse Words in a String

--- 0151. Reverse Words in a String.swift
+++ 0151. Reverse Words in a String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 151. Reverse Words in a String
     // Given an input string s, reverse the order of the words.
     // A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
