c76814ba2203f28077736067e63202f2ed136e08
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 23:03:25 +0300

    179. Largest Number

--- 0179. Largest Number.swift
+++ 0179. Largest Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 179. Largest Number
     // Given a list of non-negative integers nums, arrange them such that they form the largest number.
     // Note: The result may be very large, so you need to return a string instead of an integer.
