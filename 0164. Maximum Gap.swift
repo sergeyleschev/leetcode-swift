2bc8de4fefdc9efa90ed7f1faa687807f8159df4
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:04:32 +0300

    164. Maximum Gap

--- 0164. Maximum Gap.swift
+++ 0164. Maximum Gap.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 164. Maximum Gap
     // Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
     // You must write an algorithm that runs in linear time and uses linear extra space.
