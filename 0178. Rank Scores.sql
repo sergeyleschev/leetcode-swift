1157cf898ddc05b8498d6ad8d30236ea5cd46f34
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 22:13:41 +0300

    178. Rank Scores

--- 0178. Rank Scores.sql
+++ 0178. Rank Scores.sql
@@ -1,3 +1,5 @@
+-- Solution @ Sergey Leschev, Belarusian State University
+
 -- 178. Rank Scores
 -- Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
 -- +----+-------+
