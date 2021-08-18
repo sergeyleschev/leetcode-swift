6374fd8ffdcf784b421a43f7d694b4579be794a7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:02:36 +0300

    147. Insertion Sort List

--- 0147. Insertion Sort List.swift
+++ 0147. Insertion Sort List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 147. Insertion Sort List
     // Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
 
