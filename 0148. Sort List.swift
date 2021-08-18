160d9637852cbd1e70f863049f41e700b7722aa6
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:02:45 +0300

    148. Sort List

--- 0148. Sort List.swift
+++ 0148. Sort List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 148. Sort List
     // Given the head of a linked list, return the list after sorting it in ascending order.
     // Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
