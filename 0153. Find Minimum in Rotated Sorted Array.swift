766e5e80272f4dd0fda9a1f5e88acc763cd92de1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-18 08:03:35 +0300

    153. Find Minimum in Rotated Sorted Array

--- 0153. Find Minimum in Rotated Sorted Array.swift
+++ 0153. Find Minimum in Rotated Sorted Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 153. Find Minimum in Rotated Sorted Array
     // Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
     // [4,5,6,7,0,1,2] if it was rotated 4 times.
