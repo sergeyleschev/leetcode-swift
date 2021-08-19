3e8832f27d0619f68d410e110928d1485c77938b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:04:38 +0300

    205. Isomorphic Strings

--- 0205. Isomorphic Strings.swift
+++ 0205. Isomorphic Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+  // Solution @ Sergey Leschev, Belarusian State University
+
   // 205. Isomorphic Strings
   // Given two strings s and t, determine if they are isomorphic.
   // Two strings s and t are isomorphic if the characters in s can be replaced to get t.
