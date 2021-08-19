ebf81f1f6faa57f7c97e95799ecae0fb972c32fa
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:03:45 +0300

    200. Number of Islands

--- 0200. Number of Islands.swift
+++ 0200. Number of Islands.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 200. Number of Islands
     // Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
     // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
