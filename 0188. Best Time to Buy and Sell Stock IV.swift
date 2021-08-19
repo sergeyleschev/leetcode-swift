3bbcabf488f1975ce13fd62f501292ed835bf051
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 07:58:52 +0300

    188. Best Time to Buy and Sell Stock IV

--- 0188. Best Time to Buy and Sell Stock IV.swift
+++ 0188. Best Time to Buy and Sell Stock IV.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 188. Best Time to Buy and Sell Stock IV
     // You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
     // Find the maximum profit you can achieve. You may complete at most k transactions.
