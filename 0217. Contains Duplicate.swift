16bb8e990315c348587f81ae9f0e99927bb522fa
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 22:43:32 +0300

    217. Contains Duplicate

--- 0217. Contains Duplicate.swift
+++ 0217. Contains Duplicate.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 217. Contains Duplicate
     // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
