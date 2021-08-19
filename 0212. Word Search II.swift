610bc73000390e88d35ce217812665567bbbf36b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 22:42:34 +0300

    212. Word Search II

--- 0212. Word Search II.swift
+++ 0212. Word Search II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 212. Word Search II
     // Given an m x n board of characters and a list of strings words, return all words on the board.
     // Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
