5d58d66296bb385bdb06c013be0ce37c569682ce
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:05:09 +0300

    207. Course Schedule

--- 0207. Course Schedule.swift
+++ 0207. Course Schedule.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 207. Course Schedule
     // There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
     // For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
