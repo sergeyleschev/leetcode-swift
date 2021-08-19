8b2028cedbe58b63e4283fd88da0b989f2a9c944
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 22:43:22 +0300

    216. Combination Sum III

--- 0216. Combination Sum III.swift
+++ 0216. Combination Sum III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 216. Combination Sum III
     // Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
     // Only numbers 1 through 9 are used.
