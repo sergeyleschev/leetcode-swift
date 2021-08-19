1486b89e64f6aff59fd8f28fe449600f55502c8e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 22:42:43 +0300

    213. House Robber II

--- 0213. House Robber II.swift
+++ 0213. House Robber II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 213. House Robber II
     // You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
     // Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
