ed2496c1c355fb66bd536af276ecfe09ffd4a3f2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 22:42:51 +0300

    214. Shortest Palindrome

--- 0214. Shortest Palindrome.swift
+++ 0214. Shortest Palindrome.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 214. Shortest Palindrome
     // You are given a string s. You can convert s to a palindrome by adding characters in front of it.
     // Return the shortest palindrome you can find by performing this transformation.
