e1551a2ce5789dc1e26404bb54da5070f4ddab00
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 07:59:44 +0300

    193. Valid Phone Numbers

--- 0193. Valid Phone Numbers.sh
+++ 0193. Valid Phone Numbers.sh
@@ -1,3 +1,5 @@
+# Solution @ Sergey Leschev, Belarusian State University
+
 # 193. Valid Phone Numbers
 # Given a text file file.txt that contains a list of phone numbers (one per line), write a one-liner bash script to print all valid phone numbers.
 # You may assume that a valid phone number must appear in one of the following two formats: (xxx) xxx-xxxx or xxx-xxx-xxxx. (x means a digit)
