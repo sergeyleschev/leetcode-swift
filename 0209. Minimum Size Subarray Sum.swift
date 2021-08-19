659c7f8e2413ce5c2d11354f278f054ab40d7eb7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:05:29 +0300

    209. Minimum Size Subarray Sum

--- 0209. Minimum Size Subarray Sum.swift
+++ 0209. Minimum Size Subarray Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 209. Minimum Size Subarray Sum
     // Given an array of positive integers nums and a positive integer target, return the minimal length of a contiguous subarray [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
