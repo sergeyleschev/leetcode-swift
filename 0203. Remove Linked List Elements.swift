8d4dd22d54cf31942ef385226678bf954fbb5d34
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:04:21 +0300

    203. Remove Linked List Elements

--- 0203. Remove Linked List Elements.swift
+++ 0203. Remove Linked List Elements.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 203. Remove Linked List Elements
     // Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
 
