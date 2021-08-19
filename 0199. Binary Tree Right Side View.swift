f94eff98d3138319c5764708f5aaa3d01d111396
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:03:37 +0300

    199. Binary Tree Right Side View

--- 0199. Binary Tree Right Side View.swift
+++ 0199. Binary Tree Right Side View.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 199. Binary Tree Right Side View
     // Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
 
