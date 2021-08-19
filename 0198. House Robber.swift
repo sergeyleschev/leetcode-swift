4649911e02fa03f1cdd414e035a188da14f319da
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:03:29 +0300

    198. House Robber

--- 0198. House Robber.swift
+++ 0198. House Robber.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 198. House Robber
     // You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
     // Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
