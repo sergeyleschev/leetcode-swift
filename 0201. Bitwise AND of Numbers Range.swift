3f57d46ded97ede7267d52a745348e924f755a96
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 08:04:02 +0300

    201. Bitwise AND of Numbers Range

--- 0201. Bitwise AND of Numbers Range.swift
+++ 0201. Bitwise AND of Numbers Range.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+    
     // 201. Bitwise AND of Numbers Range
     // Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
 
