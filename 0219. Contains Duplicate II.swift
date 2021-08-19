10314f45a61fb73f43b3a1ae238cc67dccc5332c
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-19 22:43:58 +0300

    219. Contains Duplicate II

--- 0219. Contains Duplicate II.swift
+++ 0219. Contains Duplicate II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 219. Contains Duplicate II
     // Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
