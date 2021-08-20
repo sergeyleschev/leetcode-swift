d40720d452a3500f6a1781e3bab2cdcb36ee58de
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 20:17:37 +0300

    275. H-Index II

--- 0275. H-Index II.swift
+++ 0275. H-Index II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 275. H-Index II
     // Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper and citations is sorted in an ascending order, return compute the researcher's h-index.
     // According to the definition of h-index on Wikipedia: A scientist has an index h if h of their n papers have at least h citations each, and the other n − h papers have no more than h citations each.
