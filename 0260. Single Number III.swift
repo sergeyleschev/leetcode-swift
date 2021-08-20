5b61ae0534f7fa51c73998deaa78319d1ec4e188
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:06:09 +0300

    260. Single Number III

--- 0260. Single Number III.swift
+++ 0260. Single Number III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 260. Single Number III
     // Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
     // You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
