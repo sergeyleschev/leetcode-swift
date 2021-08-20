900d5193439cb0478493757410c91cce5640bea1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 07:59:05 +0300

    222. Count Complete Tree Nodes

--- 0222. Count Complete Tree Nodes.swift
+++ 0222. Count Complete Tree Nodes.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 222. Count Complete Tree Nodes
     // Given the root of a complete binary tree, return the number of the nodes in the tree.
     // According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
