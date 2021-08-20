953cd665d6fb37a4129d605ad999132872d1a588
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:03:42 +0300

    232. Implement Queue using Stacks

--- 0232. Implement Queue using Stacks.swift
+++ 0232. Implement Queue using Stacks.swift
@@ -1,5 +1,7 @@
 class MyQueue {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 232. Implement Queue using Stacks
     // Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
 
