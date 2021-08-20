8b66aa63302dba2eb017ce532712b3f4d2f0c856
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 07:59:48 +0300

    226. Invert Binary Tree

--- 0226. Invert Binary Tree.swift
+++ 0226. Invert Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 226. Invert Binary Tree
     // Given the root of a binary tree, invert the tree, and return its root.
 
