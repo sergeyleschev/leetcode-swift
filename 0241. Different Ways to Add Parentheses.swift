5bfe10a1f5942614b6065bbd002221d3e96e427f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:05:26 +0300

    241. Different Ways to Add Parentheses

--- 0241. Different Ways to Add Parentheses.swift
+++ 0241. Different Ways to Add Parentheses.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 241. Different Ways to Add Parentheses
     // Given a string expression of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. You may return the answer in any order.
 
