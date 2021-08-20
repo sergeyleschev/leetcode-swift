de10aac751d268b79de41831e34c66657e46ad78
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 20:16:34 +0300

    268. Missing Number

--- 0268. Missing Number.swift
+++ 0268. Missing Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 268. Missing Number
     // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
     // Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
