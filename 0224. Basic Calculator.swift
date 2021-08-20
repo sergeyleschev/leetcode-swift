0f4088226d2f18ec9712f61b5e63734e99f5cd23
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 07:59:24 +0300

    224. Basic Calculator

--- 0224. Basic Calculator.swift
+++ 0224. Basic Calculator.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 224. Basic Calculator
     // Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
     // Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
