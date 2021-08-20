65ffc44001348b3558e07b37021a31c7bc719ecd
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:04:24 +0300

    237. Delete Node in a Linked List

--- 0237. Delete Node in a Linked List.swift
+++ 0237. Delete Node in a Linked List.swift
@@ -12,6 +12,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 237. Delete Node in a Linked List
     // Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
     // It is guaranteed that the node to be deleted is not a tail node in the list.
