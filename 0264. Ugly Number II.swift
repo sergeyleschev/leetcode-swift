bb13e9ce8fcc991c8afd5e71b89dce957ee259cd
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 20:16:23 +0300

    264. Ugly Number II

--- 0264. Ugly Number II.swift
+++ 0264. Ugly Number II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 264. Ugly Number II
     // An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
     // Given an integer n, return the nth ugly number.
