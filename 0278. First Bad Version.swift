1bb2eb5f5b2cc2d27274a294903b4261a520fbc0
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 20:17:58 +0300

    278. First Bad Version

--- 0278. First Bad Version.swift
+++ 0278. First Bad Version.swift
@@ -5,6 +5,8 @@
 
 class Solution : VersionControl {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 278. First Bad Version
     // You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
     // Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
