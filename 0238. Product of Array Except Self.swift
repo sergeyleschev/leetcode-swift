073ccb2e07ba0c15c4e8adb6d2c65f772f28cb7b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:04:46 +0300

    238. Product of Array Except Self

--- 0238. Product of Array Except Self.swift
+++ 0238. Product of Array Except Self.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 238. Product of Array Except Self
     // Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
     // The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
