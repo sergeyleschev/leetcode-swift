70d3446a388c2e417584deaf9b6335feb3a5c899
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:04:58 +0300

    239. Sliding Window Maximum

--- 0239. Sliding Window Maximum.swift
+++ 0239. Sliding Window Maximum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 239. Sliding Window Maximum
     // You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
     
