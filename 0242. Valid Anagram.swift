9ea637155f219df8a34c73854282d38825974593
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:05:38 +0300

    242. Valid Anagram

--- 0242. Valid Anagram.swift
+++ 0242. Valid Anagram.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 242. Valid Anagram
     // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
