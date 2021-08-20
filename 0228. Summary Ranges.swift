66cd7fe318c2435d40689c8e6f6aa5dacd422153
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-20 08:00:06 +0300

    228. Summary Ranges

--- 0228. Summary Ranges.swift
+++ 0228. Summary Ranges.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 228. Summary Ranges
     // You are given a sorted unique integer array nums.
     // Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
