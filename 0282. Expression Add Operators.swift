2e19d718a54c042161da06fd8cca8c33f90a897c
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 08:45:40 +0300

    282. Expression Add Operators

--- 0282. Expression Add Operators.swift
+++ 0282. Expression Add Operators.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 282. Expression Add Operators
     // Given a string num that contains only digits and an integer target, return all possibilities to add the binary operators '+', '-', or '*' between the digits of num so that the resultant expression evaluates to the target value.
 
