c7392db64af65a65e0f293f203326c7740b508c6
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 08:45:32 +0300

    279. Perfect Squares

--- 0279. Perfect Squares.swift
+++ 0279. Perfect Squares.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+  // Solution @ Sergey Leschev, Belarusian State University
+
   // 279. Perfect Squares
   // Given an integer n, return the least number of perfect square numbers that sum to n.
   // A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
