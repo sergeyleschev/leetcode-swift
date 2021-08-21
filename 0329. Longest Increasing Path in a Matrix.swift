531b156be6b6535d3eb955ddb3bcc1126f46f3cd
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 20:29:13 +0300

    329. Longest Increasing Path in a Matrix

--- 0329. Longest Increasing Path in a Matrix.swift
+++ 0329. Longest Increasing Path in a Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 329. Longest Increasing Path in a Matrix
     // Given an m x n integers matrix, return the length of the longest increasing path in matrix.
     // From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
