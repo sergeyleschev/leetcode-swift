fa630377cd782f0dbee956abf2ac4bf7a00a1838
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 08:47:19 +0300

    289. Game of Life

--- 0289. Game of Life.swift
+++ 0289. Game of Life.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 289. Game of Life
     // According to Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
     // The board is made up of an m x n grid of cells, where each cell has an initial state: live (represented by a 1) or dead (represented by a 0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
