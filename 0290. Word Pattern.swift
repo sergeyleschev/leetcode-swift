4219dde25119507be880871e9db067bb24323200
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 08:47:29 +0300

    290. Word Pattern

--- 0290. Word Pattern.swift
+++ 0290. Word Pattern.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 290. Word Pattern
     // Given a pattern and a string s, find if s follows the same pattern.
     // Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
