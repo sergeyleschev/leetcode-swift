61c59c8cd97b6bb69767856b98adbdcf2e0a32de
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 20:27:19 +0300

    315. Count of Smaller Numbers After Self

--- 0315. Count of Smaller Numbers After Self.swift
+++ 0315. Count of Smaller Numbers After Self.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 315. Count of Smaller Numbers After Self
     // You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
 
