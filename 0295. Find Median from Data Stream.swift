4833ebae4324f8f467b3ea70db8b48c58e6244f8
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 08:54:30 +0300

    295. Find Median from Data Stream

--- 0295. Find Median from Data Stream.swift
+++ 0295. Find Median from Data Stream.swift
@@ -1,5 +1,7 @@
 class MedianFinder {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 295. Find Median from Data Stream
     // The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
     // For example, for arr = [2,3,4], the median is 3.
