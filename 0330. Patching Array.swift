670e064ce39ba6d893e201377e316b8555d79f61
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 20:29:42 +0300

    330. Patching Array

--- 0330. Patching Array.swift
+++ 0330. Patching Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 330. Patching Array
     // Given a sorted integer array nums and an integer n, add/patch elements to the array such that any number in the range [1, n] inclusive can be formed by the sum of some elements in the array.
     // Return the minimum number of patches required.
