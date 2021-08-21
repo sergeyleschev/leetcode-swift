5bbf8f35f441b8334041cabc141d82f84e17aecf
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 20:26:57 +0300

    312. Burst Balloons

--- 0312. Burst Balloons.swift
+++ 0312. Burst Balloons.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 312. Burst Balloons
     // You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
     // If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
