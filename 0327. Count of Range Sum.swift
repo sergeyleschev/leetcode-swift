b1c9b5aa3d62956b0024554e09b86f3b3b0b3920
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 20:28:36 +0300

    327. Count of Range Sum

--- 0327. Count of Range Sum.swift
+++ 0327. Count of Range Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 327. Count of Range Sum
     // Given an integer array nums and two integers lower and upper, return the number of range sums that lie in [lower, upper] inclusive.
     // Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j inclusive, where i <= j.
