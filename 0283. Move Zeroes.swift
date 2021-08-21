acc7f887d3f6b775ad07e476cf066cf53f8b5e17
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 08:46:11 +0300

    283. Move Zeroes

--- 0283. Move Zeroes.swift
+++ 0283. Move Zeroes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 283. Move Zeroes
     // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
     // Note that you must do this in-place without making a copy of the array.
