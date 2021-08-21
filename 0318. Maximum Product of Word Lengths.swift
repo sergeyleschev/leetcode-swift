fd98c89c74306bc80bb88474f5ea34c20d01e9d5
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-21 20:27:29 +0300

    318. Maximum Product of Word Lengths

--- 0318. Maximum Product of Word Lengths.swift
+++ 0318. Maximum Product of Word Lengths.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 318. Maximum Product of Word Lengths
     // Given a string array words, return the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. If no such two words exist, return 0.
 
