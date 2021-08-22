a8513dfc1c46634699c189b84ee2e5fa6732d7b8
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-22 10:12:45 +0300

    338. Counting Bits

--- 0338. Counting Bits.swift
+++ 0338. Counting Bits.swift
@@ -1,4 +1,6 @@
 class Solution {
+
+    // Solution @ Sergey Leschev, Belarusian State University
     
     // 338. Counting Bits
     // Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
@@ -34,7 +36,6 @@ class Solution {
         var ans = Array(repeating: 0, count: num + 1)
 
         for i in 1...num { ans[i] = ans[i & (i - 1)] + 1 }
-        
         return ans
     }
     
