92031720810afe80ecba1803f6270517030d0ca7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-22 10:14:00 +0300

    344. Reverse String

--- 0344. Reverse String.swift
+++ 0344. Reverse String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 344. Reverse String
     // Write a function that reverses a string. The input string is given as an array of characters s.
 
