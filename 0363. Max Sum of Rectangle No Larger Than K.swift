129cc7c2fdb833b13b2232ec0f9218ad99a7cddd
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-22 19:43:07 +0300

    363. Max Sum of Rectangle No Larger Than K

--- 0363. Max Sum of Rectangle No Larger Than K.swift
+++ 0363. Max Sum of Rectangle No Larger Than K.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
 	// 363. Max Sum of Rectangle No Larger Than K
 	// Given an m x n matrix matrix and an integer k, return the max sum of a rectangle in the matrix such that its sum is no larger than k.
 	// It is guaranteed that there will be a rectangle with a sum no larger than k.
