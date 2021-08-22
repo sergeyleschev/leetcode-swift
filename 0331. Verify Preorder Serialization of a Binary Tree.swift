5704038365dfdb13f4937db971ac88d07e604441
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-22 10:11:51 +0300

    331. Verify Preorder Serialization of a Binary Tree

--- 0331. Verify Preorder Serialization of a Binary Tree.swift
+++ 0331. Verify Preorder Serialization of a Binary Tree.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 331. Verify Preorder Serialization of a Binary Tree
     // One way to serialize a binary tree is to use preorder traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as '#'.
     // For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where '#' represents a null node.
