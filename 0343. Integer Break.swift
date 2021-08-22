40bc172c28c8f454da8424652bf6c19254a40150
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-22 10:13:51 +0300

    343. Integer Break

--- 0343. Integer Break.swift
+++ 0343. Integer Break.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 343. Integer Break
     // Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
     // Return the maximum product you can get.
