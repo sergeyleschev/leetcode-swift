006f021583dfc8e1e5a84badfa6b8cedd6403f40
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-22 10:12:00 +0300

    332. Reconstruct Itinerary

--- 0332. Reconstruct Itinerary.swift
+++ 0332. Reconstruct Itinerary.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 332. Reconstruct Itinerary
     // You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
     // All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
