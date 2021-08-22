15d7374f662517d4f8e253daf327b3cb45c395ce
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-22 10:14:51 +0300

    350. Intersection of Two Arrays II

--- 0350. Intersection of Two Arrays II.swift
+++ 0350. Intersection of Two Arrays II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 350. Intersection of Two Arrays II
     // Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
