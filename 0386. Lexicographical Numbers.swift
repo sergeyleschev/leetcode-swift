db26078b727a3e8eb92865528f55324f93fe45bf
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:55:28 +0300

    386. Lexicographical Numbers

--- 0386. Lexicographical Numbers.swift
+++ 0386. Lexicographical Numbers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 386. Lexicographical Numbers
     // Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
     // You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
