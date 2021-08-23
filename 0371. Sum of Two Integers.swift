51e2aba22cb1da73d34f322efd71f44a7d9d8d58
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:52:38 +0300

    371. Sum of Two Integers

--- 0371. Sum of Two Integers.swift
+++ 0371. Sum of Two Integers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 371. Sum of Two Integers
     // Given two integers a and b, return the sum of the two integers without using the operators + and -.
 
