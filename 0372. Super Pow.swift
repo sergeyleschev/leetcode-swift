ca233467feef6b2756b05be9412ae6bc7590cf08
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:52:49 +0300

    372. Super Pow

--- 0372. Super Pow.swift
+++ 0372. Super Pow.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 372. Super Pow
     // Your task is to calculate ab mod 1337 where a is a positive integer and b is an extremely large positive integer given in the form of an array.
 
