dffe67e87ed4bb2ef81594776b2a2939e0d9b10f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:53:00 +0300

    373. Find K Pairs with Smallest Sums

--- 0373. Find K Pairs with Smallest Sums.swift
+++ 0373. Find K Pairs with Smallest Sums.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 373. Find K Pairs with Smallest Sums
     // You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
     // Define a pair (u, v) which consists of one element from the first array and one element from the second array.
