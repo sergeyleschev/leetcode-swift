cef4a128eac24270edc93070f0a6856f527b0629
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:53:10 +0300

    374. Guess Number Higher or Lower

--- 0374. Guess Number Higher or Lower.swift
+++ 0374. Guess Number Higher or Lower.swift
@@ -9,6 +9,8 @@
 
 class Solution : GuessGame {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 374. Guess Number Higher or Lower
     // We are playing the Guess Game. The game is as follows:
     // I pick a number from 1 to n. You have to guess which number I picked.
