9ae6096700e0e998bd071fb0383c883b57368a5b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:55:45 +0300

    387. First Unique Character in a String

--- 0387. First Unique Character in a String.swift
+++ 0387. First Unique Character in a String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 387. First Unique Character in a String
     // Given a string s, return the first non-repeating character in it and return its index. If it does not exist, return -1.
 
