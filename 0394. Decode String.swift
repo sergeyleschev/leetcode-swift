1f0de8b3a76d88b72f34de6a4276953b1ad5d1df
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 08:03:39 +0300

    394. Decode String

--- 0394. Decode String.swift
+++ 0394. Decode String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 394. Decode String
     // Given an encoded string, return its decoded string.
     // The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
