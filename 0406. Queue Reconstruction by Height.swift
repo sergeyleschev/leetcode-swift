37e09c08144cd32292c3c8d26b37445a51090315
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 21:35:59 +0300

    406. Queue Reconstruction by Height

--- 0406. Queue Reconstruction by Height.swift
+++ 0406. Queue Reconstruction by Height.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 406. Queue Reconstruction by Height
     // You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
     // Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
