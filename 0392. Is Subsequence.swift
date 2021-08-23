edd9051768c3f21d63401623c51c7b92bfdb8612
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 08:03:02 +0300

    392. Is Subsequence

--- 0392. Is Subsequence.swift
+++ 0392. Is Subsequence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 392. Is Subsequence
     // Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
     // A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
