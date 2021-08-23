9c9c8deaf5126f6fddbfa3eb28865298161a2831
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 21:35:50 +0300

    405. Convert a Number to Hexadecimal

--- 0405. Convert a Number to Hexadecimal.swift
+++ 0405. Convert a Number to Hexadecimal.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 405. Convert a Number to Hexadecimal
     // Given an integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
     // All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
