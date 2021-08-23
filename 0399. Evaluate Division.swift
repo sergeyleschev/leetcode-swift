38d6d7294f59b5ffa25c6399877cf9279953ee37
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 08:04:17 +0300

    399. Evaluate Division

--- 0399. Evaluate Division.swift
+++ 0399. Evaluate Division.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 399. Evaluate Division
     // You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
     // You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
