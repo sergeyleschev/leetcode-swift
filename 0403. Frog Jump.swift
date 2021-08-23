81eed222a7d8d3c2a569fe7381c7adfb62c88abb
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 21:35:18 +0300

    403. Frog Jump

--- 0403. Frog Jump.swift
+++ 0403. Frog Jump.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 403. Frog Jump
     // A frog is crossing a river. The river is divided into some number of units, and at each unit, there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
     // Given a list of stones' positions (in units) in sorted ascending order, determine if the frog can cross the river by landing on the last stone. Initially, the frog is on the first stone and assumes the first jump must be 1 unit.
