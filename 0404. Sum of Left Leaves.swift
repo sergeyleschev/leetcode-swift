a96e9bcdaafc30c435e98dac4960656fed9bfbb1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 21:35:27 +0300

    404. Sum of Left Leaves

--- 0404. Sum of Left Leaves.swift
+++ 0404. Sum of Left Leaves.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 404. Sum of Left Leaves
     // Given the root of a binary tree, return the sum of all left leaves.
 
