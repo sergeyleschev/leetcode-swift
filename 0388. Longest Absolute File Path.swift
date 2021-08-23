e6f4852d6587b0085f0ce6d8a5eb1bdf1391a2e6
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:58:45 +0300

    388. Longest Absolute File Path

--- 0388. Longest Absolute File Path.swift
+++ 0388. Longest Absolute File Path.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 388. Longest Absolute File Path
     // Suppose we have a file system that stores both files and directories. An example of one system is represented in the following picture:
     // Here, we have dir as the only directory in the root. dir contains two subdirectories, subdir1 and subdir2. subdir1 contains a file file1.ext and subdirectory subsubdir1. subdir2 contains a subdirectory subsubdir2, which contains a file file2.ext.
