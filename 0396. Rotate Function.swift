a8eadfe371311887b5292ffad2b247486641c086
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 08:03:57 +0300

    396. Rotate Function

--- 0396. Rotate Function.swift
+++ 0396. Rotate Function.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 396. Rotate Function
     // You are given an integer array nums of length n.
     // Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
