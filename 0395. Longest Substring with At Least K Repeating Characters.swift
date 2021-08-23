d729d23a8e0a6e1ca341861da8e5c630ad03ba8c
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 08:03:49 +0300

    395. Longest Substring with At Least K Repeating Characters

--- 0395. Longest Substring with At Least K Repeating Characters.swift
+++ 0395. Longest Substring with At Least K Repeating Characters.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 395. Longest Substring with At Least K Repeating Characters
     // Given a string s and an integer k, return the length of the longest substring of s such that the frequency of each character in this substring is greater than or equal to k.
 
