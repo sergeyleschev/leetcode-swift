149c7e4e20fba6ec1ddf7f85c388a89480f3b491
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:54:10 +0300

    378. Kth Smallest Element in a Sorted Matrix

--- 0378. Kth Smallest Element in a Sorted Matrix.swift
+++ 0378. Kth Smallest Element in a Sorted Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 378. Kth Smallest Element in a Sorted Matrix
     // Given an n x n matrix where each of the rows and columns are sorted in ascending order, return the kth smallest element in the matrix.
     // Note that it is the kth smallest element in the sorted order, not the kth distinct element.
