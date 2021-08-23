863868b322f67eea8ef953a3005d004e1efca3a4
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 21:35:08 +0300

    402. Remove K Digits

--- 0402. Remove K Digits.swift
+++ 0402. Remove K Digits.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 402. Remove K Digits
     // Given string num representing a non-negative integer num, and an integer k, return the smallest possible integer after removing k digits from num.
 
