0fc7b6baa0f0774fd39fb92e2868365bdf29b7ac
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-23 07:52:28 +0300

    368. Largest Divisible Subset

--- 0368. Largest Divisible Subset.swift
+++ 0368. Largest Divisible Subset.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 368. Largest Divisible Subset
     // Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
     // answer[i] % answer[j] == 0, or
