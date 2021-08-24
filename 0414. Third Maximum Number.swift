b7317ca6feb91db35bde04745bf1f730105a0f3d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:55:34 +0300

    414. Third Maximum Number

--- 0414. Third Maximum Number.swift
+++ 0414. Third Maximum Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 414. Third Maximum Number
     // Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
 
