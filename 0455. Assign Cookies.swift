2ba9c1ee63f46c5e2c768c9ac1032e3198f8ca91
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 21:33:19 +0300

    455. Assign Cookies

--- 0455. Assign Cookies.swift
+++ 0455. Assign Cookies.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 455. Assign Cookies
     // Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
     // Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize the number of your content children and output the maximum number.
