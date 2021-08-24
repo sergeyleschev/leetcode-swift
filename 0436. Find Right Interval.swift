5e9004cd165868e897da64ac0ede9671d9a45249
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:58:55 +0300

    436. Find Right Interval

--- 0436. Find Right Interval.swift
+++ 0436. Find Right Interval.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 436. Find Right Interval
     // You are given an array of intervals, where intervals[i] = [starti, endi] and each starti is unique.
     // The right interval for an interval i is an interval j such that startj >= endi and startj is minimized.
