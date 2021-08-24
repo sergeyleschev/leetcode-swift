f547b351e0fe5c62c73f82e877e234c39e490435
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:58:01 +0300

    430. Flatten a Multilevel Doubly Linked List

--- 0430. Flatten a Multilevel Doubly Linked List.swift
+++ 0430. Flatten a Multilevel Doubly Linked List.swift
@@ -16,6 +16,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 430. Flatten a Multilevel Doubly Linked List
     // You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, which may or may not point to a separate doubly linked list. These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in the example below.
     // Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the first level of the list.
