e9afc7621e580fe0c58c2707c7c14dac1581e914
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:57:33 +0300

    424. Longest Repeating Character Replacement

--- 0424. Longest Repeating Character Replacement.swift
+++ 0424. Longest Repeating Character Replacement.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 424. Longest Repeating Character Replacement
     // You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
     // Return the length of the longest substring containing the same letter you can get after performing the above operations.
