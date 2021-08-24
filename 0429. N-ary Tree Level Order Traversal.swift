0b0b0bb52d89e86c29171c6258ae1eafa59de141
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:57:52 +0300

    429. N-ary Tree Level Order Traversal

--- 0429. N-ary Tree Level Order Traversal.swift
+++ 0429. N-ary Tree Level Order Traversal.swift
@@ -12,6 +12,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 429. N-ary Tree Level Order Traversal
     // Given an n-ary tree, return the level order traversal of its nodes' values.
     // Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
