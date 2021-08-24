6faf98f73de11bca459ece096b6b5f4bbaf9c92f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:55:06 +0300

    410. Split Array Largest Sum

--- 0410. Split Array Largest Sum.swift
+++ 0410. Split Array Largest Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+  // Solution @ Sergey Leschev, Belarusian State University
+
   // 410. Split Array Largest Sum
   // Given an array nums which consists of non-negative integers and an integer m, you can split the array into m non-empty continuous subarrays.
   // Write an algorithm to minimize the largest sum among these m subarrays.
