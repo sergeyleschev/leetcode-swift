494308d9f48f41b92cbcb4231824e860a226a1c7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 21:31:35 +0300

    447. Number of Boomerangs

--- 0447. Number of Boomerangs.swift
+++ 0447. Number of Boomerangs.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 447. Number of Boomerangs
     // You are given n points in the plane that are all distinct, where points[i] = [xi, yi]. A boomerang is a tuple of points (i, j, k) such that the distance between i and j equals the distance between i and k (the order of the tuple matters).
     // Return the number of boomerangs.
