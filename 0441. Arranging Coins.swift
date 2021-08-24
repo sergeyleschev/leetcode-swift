ee54cfa167cb86465a50ca4c86be3e5714148ddc
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:59:43 +0300

    441. Arranging Coins

--- 0441. Arranging Coins.swift
+++ 0441. Arranging Coins.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 441. Arranging Coins
     // You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
     // Given the integer n, return the number of complete rows of the staircase you will build.
