eac2ccacf252f8372287ed269cad8fc0ee386414
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:54:57 +0300

    409. Longest Palindrome

--- 0409. Longest Palindrome.swift
+++ 0409. Longest Palindrome.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 409. Longest Palindrome
     // Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
     // Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
