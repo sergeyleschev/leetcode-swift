b7d1c7966fef1266025dff1a8e23a38fb491abd5
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 21:31:55 +0300

    449. Serialize and Deserialize BST

--- 0449. Serialize and Deserialize BST.swift
+++ 0449. Serialize and Deserialize BST.swift
@@ -14,6 +14,8 @@
 
 class Codec {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 449. Serialize and Deserialize BST
     // Serialization is converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
     // Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You need to ensure that a binary search tree can be serialized to a string, and this string can be deserialized to the original tree structure.
