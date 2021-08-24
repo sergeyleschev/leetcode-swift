446dea90b078b116011e262c6551b465cdfd7c48
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 08:01:34 +0300

    445. Add Two Numbers II

--- 0445. Add Two Numbers II.swift
+++ 0445. Add Two Numbers II.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 445. Add Two Numbers II
     // You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
     // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
