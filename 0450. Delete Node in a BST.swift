7faf7405cc12961b17aa3bd082df4b7850509aa6
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 21:32:04 +0300

    450. Delete Node in a BST

--- 0450. Delete Node in a BST.swift
+++ 0450. Delete Node in a BST.swift
@@ -16,6 +16,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 450. Delete Node in a BST
     // Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
     // Basically, the deletion can be divided into two stages:
