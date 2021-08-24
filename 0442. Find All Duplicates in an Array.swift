4f79f6f30782549d886713ff8eb0969d38de5d33
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 08:00:00 +0300

    442. Find All Duplicates in an Array

--- 0442. Find All Duplicates in an Array.swift
+++ 0442. Find All Duplicates in an Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 442. Find All Duplicates in an Array
     // Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
     // You must write an algorithm that runs in O(n) time and uses only constant extra space.
