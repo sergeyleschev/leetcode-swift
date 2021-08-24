ccb0265ea881b8273a72144f6d6bb2739e4a7d9f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:56:10 +0300

    419. Battleships in a Board

--- 0419. Battleships in a Board.swift
+++ 0419. Battleships in a Board.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 419. Battleships in a Board
     // Given an m x n matrix board where each cell is a battleship 'X' or empty '.', return the number of the battleships on board.
     // Battleships can only be placed horizontally or vertically on board. In other words, they can only be made of the shape 1 x k (1 row, k columns) or k x 1 (k rows, 1 column), where k can be of any size. At least one horizontal or vertical cell separates between two battleships (i.e., there are no adjacent battleships).
