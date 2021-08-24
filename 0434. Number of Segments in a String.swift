ca073c16fcfb4b852498c3aa3502a450822a56c9
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:58:33 +0300

    434. Number of Segments in a String

--- 0434. Number of Segments in a String.swift
+++ 0434. Number of Segments in a String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 434. Number of Segments in a String
     // You are given a string s, return the number of segments in the string. 
     // A segment is defined to be a contiguous sequence of non-space characters.
