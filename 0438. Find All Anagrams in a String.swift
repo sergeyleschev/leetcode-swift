4db55bd3cb122a9152ae5159989ba9b4332ef89d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:59:23 +0300

    438. Find All Anagrams in a String

--- 0438. Find All Anagrams in a String.swift
+++ 0438. Find All Anagrams in a String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 438. Find All Anagrams in a String
     // Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
 
