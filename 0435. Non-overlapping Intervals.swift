0daa6c70993bf69b08ae9160046095b05f5850cf
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:58:41 +0300

    435. Non-overlapping Intervals

--- 0435. Non-overlapping Intervals.swift
+++ 0435. Non-overlapping Intervals.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 435. Non-overlapping Intervals
     // Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
 
