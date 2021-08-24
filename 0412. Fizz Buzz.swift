95fbf27abaeb318263b66e8e159bed04f6d51a52
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:55:15 +0300

    412. Fizz Buzz

--- 0412. Fizz Buzz.swift
+++ 0412. Fizz Buzz.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 412. Fizz Buzz
 	// Given an integer n, return a string array answer (1-indexed) where:
 	// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
