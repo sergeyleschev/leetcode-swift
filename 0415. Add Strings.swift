63d4068054b5bd45cfca62c7a6980f2c769b602b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:55:43 +0300

    415. Add Strings

--- 0415. Add Strings.swift
+++ 0415. Add Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 415. Add Strings
     // Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
     // You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
