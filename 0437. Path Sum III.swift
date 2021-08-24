3488074ce144295ad379164cb61f0c9a2693c4a2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 07:59:04 +0300

    437. Path Sum III

--- 0437. Path Sum III.swift
+++ 0437. Path Sum III.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 437. Path Sum III
     // Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
     // The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
