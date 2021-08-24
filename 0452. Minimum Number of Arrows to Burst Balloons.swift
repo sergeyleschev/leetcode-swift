eeef5f2fb2763f157a5fcba1055447a2da28c6e7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-24 21:32:24 +0300

    452. Minimum Number of Arrows to Burst Balloons

--- 0452. Minimum Number of Arrows to Burst Balloons.swift
+++ 0452. Minimum Number of Arrows to Burst Balloons.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 452. Minimum Number of Arrows to Burst Balloons
     // There are some spherical balloons spread in two-dimensional space. For each balloon, provided input is the start and end coordinates of the horizontal diameter. Since it's horizontal, y-coordinates don't matter, and hence the x-coordinates of start and end of the diameter suffice. The start is always smaller than the end.
     // An arrow can be shot up exactly vertically from different points along the x-axis. A balloon with xstart and xend bursts by an arrow shot at x if xstart ≤ x ≤ xend. There is no limit to the number of arrows that can be shot. An arrow once shot keeps traveling up infinitely.
