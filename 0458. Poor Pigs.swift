1372517e4127c378756394f9b61ec486dd71b853
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:55:51 +0300

    458. Poor Pigs

--- 0458. Poor Pigs.swift
+++ 0458. Poor Pigs.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 458. Poor Pigs
     // There are buckets buckets of liquid, where exactly one of the buckets is poisonous. To figure out which one is poisonous, you feed some number of (poor) pigs the liquid to see whether they will die or not. Unfortunately, you only have minutesToTest minutes to determine which bucket is poisonous.
     // You can feed the pigs according to these steps:
