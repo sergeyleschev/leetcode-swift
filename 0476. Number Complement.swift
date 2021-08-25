59547c9084dc9f7932cc900a88f980edc16dd4e1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:58:54 +0300

    476. Number Complement

--- 0476. Number Complement.swift
+++ 0476. Number Complement.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 476. Number Complement
     // Given a positive integer num, output its complement number. The complement strategy is to flip the bits of its binary representation.
 
