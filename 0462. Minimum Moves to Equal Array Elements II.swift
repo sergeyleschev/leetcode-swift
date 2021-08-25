fe8542169e0ca0bdac6e3237155b26f79942485c
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:56:37 +0300

    462. Minimum Moves to Equal Array Elements II

--- 0462. Minimum Moves to Equal Array Elements II.swift
+++ 0462. Minimum Moves to Equal Array Elements II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 462. Minimum Moves to Equal Array Elements II
     // Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
     // In one move, you can increment or decrement an element of the array by 1.
