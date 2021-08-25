e080b5a1eb5e8a91f9a9383b87b6b30263e5bc5e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 18:35:24 +0300

    493. Reverse Pairs

--- 0493. Reverse Pairs.swift
+++ 0493. Reverse Pairs.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 493. Reverse Pairs
     // Given an integer array nums, return the number of reverse pairs in the array.
     // A reverse pair is a pair (i, j) where 0 <= i < j < nums.length and nums[i] > 2 * nums[j].
