29d228d2588e6ce694e51d548ad2cc4d269c30fd
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:56:20 +0300

    461. Hamming Distance

--- 0461. Hamming Distance.swift
+++ 0461. Hamming Distance.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 461. Hamming Distance
     // The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
     // Given two integers x and y, return the Hamming distance between them.
