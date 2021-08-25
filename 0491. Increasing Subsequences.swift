3354597ef7803fa02db541f9e84cf3908f4623f4
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 08:01:01 +0300

    491. Increasing Subsequences

--- 0491. Increasing Subsequences.swift
+++ 0491. Increasing Subsequences.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 491. Increasing Subsequences
     // Given an integer array nums, return all the different possible increasing subsequences of the given array with at least two elements. You may return the answer in any order.
     // The given array may contain duplicates, and two equal integers should also be considered a special case of increasing sequence.
