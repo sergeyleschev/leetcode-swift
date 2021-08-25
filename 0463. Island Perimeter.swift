cbc825920ce94ebaba57eb1cd6e00edc4d9d364f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:57:00 +0300

    463. Island Perimeter

--- 0463. Island Perimeter.swift
+++ 0463. Island Perimeter.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 463. Island Perimeter
     // You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
     // Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
