23c71742eed7bbdab1946bdac862d01cc8169dc3
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:55:32 +0300

    456. 132 Pattern

--- 0456. 132 Pattern.swift
+++ 0456. 132 Pattern.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 456. 132 Pattern
     // Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
     // Return true if there is a 132 pattern in nums, otherwise, return false.
