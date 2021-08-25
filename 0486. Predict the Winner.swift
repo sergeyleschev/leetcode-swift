071e67459228f8ad242cd48e2a2819f42e35f25a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 08:00:44 +0300

    486. Predict the Winner

--- 0486. Predict the Winner.swift
+++ 0486. Predict the Winner.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 486. Predict the Winner
     // You are given an integer array nums. Two players are playing a game with this array: player 1 and player 2.
     // Player 1 and player 2 take turns, with player 1 starting first. Both players start the game with a score of 0. At each turn, the player takes one of the numbers from either end of the array (i.e., nums[0] or nums[nums.length - 1]) which reduces the size of the array by 1. The player adds the chosen number to their score. The game ends when there are no more elements in the array.
