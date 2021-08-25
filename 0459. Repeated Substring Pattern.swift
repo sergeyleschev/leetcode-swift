a70e5bf432b94dd8e3393ebb75a8b9a998ed383f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:56:02 +0300

    459. Repeated Substring Pattern

--- 0459. Repeated Substring Pattern.swift
+++ 0459. Repeated Substring Pattern.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 459. Repeated Substring Pattern
     // Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
 
