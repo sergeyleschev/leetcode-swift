f395d9d4fa872401c368d64fda26dabb06831174
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:58:26 +0300

    474. Ones and Zeroes

--- 0474. Ones and Zeroes.swift
+++ 0474. Ones and Zeroes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 474. Ones and Zeroes
     // You are given an array of binary strings strs and two integers m and n.
     // Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
