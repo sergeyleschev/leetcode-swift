e05ee84a3b3e24491007de08383f278ee51172ba
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:57:49 +0300

    468. Validate IP Address

--- 0468. Validate IP Address.swift
+++ 0468. Validate IP Address.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 468. Validate IP Address
     // Given a string IP, return "IPv4" if IP is a valid IPv4 address, "IPv6" if IP is a valid IPv6 address or "Neither" if IP is not a correct IP of any type.
     // A valid IPv4 address is an IP in the form "x1.x2.x3.x4" where 0 <= xi <= 255 and xi cannot contain leading zeros. For example, "192.168.1.1" and "192.168.1.0" are valid IPv4 addresses but "192.168.01.1", while "192.168.1.00" and "192.168@1.1" are invalid IPv4 addresses.
