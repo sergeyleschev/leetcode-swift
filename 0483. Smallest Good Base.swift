c2cfff0b75db241b4fc8adbe65ea83b0a8149a58
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 08:00:17 +0300

    483. Smallest Good Base

--- 0483. Smallest Good Base.swift
+++ 0483. Smallest Good Base.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 483. Smallest Good Base
     // Given an integer n represented as a string, return the smallest good base of n.
     // We call k >= 2 a good base of n, if all digits of n base k are 1's.
