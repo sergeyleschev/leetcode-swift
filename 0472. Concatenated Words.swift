5cc304ff3c549be98bb75e0d817f8e5fe7056fdc
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:58:08 +0300

    472. Concatenated Words

--- 0472. Concatenated Words.swift
+++ 0472. Concatenated Words.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 472. Concatenated Words
     // Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
     // A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
