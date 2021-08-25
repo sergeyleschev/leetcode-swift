ec504132c20f872362f0dcf70708adde7fe88408
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 08:00:07 +0300

    482. License Key Formatting

--- 0482. License Key Formatting.swift
+++ 0482. License Key Formatting.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 482. License Key Formatting
     // You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
     // We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
