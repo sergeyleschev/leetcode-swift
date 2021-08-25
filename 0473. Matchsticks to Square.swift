a2738974d036020234caa8bb747b24349fa8706d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:58:17 +0300

    473. Matchsticks to Square

--- 0473. Matchsticks to Square.swift
+++ 0473. Matchsticks to Square.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 473. Matchsticks to Square
     // You are given an integer array matchsticks where matchsticks[i] is the length of the ith matchstick. You want to use all the matchsticks to make one square. You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.
     // Return true if you can make this square and false otherwise.
