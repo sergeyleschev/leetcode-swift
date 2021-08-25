de182169b0e06b9ec79f40579139f5d3c9094c3a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 18:36:31 +0300

    498. Diagonal Traverse

--- 0498. Diagonal Traverse.swift
+++ 0498. Diagonal Traverse.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 498. Diagonal Traverse
     // Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
 
