4951a94a71f1943223e0f5258443c1fbd0c9f9ed
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 18:35:42 +0300

    495. Teemo Attacking

--- 0495. Teemo Attacking.swift
+++ 0495. Teemo Attacking.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 495. Teemo Attacking
     // Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
     // You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
@@ -39,12 +41,9 @@ class Solution {
     
     func findPoisonedDuration(_ timeSeries: [Int], _ duration: Int) -> Int {
         guard !timeSeries.isEmpty else { return 0 }
-
         var ans = duration
-        for i in 0..<timeSeries.count - 1 {
-            ans += min(duration, timeSeries[i + 1] - timeSeries[i])
-        }
 
+        for i in 0..<timeSeries.count - 1 { ans += min(duration, timeSeries[i + 1] - timeSeries[i]) }
         return ans
     }
 
