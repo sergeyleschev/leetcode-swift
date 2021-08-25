21a8766d51eada7c50fa098ac525ec8097054a4e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:57:59 +0300

    470. Implement Rand10() Using Rand7()

--- 0470. Implement Rand10() Using Rand7().swift
+++ 0470. Implement Rand10() Using Rand7().swift
@@ -5,6 +5,8 @@
  */
 class Solution: SolBase {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 470. Implement Rand10() Using Rand7()
     // Given the API rand7() that generates a uniform random integer in the range [1, 7], write a function rand10() that generates a uniform random integer in the range [1, 10]. You can only call the API rand7(), and you shouldn't call any other API. Please do not use a language's built-in random API.
     // Each test case will have one internal argument n, the number of times that your implemented function rand10() will be called while testing. Note that this is not an argument passed to rand10().
