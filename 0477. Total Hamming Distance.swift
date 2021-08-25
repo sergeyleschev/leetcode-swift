be39f52ee02d9a77b5aedce6bf28234fffb6fdb2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-25 07:59:04 +0300

    477. Total Hamming Distance

--- 0477. Total Hamming Distance.swift
+++ 0477. Total Hamming Distance.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 477. Total Hamming Distance
     // The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
     // Given an integer array nums, return the sum of Hamming distances between all the pairs of the integers in nums.
