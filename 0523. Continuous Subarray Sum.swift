7a24022283735336932d9250bf3a6e090bcdbb70
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:57:55 +0300

    523. Continuous Subarray Sum

--- 0523. Continuous Subarray Sum.swift
+++ 0523. Continuous Subarray Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 523. Continuous Subarray Sum
     // Given an integer array nums and an integer k, return true if nums has a continuous subarray of size at least two whose elements sum up to a multiple of k, or false otherwise.
     // An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
