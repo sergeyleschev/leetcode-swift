5d05e60f69fa39f9397cc54242522cf39f581ea8
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:29:03 +0300

    501. Find Mode in Binary Search Tree

--- 0501. Find Mode in Binary Search Tree.swift
+++ 0501. Find Mode in Binary Search Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 501. Find Mode in Binary Search Tree
     // Given the root of a binary search tree (BST) with duplicates, return all the mode(s) (i.e., the most frequently occurred element) in it.
     // If the tree has more than one mode, return them in any order.
