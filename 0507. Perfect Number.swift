95fd04b3fb457124d6235f6c96c8d2b5f2f0e4d7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:29:43 +0300

    507. Perfect Number

--- 0507. Perfect Number.swift
+++ 0507. Perfect Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 507. Perfect Number
     // A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
     // Given an integer n, return true if n is a perfect number, otherwise return false.
