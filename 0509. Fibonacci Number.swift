6d82a05916ce41890f601c9e585ebfbf1082b732
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:54:51 +0300

    509. Fibonacci Number

--- 0509. Fibonacci Number.swift
+++ 0509. Fibonacci Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 509. Fibonacci Number
     // The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
     // F(0) = 0, F(1) = 1
