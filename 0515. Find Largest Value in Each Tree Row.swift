4759c19a43dc556a4934a90c4965f001d4aa21fa
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:55:20 +0300

    515. Find Largest Value in Each Tree Row

--- 0515. Find Largest Value in Each Tree Row.swift
+++ 0515. Find Largest Value in Each Tree Row.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 515. Find Largest Value in Each Tree Row
     // Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
 
