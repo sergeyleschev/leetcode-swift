b564117f2d3152cd3f96c7e02962d6eb601f94c3
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:58:04 +0300

    524. Longest Word in Dictionary through Deleting

--- 0524. Longest Word in Dictionary through Deleting.swift
+++ 0524. Longest Word in Dictionary through Deleting.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 524. Longest Word in Dictionary through Deleting
     // Given a string s and a string array dictionary, return the longest string in the dictionary that can be formed by deleting some of the given string characters. If there is more than one possible result, return the longest word with the smallest lexicographical order. If there is no possible result, return the empty string.
 
@@ -25,9 +27,7 @@ class Solution {
             var strArr = Array(str)
             var i = 0
             
-            for char in chars {
-                if i < strArr.count && char == strArr[i]  { i += 1 }
-            }
+            for char in chars where i < strArr.count && char == strArr[i] { i += 1 }
             
             if i == strArr.count && i >= res.count {
                 if i > res.count || res.compare(str) == .orderedDescending { res = str }
