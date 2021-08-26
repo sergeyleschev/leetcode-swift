c2f7c832a03a0ad4a4b11e8363538cd74c8ece5d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:58:12 +0300

    525. Contiguous Array

--- 0525. Contiguous Array.swift
+++ 0525. Contiguous Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 525. Contiguous Array
     // Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
 
