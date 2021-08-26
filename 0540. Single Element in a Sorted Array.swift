7b6cb18c7ae151f43e82d5d4cb758389abae1195
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 08:00:00 +0300

    540. Single Element in a Sorted Array

--- 0540. Single Element in a Sorted Array.swift
+++ 0540. Single Element in a Sorted Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 540. Single Element in a Sorted Array
     // You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
     // Follow up: Your solution should run in O(log n) time and O(1) space.
