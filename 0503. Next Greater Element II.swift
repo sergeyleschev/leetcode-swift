ad80fd4ffb52dee8658dbd3232e0cf674ebf0775
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:29:13 +0300

    503. Next Greater Element II

--- 0503. Next Greater Element II.swift
+++ 0503. Next Greater Element II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 503. Next Greater Element II
     // Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
     // The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
