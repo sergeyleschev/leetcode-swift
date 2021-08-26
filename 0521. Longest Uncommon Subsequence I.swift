daaa78be2ce33165121ee598f5bd0da72645029e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:57:09 +0300

    521. Longest Uncommon Subsequence I

--- 0521. Longest Uncommon Subsequence I.swift
+++ 0521. Longest Uncommon Subsequence I.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 521. Longest Uncommon Subsequence I
     // Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
     // An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
