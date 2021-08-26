9e28a820b7786ad2bb9078effadc14150b83e608
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:54:42 +0300

    508. Most Frequent Subtree Sum

--- 0508. Most Frequent Subtree Sum.swift
+++ 0508. Most Frequent Subtree Sum.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 508. Most Frequent Subtree Sum
     // Given the root of a binary tree, return the most frequent subtree sum. If there is a tie, return all the values with the highest frequency in any order.
     // The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself).
