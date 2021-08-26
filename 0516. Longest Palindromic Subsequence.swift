d57d5f512783917f0d23aec08970429dbb41ed5f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:55:30 +0300

    516. Longest Palindromic Subsequence

--- 0516. Longest Palindromic Subsequence.swift
+++ 0516. Longest Palindromic Subsequence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 516. Longest Palindromic Subsequence
     // Given a string s, find the longest palindromic subsequence's length in s.
     // A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
