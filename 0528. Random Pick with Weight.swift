ad3539c82f4f268f9ad6533c805fb7b201d588f3
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:58:29 +0300

    528. Random Pick with Weight

--- 0528. Random Pick with Weight.swift
+++ 0528. Random Pick with Weight.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 528. Random Pick with Weight
     // You are given an array of positive integers w where w[i] describes the weight of ith index (0-indexed).
     // We need to call the function pickIndex() which randomly returns an integer in the range [0, w.length - 1]. pickIndex() should return the integer proportional to its weight in the w array. For example, for w = [1, 3], the probability of picking the index 0 is 1 / (1 + 3) = 0.25 (i.e 25%) while the probability of picking the index 1 is 3 / (1 + 3) = 0.75 (i.e 75%).
