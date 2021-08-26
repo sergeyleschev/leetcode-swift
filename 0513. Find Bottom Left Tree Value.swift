3982b74ce9e7de47320efbc1130e17dfe1a27318
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 07:55:01 +0300

    513. Find Bottom Left Tree Value

--- 0513. Find Bottom Left Tree Value.swift
+++ 0513. Find Bottom Left Tree Value.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 513. Find Bottom Left Tree Value
     // Given the root of a binary tree, return the leftmost value in the last row of the tree.
 
