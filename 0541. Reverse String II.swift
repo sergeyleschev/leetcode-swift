1800929db13ae564e41c3368a00e751e697eb6b2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 23:20:53 +0300

    541. Reverse String II

--- 0541. Reverse String II.swift
+++ 0541. Reverse String II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 541. Reverse String II
     // Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
     // If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
