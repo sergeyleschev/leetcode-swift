0d9fe9e614fae090f3996f71c509871bd4a2601a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-26 23:21:05 +0300

    542. 01 Matrix

--- 0542. 01 Matrix.swift
+++ 0542. 01 Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 542. 01 Matrix
     // Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
     // The distance between two adjacent cells is 1.
