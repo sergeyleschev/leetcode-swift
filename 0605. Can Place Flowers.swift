fee241fe953ce2111be14686afcb73c823973747
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 22:00:25 +0300

    605. Can Place Flowers

--- 0605. Can Place Flowers.swift
+++ 0605. Can Place Flowers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 605. Can Place Flowers
     // You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
     // Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
