7921fdd5b5701f539460deaa6e30c304c8c5586d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:44:56 +0300

    557. Reverse Words in a String III

--- 0557. Reverse Words in a String III.swift
+++ 0557. Reverse Words in a String III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 557. Reverse Words in a String III
     // Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 
