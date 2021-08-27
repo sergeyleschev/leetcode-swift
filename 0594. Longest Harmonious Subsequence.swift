54166f3031252dd73d29913662fad0213db819e2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:54:51 +0300

    594. Longest Harmonious Subsequence

--- 0594. Longest Harmonious Subsequence.swift
+++ 0594. Longest Harmonious Subsequence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 594. Longest Harmonious Subsequence
     // We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
     // Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
