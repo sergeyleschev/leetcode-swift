7d1638754f7db51ad94099e415bb5bbfe811aba9
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:55:36 +0300

    599. Minimum Index Sum of Two Lists

--- 0599. Minimum Index Sum of Two Lists.swift
+++ 0599. Minimum Index Sum of Two Lists.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 599. Minimum Index Sum of Two Lists
     // Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
     // You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
