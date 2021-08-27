4a888e8023fb5b7104f1c1ee958cadb8c8b9b723
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:53:45 +0300

    581. Shortest Unsorted Continuous Subarray

--- 0581. Shortest Unsorted Continuous Subarray.swift
+++ 0581. Shortest Unsorted Continuous Subarray.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 581. Shortest Unsorted Continuous Subarray
     // Given an integer array nums, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order.
     // Return the shortest such subarray and output its length.
