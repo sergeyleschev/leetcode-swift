59757dc5cf6720cf93a21dde0f83c256acf8738e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:54:20 +0300

    590. N-ary Tree Postorder Traversal

--- 0590. N-ary Tree Postorder Traversal.swift
+++ 0590. N-ary Tree Postorder Traversal.swift
@@ -11,6 +11,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 590. N-ary Tree Postorder Traversal
     // Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
     // Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
