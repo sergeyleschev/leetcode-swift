0d35e3a56c07ace225f7c220a3b3f110b725622e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:51:31 +0300

    575. Distribute Candies

--- 0575. Distribute Candies.swift
+++ 0575. Distribute Candies.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 575. Distribute Candies
     // Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
     // The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
