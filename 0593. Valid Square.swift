707956e2b1e628b2af7fa1ad965c12d1758f7adf
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:54:42 +0300

    593. Valid Square

--- 0593. Valid Square.swift
+++ 0593. Valid Square.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 593. Valid Square
     // Given the coordinates of four points in 2D space p1, p2, p3 and p4, return true if the four points construct a square.
     // The coordinate of a point pi is represented as [xi, yi]. The input is not given in any order.
