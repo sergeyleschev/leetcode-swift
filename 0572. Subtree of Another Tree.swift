611baaa007f1b36f2447c789cbd5a83f5dba9854
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:51:22 +0300

    572. Subtree of Another Tree

--- 0572. Subtree of Another Tree.swift
+++ 0572. Subtree of Another Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 572. Subtree of Another Tree
     // Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
     // A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
