69990cf87223f982f0ecbd217e62c4f6036b00e2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:45:17 +0300

    559. Maximum Depth of N-ary Tree

--- 0559. Maximum Depth of N-ary Tree.swift
+++ 0559. Maximum Depth of N-ary Tree.swift
@@ -11,6 +11,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 559. Maximum Depth of N-ary Tree
     // Given a n-ary tree, find its maximum depth.
     // The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
