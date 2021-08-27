16b9ec3f5c8619d0a04d9733dff25da588afcb4d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:53:35 +0300

    576. Out of Boundary Paths

--- 0576. Out of Boundary Paths.swift
+++ 0576. Out of Boundary Paths.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 576. Out of Boundary Paths
     // There is an m x n grid with a ball. The ball is initially at the position [startRow, startColumn]. You are allowed to move the ball to one of the four adjacent four cells in the grid (possibly out of the grid crossing the grid boundary). You can apply at most maxMove moves to the ball.
     // Given the five integers m, n, maxMove, startRow, startColumn, return the number of paths to move the ball out of the grid boundary. Since the answer can be very large, return it modulo 109 + 7.
