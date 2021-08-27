ddf3a258a5b6adf32cf0d60ba34b636a241795b1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:50:26 +0300

    561. Array Partition I

--- 0561. Array Partition I.swift
+++ 0561. Array Partition I.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 561. Array Partition I
     // Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
 
