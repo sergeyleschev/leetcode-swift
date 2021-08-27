5851427fe8fbe27b30cd6165401cbf565a91c17a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:50:44 +0300

    564. Find the Closest Palindrome

--- 0564. Find the Closest Palindrome.swift
+++ 0564. Find the Closest Palindrome.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 564. Find the Closest Palindrome
     // Given a string n representing an integer, return the closest integer (not including itself), which is a palindrome. If there is a tie, return the smaller one.
     // The closest is defined as the absolute difference minimized between two integers.
