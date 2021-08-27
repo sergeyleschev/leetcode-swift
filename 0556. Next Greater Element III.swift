ba622c0853acf91280f4148240704ed0d294652d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:44:47 +0300

    556. Next Greater Element III

--- 0556. Next Greater Element III.swift
+++ 0556. Next Greater Element III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 556. Next Greater Element III
     // Given a positive integer n, find the smallest integer which has exactly the same digits existing in the integer n and is greater in value than n. If no such positive integer exists, return -1.
     // Note that the returned integer should fit in 32-bit integer, if there is a valid answer but it does not fit in 32-bit integer, return -1.
