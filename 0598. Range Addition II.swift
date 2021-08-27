e39d0d5be58dcc0aaecb4dccfbebd20747900f67
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:55:25 +0300

    598. Range Addition II

--- 0598. Range Addition II.swift
+++ 0598. Range Addition II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 598. Range Addition II
     // You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
     // Count and return the number of maximum integers in the matrix after performing all the operations.
