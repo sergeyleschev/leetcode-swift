143549a7a9f1e5167195fb96bd0914cd99c0e63b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 22:02:00 +0300

    621. Task Scheduler

--- 0621. Task Scheduler.swift
+++ 0621. Task Scheduler.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 621. Task Scheduler
     // Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
     // However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
