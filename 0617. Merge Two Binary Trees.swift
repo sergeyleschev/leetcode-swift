23ae1c50e5421255eba9c792a7de78f625745228
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 22:01:05 +0300

    617. Merge Two Binary Trees

--- 0617. Merge Two Binary Trees.swift
+++ 0617. Merge Two Binary Trees.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 617. Merge Two Binary Trees
     // You are given two binary trees root1 and root2.
     // Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
