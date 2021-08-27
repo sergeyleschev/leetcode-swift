f819e19dac7ee0c7decc1d90a57371707360a888
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:53:53 +0300

    583. Delete Operation for Two Strings

--- 0583. Delete Operation for Two Strings.swift
+++ 0583. Delete Operation for Two Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 583. Delete Operation for Two Strings
     // Given two strings word1 and word2, return the minimum number of steps required to make word1 and word2 the same.
     // In one step, you can delete exactly one character in either string.
