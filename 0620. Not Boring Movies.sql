fbeb8625aab683a40b1b3dbd1c7f331c414d0ff3
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 22:01:15 +0300

    620. Not Boring Movies

--- 0620. Not Boring Movies.sql
+++ 0620. Not Boring Movies.sql
@@ -1,3 +1,5 @@
+-- Solution @ Sergey Leschev, Belarusian State University
+
 -- 620. Not Boring Movies
 -- X city opened a new cinema, many people would like to go to this cinema. The cinema also gives out a poster indicating the movies’ ratings and descriptions.
 -- Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating.
