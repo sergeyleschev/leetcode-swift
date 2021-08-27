62cc8388c85e8c35853ae2d112e69c583eca76c1
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:50:35 +0300

    563. Binary Tree Tilt

--- 0563. Binary Tree Tilt.swift
+++ 0563. Binary Tree Tilt.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 563. Binary Tree Tilt
     // Given the root of a binary tree, return the sum of every tree node's tilt.
     // The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0. The rule is similar if there the node does not have a right child.
