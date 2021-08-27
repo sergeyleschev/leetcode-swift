aece0f61d6344c2b1cccd34dd3e013b7758dcd22
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:54:03 +0300

    589. N-ary Tree Preorder Traversal

--- 0589. N-ary Tree Preorder Traversal.swift
+++ 0589. N-ary Tree Preorder Traversal.swift
@@ -11,6 +11,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 589. N-ary Tree Preorder Traversal
     // Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
     // Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
