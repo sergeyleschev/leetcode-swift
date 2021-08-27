2e72fcee1615800c915554cb3267669c8985a856
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:51:13 +0300

    567. Permutation in String

--- 0567. Permutation in String.swift
+++ 0567. Permutation in String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 567. Permutation in String
     // Given two strings s1 and s2, return true if s2 contains the permutation of s1.
     // In other words, one of s1's permutations is the substring of s2.
