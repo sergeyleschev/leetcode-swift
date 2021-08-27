801912acd8916aa27b76cb231dd4b5d4f1746ffc
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-27 07:44:32 +0300

    554. Brick Wall

--- 0554. Brick Wall.swift
+++ 0554. Brick Wall.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 554. Brick Wall
     // There is a rectangular brick wall in front of you with n rows of bricks. The ith row has some number of bricks each of the same height (i.e., one unit) but they can be of different widths. The total width of each row is the same.
     // Draw a vertical line from the top to the bottom and cross the least bricks. If your line goes through the edge of a brick, then the brick is not considered as crossed. You cannot draw a line just along one of the two vertical edges of the wall, in which case the line will obviously cross no bricks.
