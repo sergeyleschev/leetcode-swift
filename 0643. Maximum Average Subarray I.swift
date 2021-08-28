e6ea5058fa4c92402989c360c04c53922cdb6a34
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:14:52 +0300

    643. Maximum Average Subarray I

--- 0643. Maximum Average Subarray I.swift
+++ 0643. Maximum Average Subarray I.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 643. Maximum Average Subarray I
     // You are given an integer array nums consisting of n elements, and an integer k.
     // Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
