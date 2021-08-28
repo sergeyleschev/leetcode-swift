7cf36cd6b153f9c9d8f1b60819c5beee1fe8d7a5
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:15:28 +0300

    648. Replace Words

--- 0648. Replace Words.swift
+++ 0648. Replace Words.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 648. Replace Words
     // In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word successor. For example, when the root "an" is followed by the successor word "other", we can form a new word "another".
     // Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the successors in the sentence with the root forming it. If a successor can be replaced by more than one root, replace it with the root that has the shortest length.
