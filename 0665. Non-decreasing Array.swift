36ffe37c6717ee03a9abde05743a93a9ff2fab01
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 21:22:25 +0300

    665. Non-decreasing Array

--- 0665. Non-decreasing Array.swift
+++ 0665. Non-decreasing Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 665. Non-decreasing Array
     // Given an array nums with n integers, your task is to check if it could become non-decreasing by modifying at most one element.
     // We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
