2c1eeb7de70968ab6b63f2ef8feb63b4fdf9f1be
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:08:06 +0300

    629. K Inverse Pairs Array

--- 0629. K Inverse Pairs Array.swift
+++ 0629. K Inverse Pairs Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 629. K Inverse Pairs Array
     // For an integer array nums, an inverse pair is a pair of integers [i, j] where 0 <= i < j < nums.length and nums[i] > nums[j].
     // Given two integers n and k, return the number of different arrays consist of numbers from 1 to n such that there are exactly k inverse pairs. Since the answer can be huge, return it modulo 109 + 7.
