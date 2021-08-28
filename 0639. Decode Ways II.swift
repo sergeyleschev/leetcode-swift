3902f31c50f5a6c092cebeab62a257c40de918ea
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:09:09 +0300

    639. Decode Ways II

--- 0639. Decode Ways II.swift
+++ 0639. Decode Ways II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 639. Decode Ways II
     // A message containing letters from A-Z can be encoded into numbers using the following mapping:
     // 'A' -> "1"
