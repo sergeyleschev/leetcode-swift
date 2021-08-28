ace2979ac2421cbaa05af1b6496c5710c2d333f6
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:07:57 +0300

    628. Maximum Product of Three Numbers

--- 0628. Maximum Product of Three Numbers.swift
+++ 0628. Maximum Product of Three Numbers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 628. Maximum Product of Three Numbers
     // Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
 
