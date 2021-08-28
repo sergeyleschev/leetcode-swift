8f8c18c458f33a73d015278bd657a9e6f2fc35f4
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:08:16 +0300

    630. Course Schedule III

--- 0630. Course Schedule III.swift
+++ 0630. Course Schedule III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 630. Course Schedule III
     // There are n different online courses numbered from 1 to n. You are given an array courses where courses[i] = [durationi, lastDayi] indicate that the ith course should be taken continuously for durationi days and must be finished before or on lastDayi.
     // You will start on the 1st day and you cannot take two or more courses simultaneously.
