47dc436ebdfd67dca12e62573b2fcab4a6c0b209
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:08:51 +0300

    637. Average of Levels in Binary Tree

--- 0637. Average of Levels in Binary Tree.swift
+++ 0637. Average of Levels in Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 637. Average of Levels in Binary Tree
     // Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
 
