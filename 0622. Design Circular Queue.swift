44bf104ca8acfd79bfc69c7441a8f4ff10391453
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:05:52 +0300

    622. Design Circular Queue

--- 0622. Design Circular Queue.swift
+++ 0622. Design Circular Queue.swift
@@ -1,5 +1,7 @@
 class MyCircularQueue {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 622. Design Circular Queue
     // Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
     // One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
