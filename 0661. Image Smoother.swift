8d39b4672d4b17217cb57c55b79bb8a59ae27ecb
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 21:21:38 +0300

    661. Image Smoother

--- 0661. Image Smoother.swift
+++ 0661. Image Smoother.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 661. Image Smoother
     // An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, we do not consider it in the average (i.e., the average of the four cells in the red smoother).
     // Given an m x n integer matrix img representing the grayscale of an image, return the image after applying the smoother on each cell of it.
