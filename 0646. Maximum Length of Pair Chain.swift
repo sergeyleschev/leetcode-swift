95f58e7303a5d0a2ad55afc39602306340b7536b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:15:10 +0300

    646. Maximum Length of Pair Chain

--- 0646. Maximum Length of Pair Chain.swift
+++ 0646. Maximum Length of Pair Chain.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 646. Maximum Length of Pair Chain
     // You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
     // A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
