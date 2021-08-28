ca3a7b2ef2a4940af1301a4272d462996b8f273e
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:09:00 +0300

    638. Shopping Offers

--- 0638. Shopping Offers.swift
+++ 0638. Shopping Offers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 638. Shopping Offers
     // In LeetCode Store, there are n items to sell. Each item has a price. However, there are some special offers, and a special offer consists of one or more different kinds of items with a sale price.
     // You are given an integer array price where price[i] is the price of the ith item, and an integer array needs where needs[i] is the number of pieces of the ith item you want to buy.
