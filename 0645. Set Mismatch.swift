7332bba469ed131c85869f3340ef7c6cbe44193d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:15:01 +0300

    645. Set Mismatch

--- 0645. Set Mismatch.swift
+++ 0645. Set Mismatch.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 645. Set Mismatch
     // You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
     // You are given an integer array nums representing the data status of this set after the error.
