0087933e6b77723875d63df03ca107f4bda57af4
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 21:22:35 +0300

    667. Beautiful Arrangement II

--- 0667. Beautiful Arrangement II.swift
+++ 0667. Beautiful Arrangement II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 667. Beautiful Arrangement II
     // Given two integers n and k, construct a list answer that contains n different positive integers ranging from 1 to n and obeys the following requirement:
     // Suppose this list is answer = [a1, a2, a3, ... , an], then the list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has exactly k distinct integers.
