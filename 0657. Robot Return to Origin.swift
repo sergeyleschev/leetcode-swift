4e89e3f674d4ce39dae147e2031af062520cd159
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 13:27:13 +0300

    657. Robot Return to Origin

--- 0657. Robot Return to Origin.swift
+++ 0657. Robot Return to Origin.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 657. Robot Return to Origin
     // There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
     // The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
