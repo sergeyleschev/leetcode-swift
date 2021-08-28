ee167ab352d78288cdc6168a6a205c2d1e699931
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 08:08:26 +0300

    632. Smallest Range Covering Elements from K Lists

--- 0632. Smallest Range Covering Elements from K Lists.swift
+++ 0632. Smallest Range Covering Elements from K Lists.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 632. Smallest Range Covering Elements from K Lists
     // You have k lists of sorted integers in non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
     // We define the range [a, b] is smaller than range [c, d] if b - a < d - c or a < c if b - a == d - c.
