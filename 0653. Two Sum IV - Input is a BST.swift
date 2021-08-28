2546a2197e1eeda58ac28633e5ea2fbf6d1da787
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-28 13:25:54 +0300

    653. Two Sum IV - Input is a BST

--- 0653. Two Sum IV - Input is a BST.swift
+++ 0653. Two Sum IV - Input is a BST.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 653. Two Sum IV - Input is a BST
     // Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
 
