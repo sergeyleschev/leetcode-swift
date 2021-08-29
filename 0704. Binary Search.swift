aa318163c113e63358d8576703069b79682c92d2
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 22:45:45 +0300

    704. Binary Search

--- 0704. Binary Search.swift
+++ 0704. Binary Search.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 704. Binary Search
     // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
     // You must write an algorithm with O(log n) runtime complexity.
