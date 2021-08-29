bcd95ab49e89658ea1224635f1a6076e989e59cd
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 21:49:40 +0300

    692. Top K Frequent Words

--- 0692. Top K Frequent Words.swift
+++ 0692. Top K Frequent Words.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 692. Top K Frequent Words
     // Given a non-empty list of words, return the k most frequent elements.
     // Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
