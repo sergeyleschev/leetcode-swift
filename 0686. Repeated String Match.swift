fe31414ef6ff37c9c4d211fc46c7b5a518f7b374
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:14:55 +0300

    686. Repeated String Match

--- 0686. Repeated String Match.swift
+++ 0686. Repeated String Match.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 686. Repeated String Match
     // Given two strings a and b, return the minimum number of times you should repeat string a so that string b is a substring of it. If it is impossible for b to be a substring of a after repeating it, return -1.
     // Notice: string "abc" repeated 0 times is "",  repeated 1 time is "abc" and repeated 2 times is "abcabc".
