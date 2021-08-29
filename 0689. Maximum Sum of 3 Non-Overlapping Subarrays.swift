252b0e53a7130685a205adcaff469179966fbf0a
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:15:34 +0300

    689. Maximum Sum of 3 Non-Overlapping Subarrays

--- 0689. Maximum Sum of 3 Non-Overlapping Subarrays.swift
+++ 0689. Maximum Sum of 3 Non-Overlapping Subarrays.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 689. Maximum Sum of 3 Non-Overlapping Subarrays
     // Given an integer array nums and an integer k, find three non-overlapping subarrays of length k with maximum sum and return them.
     // Return the result as a list of indices representing the starting position of each interval (0-indexed). If there are multiple answers, return the lexicographically smallest one.
