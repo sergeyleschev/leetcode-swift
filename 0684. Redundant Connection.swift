62e3bdef264fe635dd6a4e48ed6ed41eb549002b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:14:38 +0300

    684. Redundant Connection

--- 0684. Redundant Connection.swift
+++ 0684. Redundant Connection.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 684. Redundant Connection
     // In this problem, a tree is an undirected graph that is connected and has no cycles.
     // You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
