943fbe1c910f7e677a0de2c184d072ec22db6b60
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:12:25 +0300

    671. Second Minimum Node In a Binary Tree

--- 0671. Second Minimum Node In a Binary Tree.swift
+++ 0671. Second Minimum Node In a Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 671. Second Minimum Node In a Binary Tree
     // Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
     // Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
