502870e037441a2162fb5b2640ea93850be93cbb
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:12:06 +0300

    669. Trim a Binary Search Tree

--- 0669. Trim a Binary Search Tree.swift
+++ 0669. Trim a Binary Search Tree.swift
@@ -16,6 +16,8 @@
 
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 669. Trim a Binary Search Tree
     // Given the root of a binary search tree and the lowest and highest boundaries as low and high, trim the tree so that all its elements lies in [low, high]. Trimming the tree should not change the relative structure of the elements that will remain in the tree (i.e., any node's descendant should remain a descendant). It can be proven that there is a unique answer.
     // Return the root of the trimmed binary search tree. Note that the root may change depending on the given bounds.
