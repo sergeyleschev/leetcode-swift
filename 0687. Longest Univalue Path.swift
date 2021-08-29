7293a3fedafcf09d7608f3dc6e5c65d6746224a5
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:15:15 +0300

    687. Longest Univalue Path

--- 0687. Longest Univalue Path.swift
+++ 0687. Longest Univalue Path.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 687. Longest Univalue Path
     // Given the root of a binary tree, return the length of the longest path, where each node in the path has the same value. This path may or may not pass through the root.
     // The length of the path between two nodes is represented by the number of edges between them.
