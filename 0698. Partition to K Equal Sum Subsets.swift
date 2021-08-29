a6081f91d24698f9c3b4bb3c2bb57544426b7c05
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 21:50:42 +0300

    698. Partition to K Equal Sum Subsets

--- 0698. Partition to K Equal Sum Subsets.swift
+++ 0698. Partition to K Equal Sum Subsets.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 698. Partition to K Equal Sum Subsets
     // Given an integer array nums and an integer k, return true if it is possible to divide this array into k non-empty subsets whose sums are all equal.
 
