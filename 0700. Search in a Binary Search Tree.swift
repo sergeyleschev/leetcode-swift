f2aea0c9661a51d499e53eabf2fa668dc7998d54
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 21:51:02 +0300

    700. Search in a Binary Search Tree

--- 0700. Search in a Binary Search Tree.swift
+++ 0700. Search in a Binary Search Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 700. Search in a Binary Search Tree
     // You are given the root of a binary search tree (BST) and an integer val.
     // Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
