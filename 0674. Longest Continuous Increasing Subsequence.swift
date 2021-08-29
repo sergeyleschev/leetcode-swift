f1b7ff94af481bd2fc71597ff58ea1ecd3cef0fb
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:12:54 +0300

    674. Longest Continuous Increasing Subsequence

--- 0674. Longest Continuous Increasing Subsequence.swift
+++ 0674. Longest Continuous Increasing Subsequence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 674. Longest Continuous Increasing Subsequence
     // Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence (i.e. subarray). The subsequence must be strictly increasing.
     // A continuous increasing subsequence is defined by two indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1].
