2d5e685a55cc9fe9f260b18bbe21ba409424a81b
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 21:50:17 +0300

    696. Count Binary Substrings

--- 0696. Count Binary Substrings.swift
+++ 0696. Count Binary Substrings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 696. Count Binary Substrings
     // Give a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
     // Substrings that occur multiple times are counted the number of times they occur.
