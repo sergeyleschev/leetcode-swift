2ec9e8f9dd0c20b6fb751757349be498d28fc5c7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 22:46:02 +0300

    705. Design HashSet

--- 0705. Design HashSet.swift
+++ 0705. Design HashSet.swift
@@ -1,5 +1,7 @@
 class MyHashSet {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 705. Design HashSet
     // Design a HashSet without using any built-in hash table libraries.
     // Implement MyHashSet class:
