566a98618e979f0226e50d77f9adb05b6ce0a588
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:12:16 +0300

    670. Maximum Swap

--- 0670. Maximum Swap.swift
+++ 0670. Maximum Swap.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 670. Maximum Swap
     // You are given an integer num. You can swap two digits at most once to get the maximum valued number.
     // Return the maximum valued number you can get.
