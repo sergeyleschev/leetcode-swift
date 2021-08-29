a19a682a22939b2b5acc13f1c602d25f0e50fa62
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 21:50:27 +0300

    697. Degree of an Array

--- 0697. Degree of an Array.swift
+++ 0697. Degree of an Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 697. Degree of an Array
     // Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
     // Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
