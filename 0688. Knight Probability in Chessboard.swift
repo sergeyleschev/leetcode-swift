2cd94d523ef213111ff3d729a02d4dbc18ea8acf
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-29 07:15:24 +0300

    688. Knight Probability in Chessboard

--- 0688. Knight Probability in Chessboard.swift
+++ 0688. Knight Probability in Chessboard.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 688. Knight Probability in Chessboard
     // On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1).
     // A chess knight has eight possible moves it can make, as illustrated below. Each move is two cells in a cardinal direction, then one cell in an orthogonal direction.
