67750123053d584a540764e64a414c70cbba7e66
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:02:07 +0300

    721. Accounts Merge

--- 0721. Accounts Merge.swift
+++ 0721. Accounts Merge.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 721. Accounts Merge
     // Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
     // Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
