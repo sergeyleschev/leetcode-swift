5762aa03db33d136a0165ed6f0ddf1f6e5601ee3
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:02:38 +0300

    724. Find Pivot Index

--- 0724. Find Pivot Index.swift
+++ 0724. Find Pivot Index.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 724. Find Pivot Index
     // Given an array of integers nums, calculate the pivot index of this array.
     // The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
