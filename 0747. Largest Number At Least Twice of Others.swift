4de9e19221847d1d003850602d24e5d5c7070c9f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 22:31:55 +0300

    747. Largest Number At Least Twice of Others

--- 0747. Largest Number At Least Twice of Others.swift
+++ 0747. Largest Number At Least Twice of Others.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 747. Largest Number At Least Twice of Others
     // You are given an integer array nums where the largest integer is unique.
     // Determine whether the largest element in the array is at least twice as much as every other number in the array. If it is, return the index of the largest element, or return -1 otherwise.
