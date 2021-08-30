fac75b581ab82a887804e59bbd049fbb5c712eec
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:01:49 +0300

    719. Find K-th Smallest Pair Distance

--- 0719. Find K-th Smallest Pair Distance.swift
+++ 0719. Find K-th Smallest Pair Distance.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 719. Find K-th Smallest Pair Distance
     // The distance of a pair of integers a and b is defined as the absolute difference between a and b.
     // Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
