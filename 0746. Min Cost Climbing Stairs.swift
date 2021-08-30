f1d4eb7504be89ea693111f0cb95d85c9b6a76bd
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 22:31:42 +0300

    746. Min Cost Climbing Stairs

--- 0746. Min Cost Climbing Stairs.swift
+++ 0746. Min Cost Climbing Stairs.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 746. Min Cost Climbing Stairs
     // You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
     // You can either start from the step with index 0, or the step with index 1.
