b09034a450ed43ef2aa48468624a60555b0c24eb
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 22:32:45 +0300

    752. Open the Lock

--- 0752. Open the Lock.swift
+++ 0752. Open the Lock.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 752. Open the Lock
     // You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move consists of turning one wheel one slot.
     // The lock initially starts at '0000', a string representing the state of the 4 wheels.
