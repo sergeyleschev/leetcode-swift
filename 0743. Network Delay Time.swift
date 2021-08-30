5befcbc3024d882a9a0a13ad8d32af94dc184bd9
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:05:18 +0300

    743. Network Delay Time

--- 0743. Network Delay Time.swift
+++ 0743. Network Delay Time.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 743. Network Delay Time
     // You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target.
     // We will send a signal from a given node k. Return the time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
