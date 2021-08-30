08835022122d233da17c29f8bd659e54113204c9
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:01:58 +0300

    720. Longest Word in Dictionary

--- 0720. Longest Word in Dictionary.swift
+++ 0720. Longest Word in Dictionary.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 720. Longest Word in Dictionary
     // Given an array of strings words representing an English Dictionary, return the longest word in words that can be built one character at a time by other words in words.
     // If there is more than one possible answer, return the longest word with the smallest lexicographical order. If there is no answer, return the empty string.
