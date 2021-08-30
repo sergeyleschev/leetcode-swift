995af0d78429ef053252b8359aaaeefaafe24ecb
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:00:34 +0300

    713. Subarray Product Less Than K

--- 0713. Subarray Product Less Than K.swift
+++ 0713. Subarray Product Less Than K.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 713. Subarray Product Less Than K
     // Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
 
