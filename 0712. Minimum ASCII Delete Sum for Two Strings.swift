b3bdd3054237d7907bf08c8939b200a0395e992d
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:00:25 +0300

    712. Minimum ASCII Delete Sum for Two Strings

--- 0712. Minimum ASCII Delete Sum for Two Strings.swift
+++ 0712. Minimum ASCII Delete Sum for Two Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 712. Minimum ASCII Delete Sum for Two Strings
     // Given two strings s1 and s2, return the lowest ASCII sum of deleted characters to make two strings equal.
 
