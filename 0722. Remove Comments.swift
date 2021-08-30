3b1335ca7f8fb03d6b7e9d481d88cbc538b97f5c
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 08:02:26 +0300

    722. Remove Comments

--- 0722. Remove Comments.swift
+++ 0722. Remove Comments.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 722. Remove Comments
     // Given a C++ program, remove comments from it. The program source is an array of strings source where source[i] is the ith line of the source code. This represents the result of splitting the original source code string by the newline character '\n'.
     // In C++, there are two types of comments, line comments, and block comments.
