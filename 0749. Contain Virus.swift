f0983fdba2c39e2ee2a0808a1709d55b44d0a9ce
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-30 22:32:24 +0300

    749. Contain Virus

--- 0749. Contain Virus.swift
+++ 0749. Contain Virus.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 749. Contain Virus
     // A virus is spreading rapidly, and your task is to quarantine the infected area by installing walls.
     // The world is modeled as an m x n binary grid isInfected, where isInfected[i][j] == 0 represents uninfected cells, and isInfected[i][j] == 1 represents cells contaminated with the virus. A wall (and only one wall) can be installed between any two 4-directionally adjacent cells, on the shared boundary.
