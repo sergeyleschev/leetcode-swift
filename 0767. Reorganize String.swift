84490117ccc2670e090d20db989ef967ee8e82f7
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:10:08 +0300

    767. Reorganize String

--- 0767. Reorganize String.swift
+++ 0767. Reorganize String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 767. Reorganize String
     // Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
     // Return any possible rearrangement of s or return "" if not possible.
