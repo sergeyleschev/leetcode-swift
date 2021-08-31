5b366887fd302f986f2f4be942d93f335abc9389
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:09:12 +0300

    763. Partition Labels

--- 0763. Partition Labels.swift
+++ 0763. Partition Labels.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 763. Partition Labels
     // You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
     // Return a list of integers representing the size of these parts.
