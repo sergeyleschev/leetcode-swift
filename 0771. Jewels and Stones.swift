f22e098a01906604453be024c227969feee61df0
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:10:40 +0300

    771. Jewels and Stones

--- 0771. Jewels and Stones.swift
+++ 0771. Jewels and Stones.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 771. Jewels and Stones
     // You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
     // Letters are case sensitive, so "a" is considered a different type of stone from "A".
