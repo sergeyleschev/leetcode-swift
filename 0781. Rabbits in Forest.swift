8329a9fee4923ef091fd3c168ae922df032a75a0
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:11:42 +0300

    781. Rabbits in Forest

--- 0781. Rabbits in Forest.swift
+++ 0781. Rabbits in Forest.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 781. Rabbits in Forest
     // There is a forest with an unknown number of rabbits. We asked n rabbits "How many rabbits have the same color as you?" and collected the answers in an integer array answers where answers[i] is the answer of the ith rabbit.
     // Given the array answers, return the minimum number of rabbits that could be in the forest.
