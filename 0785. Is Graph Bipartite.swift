0637a4236fa6520a38c34e48659988f62d862265
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:12:21 +0300

    785. Is Graph Bipartite

--- 0785. Is Graph Bipartite.swift
+++ 0785. Is Graph Bipartite.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 785. Is Graph Bipartite
     // There is an undirected graph with n nodes, where each node is numbered between 0 and n - 1. You are given a 2D array graph, where graph[u] is an array of nodes that node u is adjacent to. More formally, for each v in graph[u], there is an undirected edge between node u and node v. The graph has the following properties:
     // There are no self-edges (graph[u] does not contain u).
