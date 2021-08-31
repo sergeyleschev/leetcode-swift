386aa390ec2eb6b8c6524babc2453fdf5ca33f8f
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:11:06 +0300

    777. Swap Adjacent in LR String

--- 0777. Swap Adjacent in LR String.swift
+++ 0777. Swap Adjacent in LR String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 777. Swap Adjacent in LR String
     // In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL", a move consists of either replacing one occurrence of "XL" with "LX", or replacing one occurrence of "RX" with "XR". Given the starting string start and the ending string end, return True if and only if there exists a sequence of moves to transform one string to the other.
 
