463ed3bda02ae6e00a7070ca85a3d2253b003094
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 00:07:45 +0300

    787. Cheapest Flights Within K Stops

--- 0787. Cheapest Flights Within K Stops.swift
+++ 0787. Cheapest Flights Within K Stops.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 787. Cheapest Flights Within K Stops
     // There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
     // You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
