497942ed66bb07b45c737a4a22384a4a9a68aaf8
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:10:30 +0300

    769. Max Chunks To Make Sorted

--- 0769. Max Chunks To Make Sorted.swift
+++ 0769. Max Chunks To Make Sorted.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 769. Max Chunks To Make Sorted
     // You are given an integer array arr of length n that represents a permutation of the integers in the range [0, n - 1].
     // We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating them, the result should equal the sorted array.
