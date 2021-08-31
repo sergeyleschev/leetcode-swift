3bb025385e4b3736e194f461c371055a23d91e8c
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:09:22 +0300

    764. Largest Plus Sign

--- 0764. Largest Plus Sign.swift
+++ 0764. Largest Plus Sign.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 764. Largest Plus Sign
     // You are given an integer n. You have an n x n binary grid grid with all values initially 1's except for some indices given in the array mines. The ith element of the array mines is defined as mines[i] = [xi, yi] where grid[xi][yi] == 0.
     // Return the order of the largest axis-aligned plus sign of 1's contained in grid. If there is none, return 0.
