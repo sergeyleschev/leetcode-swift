e3f49d9ddda631cca72e65034a5dacc966300efa
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:11:32 +0300

    780. Reaching Points

--- 0780. Reaching Points.swift
+++ 0780. Reaching Points.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 780. Reaching Points
     // Given four integers sx, sy, tx, and ty, return true if it is possible to convert the point (sx, sy) to the point (tx, ty) through some operations, or false otherwise.
     // The allowed operation on some point (x, y) is to convert it to either (x, x + y) or (x + y, y).
