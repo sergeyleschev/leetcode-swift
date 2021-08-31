d4c42c1ff5b4958a0c61c4809a3dfecb4c56e8bf
Sergey Leschev <sergey.leschev@gmail.com>
2021-08-31 08:11:23 +0300

    779. K-th Symbol in Grammar

--- 0779. K-th Symbol in Grammar.swift
+++ 0779. K-th Symbol in Grammar.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 779. K-th Symbol in Grammar
     // We build a table of n rows (1-indexed). We start by writing 0 in the 1st row. Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
     // For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 0110.
