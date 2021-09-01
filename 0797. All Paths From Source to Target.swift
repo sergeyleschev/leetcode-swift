e03b764f3663b91d4bd7fa504cd8571d2cb2b187
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 07:56:31 +0300

    797. All Paths From Source to Target

--- 0797. All Paths From Source to Target.swift
+++ 0797. All Paths From Source to Target.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 797. All Paths From Source to Target
     // Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1, and return them in any order.
     // The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
