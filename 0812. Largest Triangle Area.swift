dddbcde1c68ad9d0bb03939f89ef9e97f03a7bf5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 08:00:41 +0300

    812. Largest Triangle Area

--- 0812. Largest Triangle Area.swift
+++ 0812. Largest Triangle Area.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 812. Largest Triangle Area
     // You have a list of points in the plane. Return the area of the largest triangle that can be formed by any 3 of the points.
 
