0bfa3e59c4a2f9f273659c151a6a6f43f3f60889
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 07:53:03 +0300

    793. Preimage Size of Factorial Zeroes Function

--- 0793. Preimage Size of Factorial Zeroes Function.swift
+++ 0793. Preimage Size of Factorial Zeroes Function.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 793. Preimage Size of Factorial Zeroes Function
     // Let f(x) be the number of zeroes at the end of x!. (Recall that x! = 1 * 2 * 3 * ... * x, and by convention, 0! = 1.)
     // For example, f(3) = 0 because 3! = 6 has no zeroes at the end, while f(11) = 2 because 11! = 39916800 has 2 zeroes at the end. Given k, find how many non-negative integers x have the property that f(x) = k.
