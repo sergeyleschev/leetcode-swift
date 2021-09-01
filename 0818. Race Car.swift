8bab57b6a947cdcdbf1e60c769539ab8007e1932
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 08:03:02 +0300

    818. Race Car

--- 0818. Race Car.swift
+++ 0818. Race Car.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 818. Race Car
     // Your car starts at position 0 and speed +1 on an infinite number line. Your car can go into negative positions. Your car drives automatically according to a sequence of instructions 'A' (accelerate) and 'R' (reverse):
     // When you get an instruction 'A', your car does the following:
