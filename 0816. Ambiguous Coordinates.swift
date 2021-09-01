265636f80e062c45408f27731b2d02401f0d135f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 08:01:48 +0300

    816. Ambiguous Coordinates

--- 0816. Ambiguous Coordinates.swift
+++ 0816. Ambiguous Coordinates.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 816. Ambiguous Coordinates
     // We had some 2-dimensional coordinates, like "(1, 3)" or "(2, 0.5)". Then, we removed all commas, decimal points, and spaces and ended up with the string s.
     // For example, "(1, 3)" becomes s = "(13)" and "(2, 0.5)" becomes s = "(205)".
