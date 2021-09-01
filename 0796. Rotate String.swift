df38317ecaf3b95425cc96fb990d067546845772
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 07:56:20 +0300

    796. Rotate String

--- 0796. Rotate String.swift
+++ 0796. Rotate String.swift
@@ -1,5 +1,8 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
+    // 796. Rotate String
     // We are given two strings, s and goal.
     // A shift on s consists of taking string s and moving the leftmost character to the rightmost position. For example, if s = 'abcde', then it will be 'bcdea' after one shift on s. Return true if and only if s can become goal after some number of shifts on s.
 
