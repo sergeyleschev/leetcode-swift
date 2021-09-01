9aa0e619a3cb544bdad3a32444099b9bce1b6e02
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 07:51:59 +0300

    788. Rotated Digits

--- 0788. Rotated Digits.swift
+++ 0788. Rotated Digits.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 788. Rotated Digits
     // x is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from x. Each digit must be rotated - we cannot choose to leave it alone.
     // A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other (on this case they are rotated in a different direction, in other words 2 or 5 gets mirrored); 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
