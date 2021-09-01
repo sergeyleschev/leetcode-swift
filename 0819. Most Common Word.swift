41eec2b8a12bfada891dbb65cc9f623fd0de2dfd
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 08:04:50 +0300

    819. Most Common Word

--- 0819. Most Common Word.swift
+++ 0819. Most Common Word.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 819. Most Common Word
     // Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
     // The words in paragraph are case-insensitive and the answer should be returned in lowercase.
