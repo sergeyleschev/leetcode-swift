fdcb802c306c555aaf08e8851c15b42542c319bb
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 08:00:32 +0300

    811. Subdomain Visit Count

--- 0811. Subdomain Visit Count.swift
+++ 0811. Subdomain Visit Count.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 811. Subdomain Visit Count
     // A website domain like "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next level, we have "leetcode.com", and at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com", we will also visit the parent domains "leetcode.com" and "com" implicitly.
     // Now, call a "count-paired domain" to be a count (representing the number of visits this domain received), followed by a space, followed by the address. An example of a count-paired domain might be "9001 discuss.leetcode.com".
