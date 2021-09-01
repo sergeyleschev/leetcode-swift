924dc89f85e595b372ac9d2ec952374b2f2ed2aa
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 07:56:40 +0300

    799. Champagne Tower

--- 0799. Champagne Tower.swift
+++ 0799. Champagne Tower.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 799. Champagne Tower
     // We stack glasses in a pyramid, where the first row has 1 glass, the second row has 2 glasses, and so on until the 100th row.  Each glass holds one cup of champagne.
     // Then, some champagne is poured into the first glass at the top.  When the topmost glass is full, any excess liquid poured will fall equally to the glass immediately to the left and right of it.  When those glasses become full, any excess champagne will fall equally to the left and right of those glasses, and so on.  (A glass at the bottom row has its excess champagne fall on the floor.)
