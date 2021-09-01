260d6e123952bcc8ed81a0f2c296b70f5a2bf39f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 08:01:00 +0300

    814. Binary Tree Pruning

--- 0814. Binary Tree Pruning.swift
+++ 0814. Binary Tree Pruning.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 814. Binary Tree Pruning
     // Given the root of a binary tree, return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
     // A subtree of a node node is node plus every node that is a descendant of node.
