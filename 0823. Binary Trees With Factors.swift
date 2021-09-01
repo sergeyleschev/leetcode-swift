0635900379bbf024f52cf09a3d37d4ffc79915ca
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 08:05:34 +0300

    823. Binary Trees With Factors

--- 0823. Binary Trees With Factors.swift
+++ 0823. Binary Trees With Factors.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 823. Binary Trees With Factors
     // Given an array of unique integers, arr, where each integer arr[i] is strictly greater than 1.
     // We make a binary tree using these integers, and each number may be used for any number of times. Each non-leaf node's value should be equal to the product of the values of its children.
