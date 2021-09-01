03a090d56629541739f148eb08dc8fc5fba799b1
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 07:58:29 +0300

    804. Unique Morse Code Words

--- 0804. Unique Morse Code Words.swift
+++ 0804. Unique Morse Code Words.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 804. Unique Morse Code Words
     // International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
     // For convenience, the full table for the 26 letters of the English alphabet is given below:
