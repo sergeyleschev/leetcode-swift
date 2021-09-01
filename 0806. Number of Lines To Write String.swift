9cc93d448f9a7584a238939e2d2224b26d341fc7
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-01 07:58:48 +0300

    806. Number of Lines To Write String

--- 0806. Number of Lines To Write String.swift
+++ 0806. Number of Lines To Write String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 806. Number of Lines To Write String
     // You are given a string s of lowercase English letters and an array widths denoting how many pixels wide each lowercase English letter is. Specifically, widths[0] is the width of 'a', widths[1] is the width of 'b', and so on.
     // You are trying to write s across several lines, where each line is no longer than 100 pixels. Starting at the beginning of s, write as many letters on the first line such that the total width does not exceed 100 pixels. Then, from where you stopped in s, continue writing as many letters as you can on the second line. Continue this process until you have written all of s.
