c07ccc58978b49d02d4f36dbd90412fe1211753b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:57:17 +0300

    833. Find And Replace in String

--- 0833. Find And Replace in String.swift
+++ 0833. Find And Replace in String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 833. Find And Replace in String
     // You are given a 0-indexed string s that you must perform k replacement operations on. The replacement operations are given as three 0-indexed parallel arrays, indices, sources, and targets, all of length k.
     // To complete the ith replacement operation:
