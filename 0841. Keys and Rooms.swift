773cde24ec78e2d3a3afcf994e01da8d0fe720af
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:58:46 +0300

    841. Keys and Rooms

--- 0841. Keys and Rooms.swift
+++ 0841. Keys and Rooms.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 841. Keys and Rooms
     // There are N rooms and you start in room 0.  Each room has a distinct number in 0, 1, 2, ..., N-1, and each room may have some keys to access the next room. 
     // Formally, each room i has a list of keys rooms[i], and each key rooms[i][j] is an integer in [0, 1, ..., N-1] where N = rooms.length.  A key rooms[i][j] = v opens the room with number v.
