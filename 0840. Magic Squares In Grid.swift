e486f9bd0497a1f5409154f74e76bad4bc85f0ea
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:58:37 +0300

    840. Magic Squares In Grid

--- 0840. Magic Squares In Grid.swift
+++ 0840. Magic Squares In Grid.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 840. Magic Squares In Grid
     // A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9 such that each row, column, and both diagonals all have the same sum.
     // Given a row x col grid of integers, how many 3 x 3 "magic square" subgrids are there?  (Each subgrid is contiguous).
