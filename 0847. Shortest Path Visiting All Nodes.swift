1a175dfb6f051497077e697e0ae745958577104f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:59:58 +0300

    847. Shortest Path Visiting All Nodes

--- 0847. Shortest Path Visiting All Nodes.swift
+++ 0847. Shortest Path Visiting All Nodes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 847. Shortest Path Visiting All Nodes
     // You have an undirected, connected graph of n nodes labeled from 0 to n - 1. You are given an array graph where graph[i] is a list of all the nodes connected with node i by an edge.
     // Return the length of the shortest path that visits every node. You may start and stop at any node, you may revisit nodes multiple times, and you may reuse edges.
