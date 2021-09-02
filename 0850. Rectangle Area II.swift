ac269647af8a54b41e57dba8942dd80e063c8262
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 08:00:40 +0300

    850. Rectangle Area II

--- 0850. Rectangle Area II.swift
+++ 0850. Rectangle Area II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 850. Rectangle Area II
     // We are given a list of (axis-aligned) rectangles. Each rectangle[i] = [xi1, yi1, xi2, yi2] , where (xi1, yi1) are the coordinates of the bottom-left corner, and (xi2, yi2) are the coordinates of the top-right corner of the ith rectangle.
     // Find the total area covered by all rectangles in the plane. Since the answer may be too large, return it modulo 109 + 7.
