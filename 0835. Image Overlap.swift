d246ecb7038489287e27a2ef40430069a07742d1
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:57:37 +0300

    835. Image Overlap

--- 0835. Image Overlap.swift
+++ 0835. Image Overlap.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 835. Image Overlap
     // You are given two images img1 and img2 both of size n x n, represented as binary, square matrices of the same size. (A binary matrix has only 0s and 1s as values.)
     // We translate one image however we choose (sliding it left, right, up, or down any number of units), and place it on top of the other image.  After, the overlap of this translation is the number of positions that have a 1 in both images.
