a61c2ff5204bba019e0d8c5e0ccb69db8932eb53
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:53:44 +0300

    828. Count Unique Characters of All Substrings of a Given String

--- 0828. Count Unique Characters of All Substrings of a Given String.swift
+++ 0828. Count Unique Characters of All Substrings of a Given String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 828. Count Unique Characters of All Substrings of a Given String
     // Let's define a function countUniqueChars(s) that returns the number of unique characters on s, for example if s = "LEETCODE" then "L", "T","C","O","D" are the unique characters since they appear only once in s, therefore countUniqueChars(s) = 5.
     // On this problem given a string s we need to return the sum of countUniqueChars(t) where t is a substring of s. Notice that some substrings can be repeated so on this case you have to count the repeated ones too.
