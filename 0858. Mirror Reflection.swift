34e294cb37744a54f2afcc4d531f7bb8912e4d3e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 08:02:32 +0300

    858. Mirror Reflection

--- 0858. Mirror Reflection.swift
+++ 0858. Mirror Reflection.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 858. Mirror Reflection
     // There is a special square room with mirrors on each of the four walls. Except for the southwest corner, there are receptors on each of the remaining corners, numbered 0, 1, and 2.
     // The square room has walls of length p, and a laser ray from the southwest corner first meets the east wall at a distance q from the 0th receptor.
