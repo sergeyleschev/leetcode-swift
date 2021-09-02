66449036c9b59cd4f2d6b1c1fe1ac612e5eeec8d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:58:04 +0300

    836. Rectangle Overlap

--- 0836. Rectangle Overlap.swift
+++ 0836. Rectangle Overlap.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 836. Rectangle Overlap
     // An axis-aligned rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) is the coordinate of its bottom-left corner, and (x2, y2) is the coordinate of its top-right corner. Its top and bottom edges are parallel to the X-axis, and its left and right edges are parallel to the Y-axis.
     // Two rectangles overlap if the area of their intersection is positive. To be clear, two rectangles that only touch at the corner or edges do not overlap.
