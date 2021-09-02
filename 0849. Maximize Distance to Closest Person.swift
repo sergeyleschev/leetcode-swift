d28dbd4bfc56105a2689692c0d92067ce87b994f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 08:00:25 +0300

    849. Maximize Distance to Closest Person

--- 0849. Maximize Distance to Closest Person.swift
+++ 0849. Maximize Distance to Closest Person.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 849. Maximize Distance to Closest Person
     // You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 represents that the ith seat is empty (0-indexed).
     // There is at least one empty seat, and at least one person sitting.
