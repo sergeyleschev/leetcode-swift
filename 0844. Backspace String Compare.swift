d0c6c95d26befbe28bcbe821e0a49824971d33fc
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:59:20 +0300

    844. Backspace String Compare

--- 0844. Backspace String Compare.swift
+++ 0844. Backspace String Compare.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 844. Backspace String Compare
     // Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
     // Note that after backspacing an empty text, the text will continue empty.
