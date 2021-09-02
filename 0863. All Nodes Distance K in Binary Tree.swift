2ce01bd6f07b1edc87f6d6fb99f24a275556e823
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 22:53:32 +0300

    863. All Nodes Distance K in Binary Tree

--- 0863. All Nodes Distance K in Binary Tree.swift
+++ 0863. All Nodes Distance K in Binary Tree.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 863. All Nodes Distance K in Binary Tree
     // We are given a binary tree (with root node root), a target node, and an integer value k.
     // Return a list of the values of all nodes that have a distance k from the target node.  The answer can be returned in any order.
