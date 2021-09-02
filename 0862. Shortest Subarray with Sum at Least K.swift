2d2e828fc111db0748dc5377ec21596fc6b788ed
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 22:53:23 +0300

    862. Shortest Subarray with Sum at Least K

--- 0862. Shortest Subarray with Sum at Least K.swift
+++ 0862. Shortest Subarray with Sum at Least K.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 862. Shortest Subarray with Sum at Least K
     // Return the length of the shortest, non-empty, contiguous subarray of nums with sum at least k.
     // If there is no non-empty subarray with sum at least k, return -1.
