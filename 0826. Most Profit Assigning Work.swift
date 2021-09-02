909aee2de36dc359fb56a4f16efe998727dcbe28
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-02 07:53:22 +0300

    826. Most Profit Assigning Work

--- 0826. Most Profit Assigning Work.swift
+++ 0826. Most Profit Assigning Work.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 826. Most Profit Assigning Work
     // We have jobs: difficulty[i] is the difficulty of the ith job, and profit[i] is the profit of the ith job. 
     // Now we have some workers. worker[i] is the ability of the ith worker, which means that this worker can only complete a job with difficulty at most worker[i]. 
