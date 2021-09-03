dadba345104b071167fde66bf1e399ad033774b0
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:57:09 +0300

    868. Binary Gap

--- 0868. Binary Gap.swift
+++ 0868. Binary Gap.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 868. Binary Gap
     // Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0.
     // Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
