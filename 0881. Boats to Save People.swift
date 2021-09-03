fff591e21731d58f629df826c12dd5a8763105db
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:59:34 +0300

    881. Boats to Save People

--- 0881. Boats to Save People.swift
+++ 0881. Boats to Save People.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 881. Boats to Save People
     // You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
     // Return the minimum number of boats to carry every given person.
