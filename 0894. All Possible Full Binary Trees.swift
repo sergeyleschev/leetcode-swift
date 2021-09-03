19c9fb23aa631eda6f479bdcc87b93de2db02ffe
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 08:01:47 +0300

    894. All Possible Full Binary Trees

--- 0894. All Possible Full Binary Trees.swift
+++ 0894. All Possible Full Binary Trees.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 894. All Possible Full Binary Trees
     // Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
     // Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
