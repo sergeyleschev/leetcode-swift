652c9a4894912845a553de1752ef558496b8a0cb
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 08:00:35 +0300

    888. Fair Candy Swap

--- 0888. Fair Candy Swap.swift
+++ 0888. Fair Candy Swap.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 888. Fair Candy Swap
     // Alice and Bob have candy bars of different sizes: aliceSizes[i] is the size of the i-th bar of candy that Alice has, and bobSizes[j] is the size of the j-th bar of candy that Bob has.
     // Since they are friends, they would like to exchange one candy bar each so that after the exchange, they both have the same total amount of candy.  (The total amount of candy a person has is the sum of the sizes of candy bars they have.)
