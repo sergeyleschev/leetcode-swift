18381c4220d6ffcc92172fe93a83a0b3ea9693f6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:58:39 +0300

    876. Middle of the Linked List

--- 0876. Middle of the Linked List.swift
+++ 0876. Middle of the Linked List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 876. Middle of the Linked List
     // Given a non-empty, singly linked list with head node head, return a middle node of linked list.
     // If there are two middle nodes, return the second middle node.
