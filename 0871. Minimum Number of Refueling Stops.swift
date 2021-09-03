24b8b18e9f92639dba7a6b49027b875107712aaf
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:57:38 +0300

    871. Minimum Number of Refueling Stops

--- 0871. Minimum Number of Refueling Stops.swift
+++ 0871. Minimum Number of Refueling Stops.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 871. Minimum Number of Refueling Stops
     // A car travels from a starting position to a destination which is target miles east of the starting position.
     // There are gas stations along the way. The gas stations are represented as an array stations where stations[i] = [positioni, fueli] indicates that the ith gas station is positioni miles east of the starting position and has fueli liters of gas.
