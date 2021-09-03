6978f33e07589d42cbd90eb5e4cdb66f47282999
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 08:00:54 +0300

    890. Find and Replace Pattern

--- 0890. Find and Replace Pattern.swift
+++ 0890. Find and Replace Pattern.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 890. Find and Replace Pattern
     // Given a list of strings words and a string pattern, return a list of words[i] that match pattern. You may return the answer in any order.
     // A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we get the desired word.
