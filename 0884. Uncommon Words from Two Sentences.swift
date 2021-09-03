a096fad4a6570ad0a9404dfb3502ba48cdea949a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:59:52 +0300

    884. Uncommon Words from Two Sentences

--- 0884. Uncommon Words from Two Sentences.swift
+++ 0884. Uncommon Words from Two Sentences.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 884. Uncommon Words from Two Sentences
     // A sentence is a string of single-space separated words where each word consists only of lowercase letters.
     // A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
