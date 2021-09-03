a8cc48ad75bd0f8dae70f3676ddf1a84d3e84c0a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 08:00:26 +0300

    887. Super Egg Drop

--- 0887. Super Egg Drop.swift
+++ 0887. Super Egg Drop.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 887. Super Egg Drop
     // You are given k identical eggs and you have access to a building with n floors labeled from 1 to n.
     // You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or below floor f will not break.
