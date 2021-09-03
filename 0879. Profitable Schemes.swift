1af25787e302e120858dc3744202721ac1db11ee
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:59:06 +0300

    879. Profitable Schemes

--- 0879. Profitable Schemes.swift
+++ 0879. Profitable Schemes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 879. Profitable Schemes
     // There is a group of n members, and a list of various crimes they could commit. The ith crime generates a profit[i] and requires group[i] members to participate in it. If a member participates in one crime, that member can't participate in another crime.
     // Let's call a profitable scheme any subset of these crimes that generates at least minProfit profit, and the total number of members participating in that subset of crimes is at most n.
