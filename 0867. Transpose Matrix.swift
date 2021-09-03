9f041bcac3c61d9d215a716ad83fa803117a4b08
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:56:59 +0300

    867. Transpose Matrix

--- 0867. Transpose Matrix.swift
+++ 0867. Transpose Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 867. Transpose Matrix
     // Given a 2D integer array matrix, return the transpose of matrix.
     // The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
