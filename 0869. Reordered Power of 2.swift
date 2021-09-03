afea3c2dcbd76cfee828f9392254d83b28a4e3d5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-03 07:57:18 +0300

    869. Reordered Power of 2

--- 0869. Reordered Power of 2.swift
+++ 0869. Reordered Power of 2.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 869. Reordered Power of 2
     // You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
     // Return true if and only if we can do this so that the resulting number is a power of two.
