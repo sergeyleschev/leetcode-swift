cee11f0d27a4a849b34c9392baaa033bfaf29058
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 23:49:34 +0300

    931. Minimum Falling Path Sum

--- 0931. Minimum Falling Path Sum.swift
+++ 0931. Minimum Falling Path Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 931. Minimum Falling Path Sum
     // Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
     // A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
