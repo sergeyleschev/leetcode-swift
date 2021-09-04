fca6740bf9294f81d1e014a96e246071ffc50252
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 10:11:44 +0300

    922. Sort Array By Parity II

--- 0922. Sort Array By Parity II.swift
+++ 0922. Sort Array By Parity II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 922. Sort Array By Parity II
     // Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
     // Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
