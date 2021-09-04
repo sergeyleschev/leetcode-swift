bfef8ff241dd955cc69e6091c59758d69fde9107
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 10:11:53 +0300

    923. 3Sum With Multiplicity

--- 0923. 3Sum With Multiplicity.swift
+++ 0923. 3Sum With Multiplicity.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 923. 3Sum With Multiplicity
     // Given an integer array arr, and an integer target, return the number of tuples i, j, k such that i < j < k and arr[i] + arr[j] + arr[k] == target.
     // As the answer can be very large, return it modulo 10^9 + 7.
