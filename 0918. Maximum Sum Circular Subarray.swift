a08e4bfdde6c75da1f777a2a864964b3f6b446d4
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 10:08:49 +0300

    918. Maximum Sum Circular Subarray

--- 0918. Maximum Sum Circular Subarray.swift
+++ 0918. Maximum Sum Circular Subarray.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 918. Maximum Sum Circular Subarray
     // Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
     // A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
