b35b9d0fbabe0fe5767745172fa5b90d23a5572b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 23:48:38 +0300

    925. Long Pressed Name

--- 0925. Long Pressed Name.swift
+++ 0925. Long Pressed Name.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 925. Long Pressed Name
     // Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
     // You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
