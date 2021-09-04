4cd428657df891ac6cc9e64f15e2c99eb4fa7f58
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 10:06:34 +0300

    907. Sum of Subarray Minimums

--- 0907. Sum of Subarray Minimums.swift
+++ 0907. Sum of Subarray Minimums.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 907. Sum of Subarray Minimums
     // Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 10^9 + 7.
 
