a5dcedb34442926ec8c09a03b64889c0b1c68cfc
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 10:05:36 +0300

    901. Online Stock Span

--- 0901. Online Stock Span.swift
+++ 0901. Online Stock Span.swift
@@ -1,5 +1,7 @@
 class StockSpanner {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 901. Online Stock Span
     // Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
     // The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
