7d6f9a424c86e7b0596b9e68adc9e0e1e35003f2
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 23:48:50 +0300

    926. Flip String to Monotone Increasing

--- 0926. Flip String to Monotone Increasing.swift
+++ 0926. Flip String to Monotone Increasing.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 926. Flip String to Monotone Increasing
     // A string of '0's and '1's is monotone increasing if it consists of some number of '0's (possibly 0), followed by some number of '1's (also possibly 0.)
     // We are given a string s of '0's and '1's, and we may flip any '0' to a '1' or a '1' to a '0'.
