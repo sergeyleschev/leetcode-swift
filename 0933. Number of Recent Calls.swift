eb46c628cd7106f9997a2b0252a343491bf478da
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 23:50:04 +0300

    933. Number of Recent Calls

--- 0933. Number of Recent Calls.swift
+++ 0933. Number of Recent Calls.swift
@@ -1,5 +1,7 @@
 class RecentCounter {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 933. Number of Recent Calls
     // You have a RecentCounter class which counts the number of recent requests within a certain time frame.
     // Implement the RecentCounter class:
