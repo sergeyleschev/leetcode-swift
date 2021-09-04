739faf731b72c2052ae2523c1561f052cccaf4f6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-04 10:07:55 +0300

    914. X of a Kind in a Deck of Cards

--- 0914. X of a Kind in a Deck of Cards.swift
+++ 0914. X of a Kind in a Deck of Cards.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 914. X of a Kind in a Deck of Cards
     // In a deck of cards, each card has an integer written on it.
     // Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, where:
