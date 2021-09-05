57a815cd0ba8104947dcf6bab3f29f0aff62b75d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 08:59:32 +0300

    943. Find the Shortest Superstring

--- 0943. Find the Shortest Superstring.swift
+++ 0943. Find the Shortest Superstring.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 943. Find the Shortest Superstring
     // Given an array of strings words, return the smallest string that contains each string in words as a substring. If there are multiple valid strings of the smallest length, return any of them.
     // You may assume that no string in words is a substring of another string in words.
