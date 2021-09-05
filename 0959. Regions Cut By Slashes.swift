5854bdb6b8f81ecb6ae46a2c560a5ccaf357ac39
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 09:02:38 +0300

    959. Regions Cut By Slashes

--- 0959. Regions Cut By Slashes.swift
+++ 0959. Regions Cut By Slashes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 959. Regions Cut By Slashes
     // An n x n grid is composed of 1 x 1 squares where each 1 x 1 square consists of a '/', '\', or blank space ' '. These characters divide the square into contiguous regions.
     // Given the grid grid represented as a string array, return the number of regions.
