b6dcec08bc28e73e649cbb06aff2c4301b0c655b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 09:00:23 +0300

    947. Most Stones Removed with Same Row or Column

--- 0947. Most Stones Removed with Same Row or Column.swift
+++ 0947. Most Stones Removed with Same Row or Column.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 947. Most Stones Removed with Same Row or Column
     // On a 2D plane, we place n stones at some integer coordinate points. Each coordinate point may have at most one stone.
     // A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
