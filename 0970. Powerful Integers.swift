2d3763ee9d901a15b6a63736428627892e83397b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 22:41:23 +0300

    970. Powerful Integers

--- 0970. Powerful Integers.swift
+++ 0970. Powerful Integers.swift
@@ -1,5 +1,7 @@
 public class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 970. Powerful Integers
     // Given three integers x, y, and bound, return a list of all the powerful integers that have a value less than or equal to bound.
     // An integer is powerful if it can be represented as xi + yj for some integers i >= 0 and j >= 0.
