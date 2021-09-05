35886a1d0d3e16d2b4ab2e0f3742ca956e43229a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 08:58:35 +0300

    939. Minimum Area Rectangle

--- 0939. Minimum Area Rectangle.swift
+++ 0939. Minimum Area Rectangle.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 939. Minimum Area Rectangle
     // Given a set of points in the xy-plane, determine the minimum area of a rectangle formed from these points, with sides parallel to the x and y axes.
     // If there isn't any rectangle, return 0.
