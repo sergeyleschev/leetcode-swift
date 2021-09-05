3658634030d8a5931144a20af55540498d1dca48
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 08:58:25 +0300

    938. Range Sum of BST

--- 0938. Range Sum of BST.swift
+++ 0938. Range Sum of BST.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 938. Range Sum of BST
     // Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
 
