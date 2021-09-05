a4a9500ea88b32450ed3b1cff23d8786723ea942
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 22:39:59 +0300

    964. Least Operators to Express Number

--- 0964. Least Operators to Express Number.swift
+++ 0964. Least Operators to Express Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 964. Least Operators to Express Number
     // Given a single positive integer x, we will write an expression of the form x (op1) x (op2) x (op3) x ... where each operator op1, op2, etc. is either addition, subtraction, multiplication, or division (+, -, *, or /). For example, with x = 3, we might write 3 * 3 / 3 + 3 - 3 which is a value of 3.
     // When writing such an expression, we adhere to the following conventions:
