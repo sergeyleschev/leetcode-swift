0c29172c12b4148e0cc957819c3be60fa772b209
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 09:00:06 +0300

    946. Validate Stack Sequences

--- 0946. Validate Stack Sequences.swift
+++ 0946. Validate Stack Sequences.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 946. Validate Stack Sequences
     // Given two sequences pushed and popped with distinct values, return true if and only if this could have been the result of a sequence of push and pop operations on an initially empty stack.
 
