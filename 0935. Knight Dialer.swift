366da60f163c886bd2e1884e1ca5536e9edecf8a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 08:57:50 +0300

    935. Knight Dialer

--- 0935. Knight Dialer.swift
+++ 0935. Knight Dialer.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 935. Knight Dialer
     // The chess knight has a unique movement, it may move two squares vertically and one square horizontally, or two squares horizontally and one square vertically (with both forming the shape of an L). The possible movements of chess knight are shown in this diagaram:
     // A chess knight can move as indicated in the chess diagram below:
