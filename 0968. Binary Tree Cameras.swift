543bb702c2d0073cc2758cef7a0a6abfc2f721fa
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 22:40:56 +0300

    968. Binary Tree Cameras

--- 0968. Binary Tree Cameras.swift
+++ 0968. Binary Tree Cameras.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 968. Binary Tree Cameras
     // You are given the root of a binary tree. We install cameras on the tree nodes where each camera at a node can monitor its parent, itself, and its immediate children.
     // Return the minimum number of cameras needed to monitor all nodes of the tree.
