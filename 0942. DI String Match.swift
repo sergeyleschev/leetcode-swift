97dfdc4aff0e0eda921c23ca996ea39b204c44ef
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 08:59:21 +0300

    942. DI String Match

--- 0942. DI String Match.swift
+++ 0942. DI String Match.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 942. DI String Match
     // A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
     // s[i] == 'I' if perm[i] < perm[i + 1], and
