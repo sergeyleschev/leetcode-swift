420e013a4a4a85275ce472be5b4e47c9cb4adcba
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 09:01:45 +0300

    953. Verifying an Alien Dictionary

--- 0953. Verifying an Alien Dictionary.swift
+++ 0953. Verifying an Alien Dictionary.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 953. Verifying an Alien Dictionary
     // In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
     // Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
