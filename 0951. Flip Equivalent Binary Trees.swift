b19531867a0e924f85c2f3191fa0fd0d1ea3b92e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 09:01:27 +0300

    951. Flip Equivalent Binary Trees

--- 0951. Flip Equivalent Binary Trees.swift
+++ 0951. Flip Equivalent Binary Trees.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 951. Flip Equivalent Binary Trees
     // For a binary tree T, we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
     // A binary tree X is flip equivalent to a binary tree Y if and only if we can make X equal to Y after some number of flip operations.
