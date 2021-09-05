6600ab4fffa1b2cf44e015a4b59f0c862744da21
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 09:02:21 +0300

    958. Check Completeness of a Binary Tree

--- 0958. Check Completeness of a Binary Tree.swift
+++ 0958. Check Completeness of a Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 958. Check Completeness of a Binary Tree
     // Given the root of a binary tree, determine if it is a complete binary tree.
     // In a complete binary tree, every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
