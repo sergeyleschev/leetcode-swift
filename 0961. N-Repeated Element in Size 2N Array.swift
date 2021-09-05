ed859773111d05d8ded969571b91451dfdabb0ba
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 22:39:24 +0300

    961. N-Repeated Element in Size 2N Array

--- 0961. N-Repeated Element in Size 2N Array.swift
+++ 0961. N-Repeated Element in Size 2N Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 961. N-Repeated Element in Size 2N Array
     // In a array nums of size 2 * n, there are n + 1 unique elements, and exactly one of these elements is repeated n times.
     // Return the element repeated n times.
