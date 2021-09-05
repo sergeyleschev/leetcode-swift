40f0cc85d33979dd10c7d850f236265479af80ed
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-05 22:40:36 +0300

    967. Numbers With Same Consecutive Differences

--- 0967. Numbers With Same Consecutive Differences.swift
+++ 0967. Numbers With Same Consecutive Differences.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 967. Numbers With Same Consecutive Differences
     // Return all non-negative integers of length n such that the absolute difference between every two consecutive digits is k.
     // Note that every number in the answer must not have leading zeros. For example, 01 has one leading zero and is invalid.
