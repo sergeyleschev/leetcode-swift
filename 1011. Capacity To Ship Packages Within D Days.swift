b971fb1b90cabe4e94a19ab38a9968ff763e7c79
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 20:29:57 +0300

    1011. Capacity To Ship Packages Within D Days

--- 1011. Capacity To Ship Packages Within D Days.swift
+++ 1011. Capacity To Ship Packages Within D Days.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1011. Capacity To Ship Packages Within D Days
     // A conveyor belt has packages that must be shipped from one port to another within days days.
     // The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
