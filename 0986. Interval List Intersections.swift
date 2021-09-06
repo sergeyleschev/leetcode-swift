d4fe9abe7dd7988eff783d8e78612bafec79d007
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:44:41 +0300

    986. Interval List Intersections

--- 0986. Interval List Intersections.swift
+++ 0986. Interval List Intersections.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 986. Interval List Intersections
     // You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
     // Return the intersection of these two interval lists.
