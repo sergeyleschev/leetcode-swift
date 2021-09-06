6237ca9a14ea38ad0f862ad654d6aecac3067ac9
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:42:47 +0300

    976. Largest Perimeter Triangle

--- 0976. Largest Perimeter Triangle.swift
+++ 0976. Largest Perimeter Triangle.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 976. Largest Perimeter Triangle
     // Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
 
