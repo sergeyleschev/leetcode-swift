20681d06605bcc34399b5e281cc54c66d8aeb1ac
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:47:31 +0300

    1004. Max Consecutive Ones III

--- 1004. Max Consecutive Ones III.swift
+++ 1004. Max Consecutive Ones III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1004. Max Consecutive Ones III
     // Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
 
