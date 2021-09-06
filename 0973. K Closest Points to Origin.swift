89145552dc7d59d619d8fd58e953bc7817e46e16
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:42:23 +0300

    973. K Closest Points to Origin

--- 0973. K Closest Points to Origin.swift
+++ 0973. K Closest Points to Origin.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 973. K Closest Points to Origin
     // Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
     // The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
