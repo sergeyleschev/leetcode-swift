ef0b3724a4a80ed7353a835ebdaf8cc7e87418c3
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:45:57 +0300

    995. Minimum Number of K Consecutive Bit Flips

--- 0995. Minimum Number of K Consecutive Bit Flips.swift
+++ 0995. Minimum Number of K Consecutive Bit Flips.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 995. Minimum Number of K Consecutive Bit Flips
     // In an array nums containing only 0s and 1s, a k-bit flip consists of choosing a (contiguous) subarray of length k and simultaneously changing every 0 in the subarray to 1, and every 1 in the subarray to 0.
     // Return the minimum number of k-bit flips required so that there is no 0 in the array.  If it is not possible, return -1.
