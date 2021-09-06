8ee6a6a436d22b189cb8168795301a6fc5e0daab
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:45:17 +0300

    990. Satisfiability of Equality Equations

--- 0990. Satisfiability of Equality Equations.swift
+++ 0990. Satisfiability of Equality Equations.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 990. Satisfiability of Equality Equations
     // Given an array equations of strings that represent relationships between variables, each string equations[i] has length 4 and takes one of two different forms: "a==b" or "a!=b".  Here, a and b are lowercase letters (not necessarily different) that represent one-letter variable names.
     // Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
