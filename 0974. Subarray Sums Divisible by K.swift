87dc265df42ae2d85116bc1f3d0bbfa94d50638c
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:42:32 +0300

    974. Subarray Sums Divisible by K

--- 0974. Subarray Sums Divisible by K.swift
+++ 0974. Subarray Sums Divisible by K.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 974. Subarray Sums Divisible by K
     // Given an array nums of integers, return the number of (contiguous, non-empty) subarrays that have a sum divisible by k.
 
