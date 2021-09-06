38a2107ba81bf3f2ac29537825dea800026488b0
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:43:17 +0300

    979. Distribute Coins in Binary Tree

--- 0979. Distribute Coins in Binary Tree.swift
+++ 0979. Distribute Coins in Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 979. Distribute Coins in Binary Tree
     // You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
     // In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
