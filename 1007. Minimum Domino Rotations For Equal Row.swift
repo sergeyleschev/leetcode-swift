f5fe621fd76817e19e0faf948859ba2e039b13ba
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:48:28 +0300

    1007. Minimum Domino Rotations For Equal Row

--- 1007. Minimum Domino Rotations For Equal Row.swift
+++ 1007. Minimum Domino Rotations For Equal Row.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1007. Minimum Domino Rotations For Equal Row
     // In a row of dominoes, tops[i] and bottoms[i] represent the top and bottom halves of the ith domino. (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
     // We may rotate the ith domino, so that tops[i] and bottoms[i] swap values.
