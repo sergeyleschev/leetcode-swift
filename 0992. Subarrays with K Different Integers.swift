31fae2f632d0bf35a093426707462bbb6a81131e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:45:34 +0300

    992. Subarrays with K Different Integers

--- 0992. Subarrays with K Different Integers.swift
+++ 0992. Subarrays with K Different Integers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 992. Subarrays with K Different Integers
     // Given an array nums of positive integers, call a (contiguous, not necessarily distinct) subarray of nums good if the number of different integers in that subarray is exactly k.
     // (For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.)
