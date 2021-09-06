a7177539af9f61184340c0d7bb7e78c0248206c4
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:46:38 +0300

    999. Available Captures for Rook

--- 0999. Available Captures for Rook.swift
+++ 0999. Available Captures for Rook.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 999. Available Captures for Rook
     // On an 8 x 8 chessboard, there is exactly one white rook 'R' and some number of white bishops 'B', black pawns 'p', and empty squares '.'.
     // When the rook moves, it chooses one of four cardinal directions (north, east, south, or west), then moves in that direction until it chooses to stop, reaches the edge of the board, captures a black pawn, or is blocked by a white bishop. A rook is considered attacking a pawn if the rook can capture the pawn on the rook's turn. The number of available captures for the white rook is the number of pawns that the rook is attacking.
