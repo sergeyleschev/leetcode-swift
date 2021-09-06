a78bc7c79da98341e6e6105704a13ad02db278e3
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:48:45 +0300

    1009. Complement of Base 10 Integer

--- 1009. Complement of Base 10 Integer.swift
+++ 1009. Complement of Base 10 Integer.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1009. Complement of Base 10 Integer
     // Every non-negative integer n has a binary representation.  For example, 5 can be represented as "101" in binary, 11 as "1011" in binary, and so on.  Note that except for n = 0, there are no leading zeroes in any binary representation.
     // The complement of a binary representation is the number in binary you get when changing every 1 to a 0 and 0 to a 1.  For example, the complement of "101" in binary is "010" in binary.
