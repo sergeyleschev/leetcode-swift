73d35a5969b0a529e05ea2522bb4ea38f2babaef
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-06 08:48:54 +0300

    1010. Pairs of Songs With Total Durations Divisible by 60

--- 1010. Pairs of Songs With Total Durations Divisible by 60.swift
+++ 1010. Pairs of Songs With Total Durations Divisible by 60.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1010. Pairs of Songs With Total Durations Divisible by 60
     // You are given a list of songs where the ith song has a duration of time[i] seconds.
     // Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
