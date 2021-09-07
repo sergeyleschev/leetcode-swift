8dd2506d2f05eca2ed295221d25497ccdf2dc57e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:02:47 +0300

    1028. Recover a Tree From Preorder Traversal

--- 1028. Recover a Tree From Preorder Traversal.swift
+++ 1028. Recover a Tree From Preorder Traversal.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1028. Recover a Tree From Preorder Traversal
     // We run a preorder depth-first search (DFS) on the root of a binary tree.
     // At each node in this traversal, we output D dashes (where D is the depth of this node), then we output the value of this node.  If the depth of a node is D, the depth of its immediate child is D + 1.  The depth of the root node is 0.
