da2cb4dd7a7da2e443a86e46e024e21fbb109359
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 07:59:58 +0300

    1014. Best Sightseeing Pair

--- 1014. Best Sightseeing Pair.swift
+++ 1014. Best Sightseeing Pair.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1014. Best Sightseeing Pair
     // You are given an integer array values where values[i] represents the value of the ith sightseeing spot. Two sightseeing spots i and j have a distance j - i between them.
     // The score of a pair (i < j) of sightseeing spots is values[i] + values[j] + i - j: the sum of the values of the sightseeing spots, minus the distance between them.
