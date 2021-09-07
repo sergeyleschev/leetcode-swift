cf4eb21d594a09cb73c846310399d021455c14bf
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:06:44 +0300

    1048. Longest String Chain

--- 1048. Longest String Chain.swift
+++ 1048. Longest String Chain.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1048. Longest String Chain
     // You are given an array of words where each word consists of lowercase English letters.
     // wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without changing the order of the other characters to make it equal to wordB.
