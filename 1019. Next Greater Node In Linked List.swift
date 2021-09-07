dbb5c2e01f6e311d8cd315d0940fc71b26b646a3
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:01:07 +0300

    1019. Next Greater Node In Linked List

--- 1019. Next Greater Node In Linked List.swift
+++ 1019. Next Greater Node In Linked List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1019. Next Greater Node In Linked List
     // We are given a linked list with head as the first node.  Let's number the nodes in the list: node_1, node_2, node_3, ... etc.
     // Each node may have a next larger value: for node_i, next_larger(node_i) is the node_j.val such that j > i, node_j.val > node_i.val, and j is the smallest possible choice.  If such a j does not exist, the next larger value is 0.
