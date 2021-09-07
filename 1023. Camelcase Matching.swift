55c0c7ab58d497b7c0d700135962aba931492e8e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:01:49 +0300

    1023. Camelcase Matching

--- 1023. Camelcase Matching.swift
+++ 1023. Camelcase Matching.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1023. Camelcase Matching
     // A query word matches a given pattern if we can insert lowercase letters to the pattern word so that it equals the query. (We may insert each character at any position, and may insert 0 characters.)
     // Given a list of queries, and a pattern, return an answer list of booleans, where answer[i] is true if and only if queries[i] matches the pattern.
