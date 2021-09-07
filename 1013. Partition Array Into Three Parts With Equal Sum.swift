032c7247e656e0448965d0c8bcb0f5d6dde7fa43
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 07:59:47 +0300

    1013. Partition Array Into Three Parts With Equal Sum

--- 1013. Partition Array Into Three Parts With Equal Sum.swift
+++ 1013. Partition Array Into Three Parts With Equal Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1013. Partition Array Into Three Parts With Equal Sum
     // Given an array of integers arr, return true if we can partition the array into three non-empty parts with equal sums.
     // Formally, we can partition the array if we can find indexes i + 1 < j with (arr[0] + arr[1] + ... + arr[i] == arr[i + 1] + arr[i + 2] + ... + arr[j - 1] == arr[j] + arr[j + 1] + ... + arr[arr.length - 1])
