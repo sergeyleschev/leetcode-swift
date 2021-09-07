f3488cd6b642e96ed80eec61d735111a0cc5f980
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:04:47 +0300

    1038. Binary Search Tree to Greater Sum Tree

--- 1038. Binary Search Tree to Greater Sum Tree.swift
+++ 1038. Binary Search Tree to Greater Sum Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1038. Binary Search Tree to Greater Sum Tree
     // Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
     // As a reminder, a binary search tree is a tree that satisfies these constraints:
