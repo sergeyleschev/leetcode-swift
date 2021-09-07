945bc7b7deda24c1ee12b87f58a16f4d188aa015
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:06:11 +0300

    1046. Last Stone Weight

--- 1046. Last Stone Weight.swift
+++ 1046. Last Stone Weight.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1046. Last Stone Weight
     // We have a collection of stones, each stone has a positive integer weight.
     // Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
