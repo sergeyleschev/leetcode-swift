8be281c08a14b3d4244c11dc670dcd30593dd449
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:04:37 +0300

    1037. Valid Boomerang

--- 1037. Valid Boomerang.swift
+++ 1037. Valid Boomerang.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1037. Valid Boomerang
     // Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
     // A boomerang is a set of three points that are all distinct and not in a straight line.
