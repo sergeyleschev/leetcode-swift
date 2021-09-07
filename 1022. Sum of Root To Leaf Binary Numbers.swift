f94863c9707b7480cd27fecaf73c390194fdcd08
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:01:39 +0300

    1022. Sum of Root To Leaf Binary Numbers

--- 1022. Sum of Root To Leaf Binary Numbers.swift
+++ 1022. Sum of Root To Leaf Binary Numbers.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1022. Sum of Root To Leaf Binary Numbers
     // You are given the root of a binary tree where each node has a value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
     // For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
