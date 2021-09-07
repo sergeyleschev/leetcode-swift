b906d2b6d23f70aa61dd2e3cdb3c4b90afad1818
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:06:01 +0300

    1044. Longest Duplicate Substring

--- 1044. Longest Duplicate Substring.swift
+++ 1044. Longest Duplicate Substring.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1044. Longest Duplicate Substring
     // Given a string s, consider all duplicated substrings: (contiguous) substrings of s that occur 2 or more times. The occurrences may overlap.
     // Return any duplicated substring that has the longest possible length. If s does not have a duplicated substring, the answer is "".
