f8e934cf25c28caaf18962918da0b50fb3d7668c
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:04:28 +0300

    1036. Escape a Large Maze

--- 1036. Escape a Large Maze.swift
+++ 1036. Escape a Large Maze.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1036. Escape a Large Maze
     // There is a 1 million by 1 million grid on an XY-plane, and the coordinates of each grid square are (x, y).
     // We start at the source = [sx, sy] square and want to reach the target = [tx, ty] square. There is also an array of blocked squares, where each blocked[i] = [xi, yi] represents a blocked square with coordinates (xi, yi).
