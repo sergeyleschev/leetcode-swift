c404928fa1e0047d02f63273fa6a60dffffb7659
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:02:28 +0300

    1026. Maximum Difference Between Node and Ancestor

--- 1026. Maximum Difference Between Node and Ancestor.swift
+++ 1026. Maximum Difference Between Node and Ancestor.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1026. Maximum Difference Between Node and Ancestor
     // Given the root of a binary tree, find the maximum value V for which there exist different nodes A and B where V = |A.val - B.val| and A is an ancestor of B.
     // A node A is an ancestor of B if either: any child of A is equal to B, or any child of A is an ancestor of B.
