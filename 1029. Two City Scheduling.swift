be603ed6ab7b66d9b0eec81124db01cd618192e6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:03:07 +0300

    1029. Two City Scheduling

--- 1029. Two City Scheduling.swift
+++ 1029. Two City Scheduling.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1029. Two City Scheduling
     // A company is planning to interview 2n people. Given the array costs where costs[i] = [aCosti, bCosti], the cost of flying the ith person to city a is aCosti, and the cost of flying the ith person to city b is bCosti.
     // Return the minimum cost to fly every person to a city such that exactly n people arrive in each city.
