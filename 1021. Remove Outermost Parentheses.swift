b93cdf48786b7d800785e6fbaa9da73dad97eb76
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:01:29 +0300

    1021. Remove Outermost Parentheses

--- 1021. Remove Outermost Parentheses.swift
+++ 1021. Remove Outermost Parentheses.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1021. Remove Outermost Parentheses
     // A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
     // For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
