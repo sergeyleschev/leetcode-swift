d23d8a2c5c6f2f246eec751fd81b97f00e9631b8
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:06:36 +0300

    1047. Remove All Adjacent Duplicates In String

--- 1047. Remove All Adjacent Duplicates In String.swift
+++ 1047. Remove All Adjacent Duplicates In String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1047. Remove All Adjacent Duplicates In String
     // You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
     // We repeatedly make duplicate removals on s until we no longer can.
