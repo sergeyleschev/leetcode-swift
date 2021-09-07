f1d374d3ad068574b43f535b778b12f8ef26b7c5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-07 08:05:51 +0300

    1043. Partition Array for Maximum Sum

--- 1043. Partition Array for Maximum Sum.swift
+++ 1043. Partition Array for Maximum Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1043. Partition Array for Maximum Sum
     // Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. After partitioning, each subarray has their values changed to become the maximum value of that subarray.
     // Return the largest sum of the given array after partitioning. Test cases are generated so that the answer fits in a 32-bit integer.
