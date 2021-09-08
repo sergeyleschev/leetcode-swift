720ddc13570bfe3a6292fb1eda32fbea461dda9a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:49:22 +0300

    1104. Path In Zigzag Labelled Binary Tree

--- 1104. Path In Zigzag Labelled Binary Tree.swift
+++ 1104. Path In Zigzag Labelled Binary Tree.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1104. Path In Zigzag Labelled Binary Tree
     // In an infinite binary tree where every node has two children, the nodes are labelled in row order.
     // In the odd numbered rows (ie., the first, third, fifth,...), the labelling is left to right, while in the even numbered rows (second, fourth, sixth,...), the labelling is right to left.
