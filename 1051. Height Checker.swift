b150500fdffcb22c9760da83d304394db3ed4327
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:45:27 +0300

    1051. Height Checker

--- 1051. Height Checker.swift
+++ 1051. Height Checker.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1051. Height Checker
     // A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
     // You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
