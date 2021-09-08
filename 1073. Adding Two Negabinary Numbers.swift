578e7a6ebbe6537876833ddf41b7ce7c5edcc8ca
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:46:36 +0300

    1073. Adding Two Negabinary Numbers

--- 1073. Adding Two Negabinary Numbers.swift
+++ 1073. Adding Two Negabinary Numbers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1073. Adding Two Negabinary Numbers
     // Given two numbers arr1 and arr2 in base -2, return the result of adding them together.
     // Each number is given in array format:  as an array of 0s and 1s, from most significant bit to least significant bit.  For example, arr = [1,1,0,1] represents the number (-2)^3 + (-2)^2 + (-2)^0 = -3.  A number arr in array, format is also guaranteed to have no leading zeros: either arr == [0] or arr[0] == 1.
