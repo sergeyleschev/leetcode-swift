85a4917431a7e85187634b6e82efa7f2024a9c2b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:46:14 +0300

    1071. Greatest Common Divisor of Strings

--- 1071. Greatest Common Divisor of Strings.swift
+++ 1071. Greatest Common Divisor of Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1071. Greatest Common Divisor of Strings
     // For two strings s and t, we say "t divides s" if and only if s = t + ... + t  (t concatenated with itself 1 or more times)
     // Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
