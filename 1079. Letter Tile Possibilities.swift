8e4a1cbb461ba0eb3a843c287797ea0fcd1fdf6c
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:47:15 +0300

    1079. Letter Tile Possibilities

--- 1079. Letter Tile Possibilities.swift
+++ 1079. Letter Tile Possibilities.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1079. Letter Tile Possibilities
     // You have n  tiles, where each tile has one letter tiles[i] printed on it.
     // Return the number of possible non-empty sequences of letters you can make using the letters printed on those tiles.
