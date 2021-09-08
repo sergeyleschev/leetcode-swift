f4a3ae1ba7e7dcfbd117bc7216cc09cf15c96263
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:46:04 +0300

    1054. Distant Barcodes

--- 1054. Distant Barcodes.swift
+++ 1054. Distant Barcodes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1054. Distant Barcodes
     // In a warehouse, there is a row of barcodes, where the ith barcode is barcodes[i].
     // Rearrange the barcodes so that no two adjacent barcodes are equal. You may return any answer, and it is guaranteed an answer exists.
