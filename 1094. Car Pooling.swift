31f7380f92b31e6438f76bb23ad77516a2426da7
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:48:33 +0300

    1094. Car Pooling

--- 1094. Car Pooling.swift
+++ 1094. Car Pooling.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1094. Car Pooling
     // You are driving a vehicle that has capacity empty seats initially available for passengers.  The vehicle only drives east (ie. it cannot turn around and drive west.)
     // Given a list of trips, trip[i] = [num_passengers, start_location, end_location] contains information about the i-th trip: the number of passengers that must be picked up, and the locations to pick them up and drop them off.  The locations are given as the number of kilometers due east from your vehicle's initial location.
