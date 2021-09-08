64d8a560dc065bf93aa0f0f7593a7f4e298a15ac
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:49:58 +0300

    1108. Defanging an IP Address

--- 1108. Defanging an IP Address.swift
+++ 1108. Defanging an IP Address.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1108. Defanging an IP Address
     // Given a valid (IPv4) IP address, return a defanged version of that IP address.
     // A defanged IP address replaces every period "." with "[.]".
