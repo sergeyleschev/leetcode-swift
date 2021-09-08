2366f317571b7909bf49acd0b606755ddca5e929
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-08 05:51:58 +0300

    1122. Relative Sort Array

--- 1122. Relative Sort Array.swift
+++ 1122. Relative Sort Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1122. Relative Sort Array
     // Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
     // Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2.  Elements that don't appear in arr2 should be placed at the end of arr1 in ascending order.
