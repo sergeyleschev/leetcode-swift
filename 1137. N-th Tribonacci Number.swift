5c5eddacd7e097db3bccd207f681f87cf2d73264
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:02:40 +0300

    1137. N-th Tribonacci Number

--- 1137. N-th Tribonacci Number.swift
+++ 1137. N-th Tribonacci Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1137. N-th Tribonacci Number
     // The Tribonacci sequence Tn is defined as follows: 
     // T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
