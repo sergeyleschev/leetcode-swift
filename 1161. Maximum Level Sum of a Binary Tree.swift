56b5cf84ec64aec10187eaeb0a25bc0f2fff5c48
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:05:14 +0300

    1161. Maximum Level Sum of a Binary Tree

--- 1161. Maximum Level Sum of a Binary Tree.swift
+++ 1161. Maximum Level Sum of a Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1161. Maximum Level Sum of a Binary Tree
     // Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
     // Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
