45b22d02ab5408affc12602a6677fe2ca6f70e1e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:03:06 +0300

    1143. Longest Common Subsequence

--- 1143. Longest Common Subsequence.swift
+++ 1143. Longest Common Subsequence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1143. Longest Common Subsequence
     // Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
     // A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
