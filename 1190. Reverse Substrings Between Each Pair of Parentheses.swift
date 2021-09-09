c5e1f5e3c0acb11770ebfe5bde2eb9d4b6de56ec
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:07:38 +0300

    1190. Reverse Substrings Between Each Pair of Parentheses

--- 1190. Reverse Substrings Between Each Pair of Parentheses.swift
+++ 1190. Reverse Substrings Between Each Pair of Parentheses.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1190. Reverse Substrings Between Each Pair of Parentheses
     // You are given a string s that consists of lower case English letters and brackets. 
     // Reverse the strings in each pair of matching parentheses, starting from the innermost one.
