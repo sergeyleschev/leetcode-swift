f93b445b4e48a6f3641bf6b58d124b0324d7ee7d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:07:49 +0300

    1192. Critical Connections in a Network

--- 1192. Critical Connections in a Network.swift
+++ 1192. Critical Connections in a Network.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1192. Critical Connections in a Network
     // There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network where connections[i] = [ai, bi] represents a connection between servers ai and bi. Any server can reach other servers directly or indirectly through the network.
     // A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
