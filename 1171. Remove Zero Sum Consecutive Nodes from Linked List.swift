7d8a853a1c8a5766e960b7b2cbe9eeb044982fc0
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:06:00 +0300

    1171. Remove Zero Sum Consecutive Nodes from Linked List

--- 1171. Remove Zero Sum Consecutive Nodes from Linked List.swift
+++ 1171. Remove Zero Sum Consecutive Nodes from Linked List.swift
@@ -1,5 +1,7 @@
  class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1171. Remove Zero Sum Consecutive Nodes from Linked List
     // Given the head of a linked list, we repeatedly delete consecutive sequences of nodes that sum to 0 until there are no such sequences.
     // After doing so, return the head of the final linked list.  You may return any such answer.
