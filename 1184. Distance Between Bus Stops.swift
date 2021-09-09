a53e0811a1f474d8a0244a45547e8f018d3b46ae
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:07:03 +0300

    1184. Distance Between Bus Stops

--- 1184. Distance Between Bus Stops.swift
+++ 1184. Distance Between Bus Stops.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1184. Distance Between Bus Stops
     // A bus has n stops numbered from 0 to n - 1 that form a circle. We know the distance between all pairs of neighboring stops where distance[i] is the distance between the stops number i and (i + 1) % n.
     // The bus goes along both directions i.e. clockwise and counterclockwise.
