93197e5d771250d0268b51b63eaf834f4661f59d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:07:29 +0300

    1189. Maximum Number of Balloons

--- 1189. Maximum Number of Balloons.swift
+++ 1189. Maximum Number of Balloons.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1189. Maximum Number of Balloons
     // Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
     // You can use each character in text at most once. Return the maximum number of instances that can be formed.
