ff6e62681af21911664f955f049e0b35909c0a7e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:07:20 +0300

    1186. Maximum Subarray Sum with One Deletion

--- 1186. Maximum Subarray Sum with One Deletion.swift
+++ 1186. Maximum Subarray Sum with One Deletion.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1186. Maximum Subarray Sum with One Deletion
     // Given an array of integers, return the maximum sum for a non-empty subarray (contiguous elements) with at most one element deletion. In other words, you want to choose a subarray and optionally delete one element from it so that there is still at least one element left and the sum of the remaining elements is maximum possible.
     // Note that the subarray needs to be non-empty after deleting one element.
