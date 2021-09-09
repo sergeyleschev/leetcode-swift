09811202680d54d97715f8037e1dd099319a9d54
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-09 08:05:23 +0300

    1162. As Far from Land as Possible

--- 1162. As Far from Land as Possible.swift
+++ 1162. As Far from Land as Possible.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1162. As Far from Land as Possible
     // Given an n x n grid containing only values 0 and 1, where 0 represents water and 1 represents land, find a water cell such that its distance to the nearest land cell is maximized, and return the distance. If no land or water exists in the grid, return -1.
     // The distance used in this problem is the Manhattan distance: the distance between two cells (x0, y0) and (x1, y1) is |x0 - x1| + |y0 - y1|.
