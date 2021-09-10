e1ef0633e92a67245edc41874aad47df46b2cbd2
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:31:43 +0300

    1218. Longest Arithmetic Subsequence of Given Difference

--- 1218. Longest Arithmetic Subsequence of Given Difference.swift
+++ 1218. Longest Arithmetic Subsequence of Given Difference.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1218. Longest Arithmetic Subsequence of Given Difference
     // Given an integer array arr and an integer difference, return the length of the longest subsequence in arr which is an arithmetic sequence such that the difference between adjacent elements in the subsequence equals difference.
     // A subsequence is a sequence that can be derived from arr by deleting some or no elements without changing the order of the remaining elements.
