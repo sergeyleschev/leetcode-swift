583615ed0c7463a7e4e0bae64be533982e86061c
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:34:10 +0300

    1235. Maximum Profit in Job Scheduling

--- 1235. Maximum Profit in Job Scheduling.swift
+++ 1235. Maximum Profit in Job Scheduling.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1235. Maximum Profit in Job Scheduling
     // We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
     // You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
