78afabecdb26eb41250821f01d2dc3117beecc1b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:34:37 +0300

    1239. Maximum Length of a Concatenated String with Unique Characters

--- 1239. Maximum Length of a Concatenated String with Unique Characters.swift
+++ 1239. Maximum Length of a Concatenated String with Unique Characters.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1239. Maximum Length of a Concatenated String with Unique Characters
     // Given an array of strings arr. String s is a concatenation of a sub-sequence of arr which have unique characters.
     // Return the maximum possible length of s.
