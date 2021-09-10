91118819e1f9f5c45eed32b09b4da3ab0287de08
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:37:12 +0300

    1268. Search Suggestions System

--- 1268. Search Suggestions System.swift
+++ 1268. Search Suggestions System.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1268. Search Suggestions System
     // Given an array of strings products and a string searchWord. We want to design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with the searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
     // Return list of lists of the suggested products after each character of searchWord is typed. 
