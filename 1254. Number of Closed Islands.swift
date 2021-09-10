7e4150efe15bebaa62af59dd99ab954ce19877e0
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:35:48 +0300

    1254. Number of Closed Islands

--- 1254. Number of Closed Islands.swift
+++ 1254. Number of Closed Islands.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1254. Number of Closed Islands
     // Given a 2D grid consists of 0s (land) and 1s (water).  An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally (all left, top, right, bottom) surrounded by 1s.
     // Return the number of closed islands.
