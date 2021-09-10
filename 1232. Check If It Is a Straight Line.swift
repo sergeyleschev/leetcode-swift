4261a5088d8ad268ea79ac4ab179f62f6556c679
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:33:39 +0300

    1232. Check If It Is a Straight Line

--- 1232. Check If It Is a Straight Line.swift
+++ 1232. Check If It Is a Straight Line.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1232. Check If It Is a Straight Line
     // You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
 
