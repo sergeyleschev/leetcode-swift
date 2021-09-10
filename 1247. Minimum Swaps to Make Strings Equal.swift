61df6ca020f152fc0b994a0f3105c23fb562bcef
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:34:47 +0300

    1247. Minimum Swaps to Make Strings Equal

--- 1247. Minimum Swaps to Make Strings Equal.swift
+++ 1247. Minimum Swaps to Make Strings Equal.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1247. Minimum Swaps to Make Strings Equal
     // You are given two strings s1 and s2 of equal length consisting of letters "x" and "y" only. Your task is to make these two strings equal to each other. You can swap any two characters that belong to different strings, which means: swap s1[i] and s2[j].
     // Return the minimum number of swaps required to make s1 and s2 equal, or return -1 if it is impossible to do so.
