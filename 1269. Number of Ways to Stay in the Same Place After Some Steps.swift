7646468e11f864a986997bdb10c3685ded33ae4e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:37:21 +0300

    1269. Number of Ways to Stay in the Same Place After Some Steps

--- 1269. Number of Ways to Stay in the Same Place After Some Steps.swift
+++ 1269. Number of Ways to Stay in the Same Place After Some Steps.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1269. Number of Ways to Stay in the Same Place After Some Steps
     // You have a pointer at index 0 in an array of size arrLen. At each step, you can move 1 position to the left, 1 position to the right in the array, or stay in the same place (The pointer should not be placed outside the array at any time).
     // Given two integers steps and arrLen, return the number of ways such that your pointer still at index 0 after exactly steps steps. Since the answer may be too large, return it modulo 109 + 7.
