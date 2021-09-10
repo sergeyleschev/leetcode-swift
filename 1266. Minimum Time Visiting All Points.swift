3f2bacaae77857fab53f45a744bd8e525b337b03
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:36:53 +0300

    1266. Minimum Time Visiting All Points

--- 1266. Minimum Time Visiting All Points.swift
+++ 1266. Minimum Time Visiting All Points.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1266. Minimum Time Visiting All Points
     // On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
     // You can move according to these rules:
