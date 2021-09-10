05734fe340ff9a3e0956ca13d2ce5bdc1a57906b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:37:03 +0300

    1267. Count Servers that Communicate

--- 1267. Count Servers that Communicate.swift
+++ 1267. Count Servers that Communicate.swift
@@ -1,5 +1,7 @@
  class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1267. Count Servers that Communicate
     // You are given a map of a server center, represented as a m * n integer matrix grid, where 1 means that on that cell there is a server and 0 means that it is no server. Two servers are said to communicate if they are on the same row or on the same column.
     // Return the number of servers that communicate with any other server.
