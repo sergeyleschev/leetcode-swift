dbc43bf7565e1f508188ba8e25910f2bf1f02556
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:34:58 +0300

    1248. Count Number of Nice Subarrays

--- 1248. Count Number of Nice Subarrays.swift
+++ 1248. Count Number of Nice Subarrays.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1248. Count Number of Nice Subarrays
     // Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
     // Return the number of nice sub-arrays.
