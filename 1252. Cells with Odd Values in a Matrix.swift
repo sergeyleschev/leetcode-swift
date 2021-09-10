f9bb7d465915656cd8605d5c1edc6390a466c776
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:35:29 +0300

    1252. Cells with Odd Values in a Matrix

--- 1252. Cells with Odd Values in a Matrix.swift
+++ 1252. Cells with Odd Values in a Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 1252. Cells with Odd Values in a Matrix
 	// There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
 	// For each location indices[i], do both of the following:
