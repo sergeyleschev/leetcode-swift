7809265515d92fb3712682e98e68b8a1e494db09
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:36:36 +0300

    1262. Greatest Sum Divisible by Three

--- 1262. Greatest Sum Divisible by Three.swift
+++ 1262. Greatest Sum Divisible by Three.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1262. Greatest Sum Divisible by Three
     // Given an array nums of integers, we need to find the maximum possible sum of elements of the array such that it is divisible by three.
 
