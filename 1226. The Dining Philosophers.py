72a787bb23ca033097b2247a13d15a8d5f80db29
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:33:13 +0300

    1226. The Dining Philosophers

--- 1226. The Dining Philosophers.py
+++ 1226. The Dining Philosophers.py
@@ -1,3 +1,5 @@
+# Solution @ Sergey Leschev, Belarusian State University
+
 # 1226. The Dining Philosophers
 # Five silent philosophers sit at a round table with bowls of spaghetti. Forks are placed between each pair of adjacent philosophers.
 # Each philosopher must alternately think and eat. However, a philosopher can only eat spaghetti when they have both left and right forks. Each fork can be held by only one philosopher and so a philosopher can use the fork only if it is not being used by another philosopher. After an individual philosopher finishes eating, they need to put down both forks so that the forks become available to others. A philosopher can take the fork on their right or the one on their left as they become available, but cannot start eating before getting both forks.
