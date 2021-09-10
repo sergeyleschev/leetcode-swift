5146ed0bc076948909b1371fa65d90911b9a0f24
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:33:04 +0300

    1223. Dice Roll Simulation

--- 1223. Dice Roll Simulation.swift
+++ 1223. Dice Roll Simulation.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1223. Dice Roll Simulation
     // A die simulator generates a random number from 1 to 6 for each roll. You introduced a constraint to the generator such that it cannot roll the number i more than rollMax[i] (1-indexed) consecutive times. 
     // Given an array of integers rollMax and an integer n, return the number of distinct sequences that can be obtained with exact n rolls.
