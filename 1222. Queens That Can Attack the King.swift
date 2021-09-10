fb015e4de8d1bb92ce9910f21f58cff4472c9713
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-10 07:32:56 +0300

    1222. Queens That Can Attack the King

--- 1222. Queens That Can Attack the King.swift
+++ 1222. Queens That Can Attack the King.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1222. Queens That Can Attack the King
     // On an 8x8 chessboard, there can be multiple Black Queens and one White King.
     // Given an array of integer coordinates queens that represents the positions of the Black Queens, and a pair of coordinates king that represent the position of the White King, return the coordinates of all the queens (in any order) that can attack the King.
