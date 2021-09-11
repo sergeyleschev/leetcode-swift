7ca93bab2fed112c56a11308ad3170d3445c5c73
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:43:11 +0300

    1305. All Elements in Two Binary Search Trees

--- 1305. All Elements in Two Binary Search Trees.swift
+++ 1305. All Elements in Two Binary Search Trees.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1305. All Elements in Two Binary Search Trees
     // Given two binary search trees root1 and root2.
     // Return a list containing all the integers from both trees sorted in ascending order.
