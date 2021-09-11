8f98152f5623af6fbc222864aecee321b376ea9b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:37:36 +0300

    1283. Find the Smallest Divisor Given a Threshold

--- 1283. Find the Smallest Divisor Given a Threshold.swift
+++ 1283. Find the Smallest Divisor Given a Threshold.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1283. Find the Smallest Divisor Given a Threshold
     // Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, and sum the division's result. Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
     // Each result of the division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
