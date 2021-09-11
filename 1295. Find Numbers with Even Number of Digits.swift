47525b028df4a73df552c802ba2be1eec22846ec
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:39:42 +0300

    1295. Find Numbers with Even Number of Digits

--- 1295. Find Numbers with Even Number of Digits.swift
+++ 1295. Find Numbers with Even Number of Digits.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1295. Find Numbers with Even Number of Digits
     // Given an array nums of integers, return how many of them contain an even number of digits.
 
