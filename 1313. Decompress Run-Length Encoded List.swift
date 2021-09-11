99bcf93dde1378ee11f576132bcc990814c6fba5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:44:48 +0300

    1313. Decompress Run-Length Encoded List

--- 1313. Decompress Run-Length Encoded List.swift
+++ 1313. Decompress Run-Length Encoded List.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1313. Decompress Run-Length Encoded List
     // We are given a list nums of integers representing a list compressed with run-length encoding.
     // Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
