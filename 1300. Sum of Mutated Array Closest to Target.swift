cda67cdff602937df9042ac484907368f25fe621
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:42:29 +0300

    1300. Sum of Mutated Array Closest to Target

--- 1300. Sum of Mutated Array Closest to Target.swift
+++ 1300. Sum of Mutated Array Closest to Target.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1300. Sum of Mutated Array Closest to Target
     // Given an integer array arr and a target value target, return the integer value such that when we change all the integers larger than value in the given array to be equal to value, the sum of the array gets as close as possible (in absolute difference) to target.
     // In case of a tie, return the minimum such integer.
