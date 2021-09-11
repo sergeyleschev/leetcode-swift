0b10c64858a665e8f070ae5dd3581f2cfdecf29b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:43:23 +0300

    1306. Jump Game III

--- 1306. Jump Game III.swift
+++ 1306. Jump Game III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1306. Jump Game III
     // Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
     // Notice that you can not jump outside of the array at any time.
