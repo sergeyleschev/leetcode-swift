92ac3e1cba73b1662a00fd75304f0fa52f582163
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:40:37 +0300

    1299. Replace Elements with Greatest Element on Right Side

--- 1299. Replace Elements with Greatest Element on Right Side.swift
+++ 1299. Replace Elements with Greatest Element on Right Side.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1299. Replace Elements with Greatest Element on Right Side
     // Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
     // After doing so, return the array.
