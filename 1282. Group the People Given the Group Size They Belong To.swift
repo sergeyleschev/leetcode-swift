869ddb224e3efb8603c0e5ca3f7c28b12577bc64
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:36:27 +0300

    1282. Group the People Given the Group Size They Belong To

--- 1282. Group the People Given the Group Size They Belong To.swift
+++ 1282. Group the People Given the Group Size They Belong To.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1282. Group the People Given the Group Size They Belong To
     // There are n people that are split into some unknown number of groups. Each person is labeled with a unique ID from 0 to n - 1.
     // You are given an integer array groupSizes, where groupSizes[i] is the size of the group that person i is in. For example, if groupSizes[1] = 3, then person 1 must be in a group of size 3.
