63c3cb302a08fb60ad1e317d2cfe36291597202b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 22:50:21 +0300

    1323. Maximum 69 Number

--- 1323. Maximum 69 Number.swift
+++ 1323. Maximum 69 Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1323. Maximum 69 Number
     // Given a positive integer num consisting only of digits 6 and 9.
     // Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
