5e77a03233d9129af99d3936672a89350f9e5cd0
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 22:50:04 +0300

    1320. Minimum Distance to Type a Word Using Two Fingers

--- 1320. Minimum Distance to Type a Word Using Two Fingers.swift
+++ 1320. Minimum Distance to Type a Word Using Two Fingers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1320. Minimum Distance to Type a Word Using Two Fingers
     // You have a keyboard layout as shown above in the XY plane, where each English uppercase letter is located at some coordinate, for example, the letter A is located at coordinate (0,0), the letter B is located at coordinate (0,1), the letter P is located at coordinate (2,3) and the letter Z is located at coordinate (4,1).
     // Given the string word, return the minimum total distance to type such string using only two fingers. The distance between coordinates (x1,y1) and (x2,y2) is |x1 - x2| + |y1 - y2|. 
