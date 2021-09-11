0948b0e449fa2d801dee446cb8528a22c5288f27
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:38:35 +0300

    1290. Convert Binary Number in a Linked List to Integer

--- 1290. Convert Binary Number in a Linked List to Integer.swift
+++ 1290. Convert Binary Number in a Linked List to Integer.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1290. Convert Binary Number in a Linked List to Integer
     // Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
     // Return the decimal value of the number in the linked list.
