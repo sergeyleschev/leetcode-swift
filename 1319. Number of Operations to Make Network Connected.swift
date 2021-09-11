b6c6e74d98d8b9b5c4568956377daae1bb209bbf
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 22:49:53 +0300

    1319. Number of Operations to Make Network Connected

--- 1319. Number of Operations to Make Network Connected.swift
+++ 1319. Number of Operations to Make Network Connected.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1319. Number of Operations to Make Network Connected
     // There are n computers numbered from 0 to n-1 connected by ethernet cables connections forming a network where connections[i] = [a, b] represents a connection between computers a and b. Any computer can reach any other computer directly or indirectly through the network.
     // Given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected. Return the minimum number of times you need to do this in order to make all the computers connected. If it's not possible, return -1. 
