618547c1e893be5569f44843128af681dc1a4c29
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:45:06 +0300

    1315. Sum of Nodes with Even-Valued Grandparent

--- 1315. Sum of Nodes with Even-Valued Grandparent.swift
+++ 1315. Sum of Nodes with Even-Valued Grandparent.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1315. Sum of Nodes with Even-Valued Grandparent
     // Given a binary tree, return the sum of values of nodes with even-valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.)
     // If there are no nodes with an even-valued grandparent, return 0.
