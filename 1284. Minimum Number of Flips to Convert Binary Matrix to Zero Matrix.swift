6a430ed8dd19b23d1b45986b7a301a2d3af6e140
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:37:46 +0300

    1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix

--- 1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix.swift
+++ 1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1284. Minimum Number of Flips to Convert Binary Matrix to Zero Matrix
     // Given a m x n binary matrix mat. In one step, you can choose one cell and flip it and all the four neighbors of it if they exist (Flip is changing 1 to 0 and 0 to 1). A pair of cells are called neighbors if they share one edge.
     // Return the minimum number of steps required to convert mat to a zero matrix or -1 if you cannot.
