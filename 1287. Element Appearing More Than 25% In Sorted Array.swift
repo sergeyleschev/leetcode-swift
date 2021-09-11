b2bc7757ac1863c6b6b46f816ad98b581496f1ff
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:38:11 +0300

    1287. Element Appearing More Than 25% In Sorted Array

--- 1287. Element Appearing More Than 25% In Sorted Array.swift
+++ 1287. Element Appearing More Than 25% In Sorted Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1287. Element Appearing More Than 25% In Sorted Array
     // Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
 
