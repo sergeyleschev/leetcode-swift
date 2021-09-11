a1286529ba7ba7e8f2cbff77ed35e6002c340b3d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-11 08:39:03 +0300

    1291. Sequential Digits

--- 1291. Sequential Digits.swift
+++ 1291. Sequential Digits.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1291. Sequential Digits
     // An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
     // Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
