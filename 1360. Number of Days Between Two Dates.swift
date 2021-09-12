bb77a7446b81112035bf7aae24f7b9ef916831c5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:22:51 +0300

    1360. Number of Days Between Two Dates

--- 1360. Number of Days Between Two Dates.swift
+++ 1360. Number of Days Between Two Dates.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1360. Number of Days Between Two Dates
     // Write a program to count the number of days between two dates.
     // The two dates are given as strings, their format is YYYY-MM-DD as shown in the examples.
