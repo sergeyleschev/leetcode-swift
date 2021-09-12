aa3c6ca4edeb08671d60900a0b1e190d3c1c6682
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:21:32 +0300

    1351. Count Negative Numbers in a Sorted Matrix

--- 1351. Count Negative Numbers in a Sorted Matrix.swift
+++ 1351. Count Negative Numbers in a Sorted Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1351. Count Negative Numbers in a Sorted Matrix
     // Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 
