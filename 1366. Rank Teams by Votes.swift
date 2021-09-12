6c320aa36ca755a1b23dcb5b2cdb8caf27daeef9
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 21:55:44 +0300

    1366. Rank Teams by Votes

--- 1366. Rank Teams by Votes.swift
+++ 1366. Rank Teams by Votes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1366. Rank Teams by Votes
     // In a special ranking system, each voter gives a rank from highest to lowest to all teams participated in the competition.
     // The ordering of teams is decided by who received the most position-one votes. If two or more teams tie in the first position, we consider the second position to resolve the conflict, if they tie again, we continue this process until the ties are resolved. If two or more teams are still tied after considering all positions, we rank them alphabetically based on their team letter.
