bad766e67644892cdb74c9767dd0487e659db843
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:22:07 +0300

    1356. Sort Integers by The Number of 1 Bits

--- 1356. Sort Integers by The Number of 1 Bits.swift
+++ 1356. Sort Integers by The Number of 1 Bits.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1356. Sort Integers by The Number of 1 Bits
     // Given an integer array arr. You have to sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
     // Return the sorted array.
