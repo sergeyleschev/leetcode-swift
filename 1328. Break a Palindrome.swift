c09a2fc6e4621ae68fa00b472420ff0ccf21621e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:17:12 +0300

    1328. Break a Palindrome

--- 1328. Break a Palindrome.swift
+++ 1328. Break a Palindrome.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1328. Break a Palindrome
     // Given a palindromic string of lowercase English letters palindrome, replace exactly one character with any lowercase English letter so that the resulting string is not a palindrome and that it is the lexicographically smallest one possible.
     // Return the resulting string. If there is no way to replace a character to make it not a palindrome, return an empty string.
