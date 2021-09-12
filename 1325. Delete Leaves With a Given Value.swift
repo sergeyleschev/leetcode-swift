1488226a0057cbb3b2ce44b580e2f286eba7f7bd
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:16:53 +0300

    1325. Delete Leaves With a Given Value

--- 1325. Delete Leaves With a Given Value.swift
+++ 1325. Delete Leaves With a Given Value.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1325. Delete Leaves With a Given Value
     // Given a binary tree root and an integer target, delete all the leaf nodes with value target.
     // Note that once you delete a leaf node with value target, if it's parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you can't).
