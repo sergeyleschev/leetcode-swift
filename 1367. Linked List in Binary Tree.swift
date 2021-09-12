8a488bc9cbb3af25113b72350b373f5f50517f2a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 21:55:52 +0300

    1367. Linked List in Binary Tree

--- 1367. Linked List in Binary Tree.swift
+++ 1367. Linked List in Binary Tree.swift
@@ -25,6 +25,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1367. Linked List in Binary Tree
     // Given a binary tree root and a linked list with head as the first node. 
     // Return True if all the elements in the linked list starting from the head correspond to some downward path connected in the binary tree otherwise return False.
