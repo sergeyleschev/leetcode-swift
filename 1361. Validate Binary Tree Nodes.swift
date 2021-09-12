2fc54c9ec1f7587f1ba748137cf9c4de01ccf03f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 21:54:58 +0300

    1361. Validate Binary Tree Nodes

--- 1361. Validate Binary Tree Nodes.swift
+++ 1361. Validate Binary Tree Nodes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1361. Validate Binary Tree Nodes
     // You have n binary tree nodes numbered from 0 to n - 1 where node i has two children leftChild[i] and rightChild[i], return true if and only if all the given nodes form exactly one valid binary tree.
     // If node i has no left child then leftChild[i] will equal -1, similarly for the right child.
