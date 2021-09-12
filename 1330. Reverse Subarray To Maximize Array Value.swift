8f081eeefb2b1a079d86248d0d40eca5d3bc7547
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:17:29 +0300

    1330. Reverse Subarray To Maximize Array Value

--- 1330. Reverse Subarray To Maximize Array Value.swift
+++ 1330. Reverse Subarray To Maximize Array Value.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1330. Reverse Subarray To Maximize Array Value
     // You are given an integer array nums. The value of this array is defined as the sum of |nums[i]-nums[i+1]| for all 0 <= i < nums.length-1.
     // You are allowed to select any subarray of the given array and reverse it. You can perform this operation only once.
