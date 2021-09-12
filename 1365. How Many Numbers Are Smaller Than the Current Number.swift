3890118f25eb15b3aff5d94aaaa91ba7f94a524d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 21:55:34 +0300

    1365. How Many Numbers Are Smaller Than the Current Number

--- 1365. How Many Numbers Are Smaller Than the Current Number.swift
+++ 1365. How Many Numbers Are Smaller Than the Current Number.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1365. How Many Numbers Are Smaller Than the Current Number
     // Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
     // Return the answer in an array.
