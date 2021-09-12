5f5f32b31e3386fc0d9758fb43b9f9ec6e4da09b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:19:35 +0300

    1338. Reduce Array Size to The Half

--- 1338. Reduce Array Size to The Half.swift
+++ 1338. Reduce Array Size to The Half.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1338. Reduce Array Size to The Half
     // You are given an integer array arr. You can choose a set of integers and remove all the occurrences of these integers in the array.
     // Return the minimum size of the set so that at least half of the integers of the array are removed.
