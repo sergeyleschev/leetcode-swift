ccb7e167575691a7ab6d496a9cea06b407f7ec6e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 21:56:23 +0300

    1368. Minimum Cost to Make at Least One Valid Path in a Grid

--- 1368. Minimum Cost to Make at Least One Valid Path in a Grid.swift
+++ 1368. Minimum Cost to Make at Least One Valid Path in a Grid.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1368. Minimum Cost to Make at Least One Valid Path in a Grid
     // Given a m x n grid. Each cell of the grid has a sign pointing to the next cell you should visit if you are currently in this cell. The sign of grid[i][j] can be:
     // 1 which means go to the cell to the right. (i.e go from grid[i][j] to grid[i][j + 1])
