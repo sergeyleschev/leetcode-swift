429963e63f28fb0e22bc8a635f786230fffce79e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:19:54 +0300

    1342. Number of Steps to Reduce a Number to Zero

--- 1342. Number of Steps to Reduce a Number to Zero.swift
+++ 1342. Number of Steps to Reduce a Number to Zero.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1342. Number of Steps to Reduce a Number to Zero
     // Given a non-negative integer num, return the number of steps to reduce it to zero. If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 
