36e7c2c2ed0cefa43c880f093d033d73b7b5acca
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-12 08:19:05 +0300

    1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance

--- 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.swift
+++ 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance
     // There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distanceThreshold.
     // Return the city with the smallest number of cities that are reachable through some path and whose distance is at most distanceThreshold, If there are multiple such cities, return the city with the greatest number.
