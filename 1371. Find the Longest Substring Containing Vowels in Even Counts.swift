6bfc262518bd32db538a73478cdb68db4a9b0ab1
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 07:59:41 +0300

    1371. Find the Longest Substring Containing Vowels in Even Counts

--- 1371. Find the Longest Substring Containing Vowels in Even Counts.swift
+++ 1371. Find the Longest Substring Containing Vowels in Even Counts.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1371. Find the Longest Substring Containing Vowels in Even Counts
     // Given the string s, return the size of the longest substring containing each vowel an even number of times. That is, 'a', 'e', 'i', 'o', and 'u' must appear an even number of times.
 
