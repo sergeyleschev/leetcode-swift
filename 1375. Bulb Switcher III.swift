37a3b5375730e785b931b30cb96079f36bc2fea3
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:00:24 +0300

    1375. Bulb Switcher III

--- 1375. Bulb Switcher III.swift
+++ 1375. Bulb Switcher III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1375. Bulb Switcher III
     // There is a room with n bulbs, numbered from 1 to n, arranged in a row from left to right. Initially, all the bulbs are turned off.
     // At moment k (for k from 0 to n - 1), we turn on the light[k] bulb. A bulb change color to blue only if it is on and all the previous bulbs (to the left) are turned on too.
