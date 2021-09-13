a38b676f355460bbd58bc920ca55e13bf96f4f7f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:03:06 +0300

    1392. Longest Happy Prefix

--- 1392. Longest Happy Prefix.swift
+++ 1392. Longest Happy Prefix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1392. Longest Happy Prefix
     // A string is called a happy prefix if is a non-empty prefix which is also a suffix (excluding itself).
     // Given a string s, return the longest happy prefix of s. Return an empty string "" if no such prefix exists.
