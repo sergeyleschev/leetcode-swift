1cb9495d00cea56f7e7a524120937fd421185000
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:04:22 +0300

    1400. Construct K Palindrome Strings

--- 1400. Construct K Palindrome Strings.swift
+++ 1400. Construct K Palindrome Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+  // Solution @ Sergey Leschev, Belarusian State University
+
   // 1400. Construct K Palindrome Strings
   // Given a string s and an integer k. You should construct k non-empty palindrome strings using all the characters in s.
   // Return True if you can use all the characters in s to construct k palindrome strings or False otherwise.
