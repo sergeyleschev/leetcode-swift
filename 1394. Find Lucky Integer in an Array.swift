8cc1b3c960ff89c32b43aba1bacaa908da650a18
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:03:15 +0300

    1394. Find Lucky Integer in an Array

--- 1394. Find Lucky Integer in an Array.swift
+++ 1394. Find Lucky Integer in an Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1394. Find Lucky Integer in an Array
     // Given an array of integers arr, a lucky integer is an integer which has a frequency in the array equal to its value.
     // Return a lucky integer in the array. If there are multiple lucky integers return the largest of them. If there is no lucky integer return -1.
