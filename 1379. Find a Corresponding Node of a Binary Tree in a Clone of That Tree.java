b050b094b1e1b7a3920a8696dbb4237e44de6e82
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:01:00 +0300

    1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

--- 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.java
+++ 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree.java
@@ -20,6 +20,8 @@ class TreeNode {
 
 public class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
     // Given two binary trees original and cloned and given a reference to a node target in the original tree.
     // The cloned tree is a copy of the original tree.
