3817bf841e7ef75908d970df41c5cd2ca642c38f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:01:49 +0300

    1385. Find the Distance Value Between Two Arrays

--- 1385. Find the Distance Value Between Two Arrays.swift
+++ 1385. Find the Distance Value Between Two Arrays.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1385. Find the Distance Value Between Two Arrays
     // Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
     // The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
