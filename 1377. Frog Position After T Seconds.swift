2317f89ada1f6f42d59f119823e52d487cb399b6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:00:50 +0300

    1377. Frog Position After T Seconds

--- 1377. Frog Position After T Seconds.swift
+++ 1377. Frog Position After T Seconds.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1377. Frog Position After T Seconds
     // Given an undirected tree consisting of n vertices numbered from 1 to n. A frog starts jumping from vertex 1. In one second, the frog jumps from its current vertex to another unvisited vertex if they are directly connected. The frog can not jump back to a visited vertex. In case the frog can jump to several vertices, it jumps randomly to one of them with the same probability. Otherwise, when the frog can not jump to any unvisited vertex, it jumps forever on the same vertex.
     // The edges of the undirected tree are given in the array edges, where edges[i] = [ai, bi] means that exists an edge connecting the vertices ai and bi.
