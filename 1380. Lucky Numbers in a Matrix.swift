6aac8f10e333d5cda8365683b93d8492be110702
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:01:09 +0300

    1380. Lucky Numbers in a Matrix

--- 1380. Lucky Numbers in a Matrix.swift
+++ 1380. Lucky Numbers in a Matrix.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1380. Lucky Numbers in a Matrix
     // Given a m * n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
     // A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
