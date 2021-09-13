64dedf00a33a0de23efd394d092525f7ea3139db
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-13 08:00:12 +0300

    1374. Generate a String With Characters That Have Odd Counts

--- 1374. Generate a String With Characters That Have Odd Counts.swift
+++ 1374. Generate a String With Characters That Have Odd Counts.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1374. Generate a String With Characters That Have Odd Counts
     // Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
     // The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
