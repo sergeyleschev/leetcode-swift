8520945c6450d93ee8a7648da8393ce09bf7f577
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:05:47 +0300

    1411. Number of Ways to Paint N × 3 Grid

--- 1411. Number of Ways to Paint N × 3 Grid.swift
+++ 1411. Number of Ways to Paint N × 3 Grid.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1411. Number of Ways to Paint N × 3 Grid
     // You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colors: Red, Yellow, or Green while making sure that no two adjacent cells have the same color (i.e., no two cells that share vertical or horizontal sides have the same color).
     // Given n the number of rows of the grid, return the number of ways you can paint this grid. As the answer may grow large, the answer must be computed modulo 10^9 + 7.
