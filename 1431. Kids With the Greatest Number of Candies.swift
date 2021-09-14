8dac624c6bf62d49106a790374acae74de562e14
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:07:31 +0300

    1431. Kids With the Greatest Number of Candies

--- 1431. Kids With the Greatest Number of Candies.swift
+++ 1431. Kids With the Greatest Number of Candies.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1431. Kids With the Greatest Number of Candies
     // There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
     // Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
