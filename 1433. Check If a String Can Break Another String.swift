561ab43dd9a505c5a3afc8a4ff57d3ab1b69d856
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:07:50 +0300

    1433. Check If a String Can Break Another String

--- 1433. Check If a String Can Break Another String.swift
+++ 1433. Check If a String Can Break Another String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1433. Check If a String Can Break Another String
     // Given two strings: s1 and s2 with the same size, check if some permutation of string s1 can break some permutation of string s2 or vice-versa. In other words s2 can break s1 or vice-versa.
     // A string x can break string y (both of size n) if x[i] >= y[i] (in alphabetical order) for all i between 0 and n-1.
