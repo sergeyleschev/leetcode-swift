a1cb6c5c066f94878cc19ee5a04baf351ddaa0bc
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:09:19 +0300

    1443. Minimum Time to Collect All Apples in a Tree

--- 1443. Minimum Time to Collect All Apples in a Tree.swift
+++ 1443. Minimum Time to Collect All Apples in a Tree.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1443. Minimum Time to Collect All Apples in a Tree
     // Given an undirected tree consisting of n vertices numbered from 0 to n-1, which has some apples in their vertices. You spend 1 second to walk over one edge of the tree. Return the minimum time in seconds you have to spend to collect all apples in the tree, starting at vertex 0 and coming back to this vertex.
     // The edges of the undirected tree are given in the array edges, where edges[i] = [ai, bi] means that exists an edge connecting the vertices ai and bi. Additionally, there is a boolean array hasApple, where hasApple[i] = true means that vertex i has an apple; otherwise, it does not have any apple.
