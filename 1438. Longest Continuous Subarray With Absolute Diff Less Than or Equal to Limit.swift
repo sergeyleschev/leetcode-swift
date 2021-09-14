3aa73204a6a6bc201309ea64784aeb30e37fd45a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:08:34 +0300

    1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.swift
+++ 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
     // Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
 
