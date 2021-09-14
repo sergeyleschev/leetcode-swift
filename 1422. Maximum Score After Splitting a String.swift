5ec993eb7dd470f83d09f316a5bea8f7d1787122
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:06:47 +0300

    1422. Maximum Score After Splitting a String

--- 1422. Maximum Score After Splitting a String.swift
+++ 1422. Maximum Score After Splitting a String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1422. Maximum Score After Splitting a String
     // Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
     // The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
