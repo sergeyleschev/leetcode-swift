15a5cd317ee5a51fb0175e97397a0ce253285d7d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:10:07 +0300

    1448. Count Good Nodes in Binary Tree

--- 1448. Count Good Nodes in Binary Tree.swift
+++ 1448. Count Good Nodes in Binary Tree.swift
@@ -15,6 +15,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1448. Count Good Nodes in Binary Tree
     // Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
     // Return the number of good nodes in the binary tree.
