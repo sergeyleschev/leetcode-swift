e523859803c1583bce022e11e70c9c13a3ee1ba5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:05:21 +0300

    1408. String Matching in an Array

--- 1408. String Matching in an Array.swift
+++ 1408. String Matching in an Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1408. String Matching in an Array
     // Given an array of string words. Return all strings in words which is substring of another word in any order. 
     // String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
