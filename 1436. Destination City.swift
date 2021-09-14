9e5f1bfafa3c11a7327b1641abc7ed7e02e2ac7b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:08:09 +0300

    1436. Destination City

--- 1436. Destination City.swift
+++ 1436. Destination City.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1436. Destination City
     // You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
     // It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
