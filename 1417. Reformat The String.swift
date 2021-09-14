5efe479cb76ddd5ed40389d023e6672a2861029a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-14 08:06:22 +0300

    1417. Reformat The String

--- 1417. Reformat The String.swift
+++ 1417. Reformat The String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1417. Reformat The String
     // Given alphanumeric string s. (Alphanumeric string is a string consisting of lowercase English letters and digits).
     // You have to find a permutation of the string where no letter is followed by another letter and no digit is followed by another digit. That is, no two adjacent characters have the same type.
