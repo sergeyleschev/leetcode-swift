421837fe46da6a9e1b523f277797034a37d4d972
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 22:16:16 +0300

    1502. Can Make Arithmetic Progression From Sequence

--- 1502. Can Make Arithmetic Progression From Sequence.swift
+++ 1502. Can Make Arithmetic Progression From Sequence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1502. Can Make Arithmetic Progression From Sequence
     // A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.
     // Given an array of numbers arr, return true if the array can be rearranged to form an arithmetic progression. Otherwise, return false.
