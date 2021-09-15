e186dce346070d8216bf73cd08f8b36267d2384a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:10:32 +0300

    1462. Course Schedule IV

--- 1462. Course Schedule IV.swift
+++ 1462. Course Schedule IV.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1462. Course Schedule IV
     // There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course ai first if you want to take course bi.
     // For example, the pair [0, 1] indicates that you have to take course 0 before you can take course 1.
