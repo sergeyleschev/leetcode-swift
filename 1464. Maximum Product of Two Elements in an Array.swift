4b2fbc0edad80f8e365f0cab3d7607482789b722
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:10:49 +0300

    1464. Maximum Product of Two Elements in an Array

--- 1464. Maximum Product of Two Elements in an Array.swift
+++ 1464. Maximum Product of Two Elements in an Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1464. Maximum Product of Two Elements in an Array
     // Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
      
