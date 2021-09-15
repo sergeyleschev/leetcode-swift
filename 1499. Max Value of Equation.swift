9fe3feff7d698a2f1ac94378cbe69bf40b28b593
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:18:49 +0300

    1499. Max Value of Equation

--- 1499. Max Value of Equation.swift
+++ 1499. Max Value of Equation.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1499. Max Value of Equation
     // You are given an array points containing the coordinates of points on a 2D plane, sorted by the x-values, where points[i] = [xi, yi] such that xi < xj for all 1 <= i < j <= points.length. You are also given an integer k.
     // Return the maximum value of the equation yi + yj + |xi - xj| where |xi - xj| <= k and 1 <= i < j <= points.length.
