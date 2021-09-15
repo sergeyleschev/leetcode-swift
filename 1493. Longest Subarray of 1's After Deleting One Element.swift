7f8b23d07a62371c53750deb9b958768864ed4c0
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:17:59 +0300

    1493. Longest Subarray of 1's After Deleting One Element

--- 1493. Longest Subarray of 1's After Deleting One Element.swift
+++ 1493. Longest Subarray of 1's After Deleting One Element.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1493. Longest Subarray of 1's After Deleting One Element
     // Given a binary array nums, you should delete one element from it.
     // Return the size of the longest non-empty subarray containing only 1's in the resulting array.
