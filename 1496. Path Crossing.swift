14744b5bdfa8f80ed724fa1765afb9c3fe63ac0f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:18:11 +0300

    1496. Path Crossing

--- 1496. Path Crossing.swift
+++ 1496. Path Crossing.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1496. Path Crossing
     // Given a string path, where path[i] = 'N', 'S', 'E' or 'W', each representing moving one unit north, south, east, or west, respectively. You start at the origin (0, 0) on a 2D plane and walk on the path specified by path.
     // Return true if the path crosses itself at any point, that is, if at any time you are on a location you have previously visited. Return false otherwise.
