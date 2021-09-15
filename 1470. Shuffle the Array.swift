dbce45327a4e0b2702c6ca8ae3c2c0a788926b20
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:11:19 +0300

    1470. Shuffle the Array

--- 1470. Shuffle the Array.swift
+++ 1470. Shuffle the Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1470. Shuffle the Array
     // Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
     // Return the array in the form [x1,y1,x2,y2,...,xn,yn].
