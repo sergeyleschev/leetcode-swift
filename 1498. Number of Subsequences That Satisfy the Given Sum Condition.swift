7273d776b94d5baec7c53d71dbd03df6dbbc4f78
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:18:39 +0300

    1498. Number of Subsequences That Satisfy the Given Sum Condition

--- 1498. Number of Subsequences That Satisfy the Given Sum Condition.swift
+++ 1498. Number of Subsequences That Satisfy the Given Sum Condition.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1498. Number of Subsequences That Satisfy the Given Sum Condition
     // Given an array of integers nums and an integer target.
     // Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 10^9 + 7.
