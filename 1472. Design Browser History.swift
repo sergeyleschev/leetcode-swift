cbc2412013af2916ebe4dc08fe55bc5909af1737
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:11:36 +0300

    1472. Design Browser History

--- 1472. Design Browser History.swift
+++ 1472. Design Browser History.swift
@@ -1,5 +1,7 @@
 class BrowserHistory {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1472. Design Browser History
     // You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
 
