3fa5d72b0807aa48fc14c92838ed071a056c7d66
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:10:40 +0300

    1463. Cherry Pickup II

--- 1463. Cherry Pickup II.swift
+++ 1463. Cherry Pickup II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1463. Cherry Pickup II
     // Given a rows x cols matrix grid representing a field of cherries. Each cell in grid represents the number of cherries that you can collect.
     // You have two robots that can collect cherries for you, Robot #1 is located at the top-left corner (0,0) , and Robot #2 is located at the top-right corner (0, cols-1) of the grid.
