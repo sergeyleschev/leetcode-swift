d7a18b5f510ec06a2abc01025a8ec665eff74ede
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-15 08:14:58 +0300

    1475. Final Prices With a Special Discount in a Shop

--- 1475. Final Prices With a Special Discount in a Shop.swift
+++ 1475. Final Prices With a Special Discount in a Shop.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1475. Final Prices With a Special Discount in a Shop
     // Given the array prices where prices[i] is the price of the ith item in a shop. There is a special discount for items in the shop, if you buy the ith item, then you will receive a discount equivalent to prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i], otherwise, you will not receive any discount at all.
     // Return an array where the ith element is the final price you will pay for the ith item of the shop considering the special discount.
