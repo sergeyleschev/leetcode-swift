776b3fd324adb60d09b86c3bcad7f23c5a7f419f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-16 07:57:09 +0300

    1551. Minimum Operations to Make Array Equal

--- 1551. Minimum Operations to Make Array Equal.swift
+++ 1551. Minimum Operations to Make Array Equal.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1551. Minimum Operations to Make Array Equal
     // You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid values of i (i.e. 0 <= i < n).
     // In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and add 1 to arr[y] (i.e. perform arr[x] -=1 and arr[y] += 1). The goal is to make all the elements of the array equal. It is guaranteed that all the elements of the array can be made equal using some operations.
