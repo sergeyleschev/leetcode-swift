050a0e0b7cedf2609934586ab78a27701228c712
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-16 07:53:11 +0300

    1523. Count Odd Numbers in an Interval Range

--- 1523. Count Odd Numbers in an Interval Range.swift
+++ 1523. Count Odd Numbers in an Interval Range.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1523. Count Odd Numbers in an Interval Range
     // Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
 
