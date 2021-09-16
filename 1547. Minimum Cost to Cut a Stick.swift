a242f6358f0614062b90b36b190d5bef1410326b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-16 07:56:41 +0300

    1547. Minimum Cost to Cut a Stick

--- 1547. Minimum Cost to Cut a Stick.swift
+++ 1547. Minimum Cost to Cut a Stick.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1547. Minimum Cost to Cut a Stick
     // Given a wooden stick of length n units. The stick is labelled from 0 to n. For example, a stick of length 6 is labelled as follows:
     // Given an integer array cuts where cuts[i] denotes a position you should perform a cut at.
