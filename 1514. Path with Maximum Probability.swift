170deb7b63bbceca67b6391a778935ebdcd7269a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-16 07:52:41 +0300

    1514. Path with Maximum Probability

--- 1514. Path with Maximum Probability.swift
+++ 1514. Path with Maximum Probability.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1514. Path with Maximum Probability
     // You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
     // Given two nodes start and end, find the path with the maximum probability of success to go from start to end and return its success probability.
