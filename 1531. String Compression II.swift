f24cc0e5f6c53c8ca4ef00fe25a2a2fc8aa047d6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-16 07:54:18 +0300

    1531. String Compression II

--- 1531. String Compression II.swift
+++ 1531. String Compression II.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1531. String Compression II
     // Run-length encoding is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run). For example, to compress the string "aabccc" we replace "aa" by "a2" and replace "ccc" by "c3". Thus the compressed string becomes "a2bc3".
     // Notice that in this problem, we are not adding '1' after single characters.
