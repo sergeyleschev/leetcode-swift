28f97dd4e75e771c0891652dbbe852bc0735f99d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-16 07:53:51 +0300

    1528. Shuffle String

--- 1528. Shuffle String.swift
+++ 1528. Shuffle String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1528. Shuffle String
     // Given a string s and an integer array indices of the same length.
     // The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
