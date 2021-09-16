a498b3c56d0f1af3d6e7d7fae47972ee866b1372
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-16 07:53:02 +0300

    1518. Water Bottles

--- 1518. Water Bottles.swift
+++ 1518. Water Bottles.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1518. Water Bottles
     // Given numBottles full water bottles, you can exchange numExchange empty water bottles for one full water bottle.
     // The operation of drinking a full water bottle turns it into an empty bottle.
