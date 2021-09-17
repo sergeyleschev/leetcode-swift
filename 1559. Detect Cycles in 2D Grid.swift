10b0c753d0cd485c0cfc7654e33834179c619fc5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:06:54 +0300

    1559. Detect Cycles in 2D Grid

--- 1559. Detect Cycles in 2D Grid.swift
+++ 1559. Detect Cycles in 2D Grid.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1559. Detect Cycles in 2D Grid
     // Given a 2D array of characters grid of size m x n, you need to find if there exists any cycle consisting of the same value in grid.
     // A cycle is a path of length 4 or more in the grid that starts and ends at the same cell. From a given cell, you can move to one of the cells adjacent to it - in one of the four directions (up, down, left, or right), if it has the same value of the current cell.
