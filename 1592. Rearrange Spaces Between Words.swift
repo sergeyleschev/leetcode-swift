7fe0b79effafb0e72e441b4c44ff899f91c4c580
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:11:23 +0300

    1592. Rearrange Spaces Between Words

--- 1592. Rearrange Spaces Between Words.swift
+++ 1592. Rearrange Spaces Between Words.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1592. Rearrange Spaces Between Words
     // You are given a string text of words that are placed among some number of spaces. Each word consists of one or more lowercase English letters and are separated by at least one space. It's guaranteed that text contains at least one word.
     // Rearrange the spaces so that there is an equal number of spaces between every pair of adjacent words and that number is maximized. If you cannot redistribute all the spaces equally, place the extra spaces at the end, meaning the returned string should be the same length as text.
