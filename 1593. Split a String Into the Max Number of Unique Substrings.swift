2624020d0497bba0242fb836d8ddfd18eb0ae9ec
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:11:40 +0300

    1593. Split a String Into the Max Number of Unique Substrings

--- 1593. Split a String Into the Max Number of Unique Substrings.swift
+++ 1593. Split a String Into the Max Number of Unique Substrings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1593. Split a String Into the Max Number of Unique Substrings
     // Given a string s, return the maximum number of unique substrings that the given string can be split into.
     // You can split string s into any list of non-empty substrings, where the concatenation of the substrings forms the original string. However, you must split the substrings such that all of them are unique.
