9e2c9b5f2a34d912d3391286dd20f36202a93e12
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:12:36 +0300

    1600. Throne Inheritance

--- 1600. Throne Inheritance.swift
+++ 1600. Throne Inheritance.swift
@@ -1,5 +1,7 @@
 class ThroneInheritance {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1600. Throne Inheritance
     // A kingdom consists of a king, his children, his grandchildren, and so on. Every once in a while, someone in the family dies or a child is born.
     // The kingdom has a well-defined order of inheritance that consists of the king as the first member. Let's define the recursive function Successor(x, curOrder), which given a person x and the inheritance order so far, returns who should be the next person after x in the order of inheritance.
