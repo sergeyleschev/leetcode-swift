dd6852690257d4be2ee3d6741e20158d41e34b51
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:08:39 +0300

    1572. Matrix Diagonal Sum

--- 1572. Matrix Diagonal Sum.swift
+++ 1572. Matrix Diagonal Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1572. Matrix Diagonal Sum
     // Given a square matrix mat, return the sum of the matrix diagonals.
     // Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
