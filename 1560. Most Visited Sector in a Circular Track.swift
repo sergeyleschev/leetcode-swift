f3a6085859d096f8f209c6c6bb38a4e1bdb1dd82
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:07:04 +0300

    1560. Most Visited Sector in a Circular Track

--- 1560. Most Visited Sector in a Circular Track.swift
+++ 1560. Most Visited Sector in a Circular Track.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1560. Most Visited Sector in a Circular Track
     // Given an integer n and an integer array rounds. We have a circular track which consists of n sectors labeled from 1 to n. A marathon will be held on this track, the marathon consists of m rounds. The ith round starts at sector rounds[i - 1] and ends at sector rounds[i]. For example, round 1 starts at sector rounds[0] and ends at sector rounds[1]
     // Return an array of the most visited sectors sorted in ascending order.
