c579cbbc0194f847675657831fb01317b128952f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:14:07 +0300

    1610. Maximum Number of Visible Points

--- 1610. Maximum Number of Visible Points.swift
+++ 1610. Maximum Number of Visible Points.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1610. Maximum Number of Visible Points
     // You are given an array points, an integer angle, and your location, where location = [posx, posy] and points[i] = [xi, yi] both denote integral coordinates on the X-Y plane.
     // Initially, you are facing directly east from your position. You cannot move from your position, but you can rotate. In other words, posx and posy cannot be changed. Your field of view in degrees is represented by angle, determining how wide you can see from any given view direction. Let d be the amount in degrees that you rotate counterclockwise. Then, your field of view is the inclusive range of angles [d - angle/2, d + angle/2].
