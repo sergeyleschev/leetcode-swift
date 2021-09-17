22fd6a5ef746d5dfebc98a2ebb8e3b22f0560865
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:09:05 +0300

    1575. Count All Possible Routes

--- 1575. Count All Possible Routes.swift
+++ 1575. Count All Possible Routes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1575. Count All Possible Routes
     // You are given an array of distinct positive integers locations where locations[i] represents the position of city i. You are also given integers start, finish and fuel representing the starting city, ending city, and the initial amount of fuel you have, respectively.
     // At each step, if you are at city i, you can pick any city j such that j != i and 0 <= j < locations.length and move to city j. Moving from city i to city j reduces the amount of fuel you have by |locations[i] - locations[j]|. Please notice that |x| denotes the absolute value of x.
