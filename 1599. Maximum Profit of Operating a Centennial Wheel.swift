2cce7a0c40a54e49e6b02b016297c06bfe9f37c3
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:12:16 +0300

    1599. Maximum Profit of Operating a Centennial Wheel

--- 1599. Maximum Profit of Operating a Centennial Wheel.swift
+++ 1599. Maximum Profit of Operating a Centennial Wheel.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1599. Maximum Profit of Operating a Centennial Wheel
     // You are the operator of a Centennial Wheel that has four gondolas, and each gondola has room for up to four people. You have the ability to rotate the gondolas counterclockwise, which costs you runningCost dollars.
     // You are given an array customers of length n where customers[i] is the number of new customers arriving just before the ith rotation (0-indexed). This means you must rotate the wheel i times before the customers[i] customers arrive. You cannot make customers wait if there is room in the gondola. Each customer pays boardingCost dollars when they board on the gondola closest to the ground and will exit once that gondola reaches the ground again.
