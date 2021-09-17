500290aaaccaa8f33660da5d59c2141892c466b5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:07:23 +0300

    1562. Find Latest Group of Size M

--- 1562. Find Latest Group of Size M.swift
+++ 1562. Find Latest Group of Size M.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1562. Find Latest Group of Size M
     // Given an array arr that represents a permutation of numbers from 1 to n. You have a binary string of size n that initially has all its bits set to zero.
     // At each step i (assuming both the binary string and arr are 1-indexed) from 1 to n, the bit at position arr[i] is set to 1. You are given an integer m and you need to find the latest step at which there exists a group of ones of length m. A group of ones is a contiguous substring of 1s such that it cannot be extended in either direction.
