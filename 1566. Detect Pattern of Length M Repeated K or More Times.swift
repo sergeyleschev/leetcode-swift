e7176c10cb57810d6eb80bae2a8b74a5eea9e256
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:08:20 +0300

    1566. Detect Pattern of Length M Repeated K or More Times

--- 1566. Detect Pattern of Length M Repeated K or More Times.swift
+++ 1566. Detect Pattern of Length M Repeated K or More Times.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1566. Detect Pattern of Length M Repeated K or More Times
     // Given an array of positive integers arr,  find a pattern of length m that is repeated k or more times.
     // A pattern is a subarray (consecutive sub-sequence) that consists of one or more values, repeated multiple times consecutively without overlapping. A pattern is defined by its length and the number of repetitions.
