5e254ed7e77d97113a4ae2084eff5152e238c30f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:11:03 +0300

    1589. Maximum Sum Obtained of Any Permutation

--- 1589. Maximum Sum Obtained of Any Permutation.swift
+++ 1589. Maximum Sum Obtained of Any Permutation.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1589. Maximum Sum Obtained of Any Permutation
     // We have an array of integers, nums, and an array of requests where requests[i] = [starti, endi]. The ith request asks for the sum of nums[starti] + nums[starti + 1] + ... + nums[endi - 1] + nums[endi]. Both starti and endi are 0-indexed.
     // Return the maximum total sum of all requests among all permutations of nums.
