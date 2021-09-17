7315786674869aee00035252c8055e437ed92a9a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:06:36 +0300

    1557. Minimum Number of Vertices to Reach All Nodes

--- 1557. Minimum Number of Vertices to Reach All Nodes.swift
+++ 1557. Minimum Number of Vertices to Reach All Nodes.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1557. Minimum Number of Vertices to Reach All Nodes
     // Given a directed acyclic graph, with n vertices numbered from 0 to n-1, and an array edges where edges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
     // Find the smallest set of vertices from which all nodes in the graph are reachable. It's guaranteed that a unique solution exists.
