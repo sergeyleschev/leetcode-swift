6a998eebe530d2acb139188e44d3de569fd4ab11
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:13:16 +0300

    1605. Find Valid Matrix Given Row and Column Sums

--- 1605. Find Valid Matrix Given Row and Column Sums.swift
+++ 1605. Find Valid Matrix Given Row and Column Sums.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1605. Find Valid Matrix Given Row and Column Sums
     // You are given two arrays rowSum and colSum of non-negative integers where rowSum[i] is the sum of the elements in the ith row and colSum[j] is the sum of the elements of the jth column of a 2D matrix. In other words, you do not know the elements of the matrix, but you do know the sums of each row and column.
     // Find any matrix of non-negative integers of size rowSum.length x colSum.length that satisfies the rowSum and colSum requirements.
