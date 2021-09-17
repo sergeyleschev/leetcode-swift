f694fdc575cfdb1fe42a87c952713e4fd48af3ce
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:10:43 +0300

    1584. Min Cost to Connect All Points

--- 1584. Min Cost to Connect All Points.swift
+++ 1584. Min Cost to Connect All Points.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1584. Min Cost to Connect All Points
     // You are given an array points representing integer coordinates of some points on a 2D-plane, where points[i] = [xi, yi].
     // The cost of connecting two points [xi, yi] and [xj, yj] is the manhattan distance between them: |xi - xj| + |yi - yj|, where |val| denotes the absolute value of val.
