b4c40f5762b8a2f7626cf21a785ff229e32c8d84
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-17 08:13:33 +0300

    1608. Special Array With X Elements Greater Than or Equal X

--- 1608. Special Array With X Elements Greater Than or Equal X.swift
+++ 1608. Special Array With X Elements Greater Than or Equal X.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1608. Special Array With X Elements Greater Than or Equal X
     // You are given an array nums of non-negative integers. nums is considered special if there exists a number x such that there are exactly x numbers in nums that are greater than or equal to x.
     // Notice that x does not have to be an element in nums.
