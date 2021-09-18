7fffa5be091b3a7c49af3eb25626bac841d0f996
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 23:52:45 +0300

    1668. Maximum Repeating Substring

--- 1668. Maximum Repeating Substring.swift
+++ 1668. Maximum Repeating Substring.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1668. Maximum Repeating Substring
     // For a string sequence, a string word is k-repeating if word concatenated k times is a substring of sequence. The word's maximum k-repeating value is the highest value k where word is k-repeating in sequence. If word is not a substring of sequence, word's maximum k-repeating value is 0.
     // Given strings sequence and word, return the maximum k-repeating value of word in sequence.
