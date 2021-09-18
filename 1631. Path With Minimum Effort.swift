e12b4415ef9521e4d9a559b2e1aced501e9c3ca6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:10:08 +0300

    1631. Path With Minimum Effort

--- 1631. Path With Minimum Effort.swift
+++ 1631. Path With Minimum Effort.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1631. Path With Minimum Effort
     // You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows x columns, where heights[row][col] represents the height of cell (row, col). You are situated in the top-left cell, (0, 0), and you hope to travel to the bottom-right cell, (rows-1, columns-1) (i.e., 0-indexed). You can move up, down, left, or right, and you wish to find a route that requires the minimum effort.
     // A route's effort is the maximum absolute difference in heights between two consecutive cells of the route.
