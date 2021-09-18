175328321594143076b81ddb8945d146ed1fc472
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:03:37 +0300

    1619. Mean of Array After Removing Some Elements

--- 1619. Mean of Array After Removing Some Elements.swift
+++ 1619. Mean of Array After Removing Some Elements.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1619. Mean of Array After Removing Some Elements
     // Given an integer array arr, return the mean of the remaining integers after removing the smallest 5% and the largest 5% of the elements.
     // Answers within 10-5 of the actual answer will be considered accepted.
