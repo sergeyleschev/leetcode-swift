406b03e68e3439b25a7331c4813f1b8f8f75ece9
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:10:25 +0300

    1637. Widest Vertical Area Between Two Points Containing No Points

--- 1637. Widest Vertical Area Between Two Points Containing No Points.swift
+++ 1637. Widest Vertical Area Between Two Points Containing No Points.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1637. Widest Vertical Area Between Two Points Containing No Points
     // Given n points on a 2D plane where points[i] = [xi, yi], Return the widest vertical area between two points such that no points are inside the area.
     // A vertical area is an area of fixed-width extending infinitely along the y-axis (i.e., infinite height). The widest vertical area is the one with the maximum width.
