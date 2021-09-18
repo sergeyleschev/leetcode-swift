e6adfcaadc70687ee4c8d713466d433c3c59081f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:12:56 +0300

    1653. Minimum Deletions to Make String Balanced

--- 1653. Minimum Deletions to Make String Balanced.swift
+++ 1653. Minimum Deletions to Make String Balanced.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1653. Minimum Deletions to Make String Balanced
     // You are given a string s consisting only of characters 'a' and 'b'.
     // You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
