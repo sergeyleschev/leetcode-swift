52b55c567504c823f64e74541ff091c8d93d1724
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:11:40 +0300

    1643. Kth Smallest Instructions

--- 1643. Kth Smallest Instructions.swift
+++ 1643. Kth Smallest Instructions.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1643. Kth Smallest Instructions
     // Bob is standing at cell (0, 0), and he wants to reach destination: (row, column). He can only travel right and down. You are going to help Bob by providing instructions for him to reach destination.
     // The instructions are represented as a string, where each character is either:
