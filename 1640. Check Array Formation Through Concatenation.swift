a38af289d9b3a5042257192151d85edeaee11934
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:10:55 +0300

    1640. Check Array Formation Through Concatenation

--- 1640. Check Array Formation Through Concatenation.swift
+++ 1640. Check Array Formation Through Concatenation.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1640. Check Array Formation Through Concatenation
     // You are given an array of distinct integers arr and an array of integer arrays pieces, where the integers in pieces are distinct. Your goal is to form arr by concatenating the arrays in pieces in any order. However, you are not allowed to reorder the integers in each array pieces[i].
 
