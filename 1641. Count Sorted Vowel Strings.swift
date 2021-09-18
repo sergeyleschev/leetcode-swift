9f6b0a835564d989d83688688c6c8f7c2c466d9f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:11:15 +0300

    1641. Count Sorted Vowel Strings

--- 1641. Count Sorted Vowel Strings.swift
+++ 1641. Count Sorted Vowel Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1641. Count Sorted Vowel Strings
     // Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
     // A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
