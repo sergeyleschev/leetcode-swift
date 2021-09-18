bf75a59d7491aa725172576282394d5f6336f256
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:14:27 +0300

    1662. Check If Two String Arrays are Equivalent

--- 1662. Check If Two String Arrays are Equivalent.swift
+++ 1662. Check If Two String Arrays are Equivalent.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1662. Check If Two String Arrays are Equivalent
     // Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
     // A string is represented by an array if the array elements concatenated in order forms the string.
