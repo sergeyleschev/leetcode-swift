3eaf282a14b2387a16b96504d65ee26dce4f74f2
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:09:59 +0300

    1630. Arithmetic Subarrays

--- 1630. Arithmetic Subarrays.swift
+++ 1630. Arithmetic Subarrays.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1630. Arithmetic Subarrays
     // A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same. More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
     // For example, these are arithmetic sequences:
