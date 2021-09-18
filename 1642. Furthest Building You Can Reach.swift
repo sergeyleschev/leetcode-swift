bea7f30b5c63cf691e4f32e645616a0b682f1315
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-18 08:11:30 +0300

    1642. Furthest Building You Can Reach

--- 1642. Furthest Building You Can Reach.swift
+++ 1642. Furthest Building You Can Reach.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1642. Furthest Building You Can Reach
     // You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
     // You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
