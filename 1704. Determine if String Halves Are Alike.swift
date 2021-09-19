912210bb5d49d230b15db7d4c34118d0312736d3
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:34:24 +0300

    1704. Determine if String Halves Are Alike

--- 1704. Determine if String Halves Are Alike.swift
+++ 1704. Determine if String Halves Are Alike.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1704. Determine if String Halves Are Alike
     // You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
     // Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
