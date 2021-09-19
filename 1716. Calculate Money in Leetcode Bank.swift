0461e46d20aa4b5b0b5487f65b33a0ca376c271c
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:41:14 +0300

    1716. Calculate Money in Leetcode Bank

--- 1716. Calculate Money in Leetcode Bank.swift
+++ 1716. Calculate Money in Leetcode Bank.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1716. Calculate Money in Leetcode Bank
     // Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
     // He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
