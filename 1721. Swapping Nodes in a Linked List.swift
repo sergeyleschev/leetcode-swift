bcd0e1780244eb9df1860230c61379c9df673f16
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:42:15 +0300

    1721. Swapping Nodes in a Linked List

--- 1721. Swapping Nodes in a Linked List.swift
+++ 1721. Swapping Nodes in a Linked List.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1721. Swapping Nodes in a Linked List
     // You are given the head of a linked list, and an integer k.
     // Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
