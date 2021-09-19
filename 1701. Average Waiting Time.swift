cc5f0348969ca60ba08aa8d8dcc22807c05378e3
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:33:52 +0300

    1701. Average Waiting Time

--- 1701. Average Waiting Time.swift
+++ 1701. Average Waiting Time.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1701. Average Waiting Time
     // There is a restaurant with a single chef. You are given an array customers, where customers[i] = [arrivali, timei]:
     // arrivali is the arrival time of the ith customer. The arrival times are sorted in non-decreasing order.
