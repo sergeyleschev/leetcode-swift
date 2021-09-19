a3ed1592951c040fe6c7703afcf0b9158ffed71d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:31:34 +0300

    1684. Count the Number of Consistent Strings

--- 1684. Count the Number of Consistent Strings.swift
+++ 1684. Count the Number of Consistent Strings.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1684. Count the Number of Consistent Strings
     // You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
 
