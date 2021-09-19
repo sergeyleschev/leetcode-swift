6819c8aceedfddb582769de1eba7f629c7a7bcbf
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:42:34 +0300

    1722. Minimize Hamming Distance After Swap Operations

--- 1722. Minimize Hamming Distance After Swap Operations.swift
+++ 1722. Minimize Hamming Distance After Swap Operations.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1722. Minimize Hamming Distance After Swap Operations
     // You are given two integer arrays, source and target, both of length n. You are also given an array allowedSwaps where each allowedSwaps[i] = [ai, bi] indicates that you are allowed to swap the elements at index ai and index bi (0-indexed) of array source. Note that you can swap elements at a specific pair of indices multiple times and in any order.
     // The Hamming distance of two arrays of the same length, source and target, is the number of positions where the elements are different. Formally, it is the number of indices i for 0 <= i <= n-1 where source[i] != target[i] (0-indexed).
