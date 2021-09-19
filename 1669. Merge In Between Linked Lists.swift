4102a49290e27a1c0150f5df58678e3eca5219c2
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:28:26 +0300

    1669. Merge In Between Linked Lists

--- 1669. Merge In Between Linked Lists.swift
+++ 1669. Merge In Between Linked Lists.swift
@@ -10,6 +10,8 @@
  */
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1669. Merge In Between Linked Lists
     // You are given two linked lists: list1 and list2 of sizes n and m respectively.
     // Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
