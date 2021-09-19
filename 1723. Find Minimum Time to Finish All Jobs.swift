87d636a516b591ded0583f7a88f75582c8961500
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 18:46:25 +0300

    1723. Find Minimum Time to Finish All Jobs

--- 1723. Find Minimum Time to Finish All Jobs.swift
+++ 1723. Find Minimum Time to Finish All Jobs.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1723. Find Minimum Time to Finish All Jobs
     // You are given an integer array jobs, where jobs[i] is the amount of time it takes to complete the ith job.
     // There are k workers that you can assign jobs to. Each job should be assigned to exactly one worker. The working time of a worker is the sum of the time it takes to complete all jobs assigned to them. Your goal is to devise an optimal assignment such that the maximum working time of any worker is minimized.
