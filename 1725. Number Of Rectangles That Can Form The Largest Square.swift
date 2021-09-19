8418b527680af95bb81de06ea47fa2c71065ec47
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 18:46:36 +0300

    1725. Number Of Rectangles That Can Form The Largest Square

--- 1725. Number Of Rectangles That Can Form The Largest Square.swift
+++ 1725. Number Of Rectangles That Can Form The Largest Square.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1725. Number Of Rectangles That Can Form The Largest Square
     // You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
     // You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
