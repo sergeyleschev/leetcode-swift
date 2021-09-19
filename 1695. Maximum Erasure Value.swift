8002d4e3416fc93199844be552315a70ab03aa19
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:33:24 +0300

    1695. Maximum Erasure Value

--- 1695. Maximum Erasure Value.swift
+++ 1695. Maximum Erasure Value.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1695. Maximum Erasure Value
     // You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
 
