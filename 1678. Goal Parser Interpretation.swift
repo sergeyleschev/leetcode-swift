7fa6358ef03ff5a1e1593b69a9545546148bad0b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:31:05 +0300

    1678. Goal Parser Interpretation

--- 1678. Goal Parser Interpretation.swift
+++ 1678. Goal Parser Interpretation.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1678. Goal Parser Interpretation
     // You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
     // Given the string command, return the Goal Parser's interpretation of command.
