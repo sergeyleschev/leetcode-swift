db4f5a806b55b79e4d708a8221f98c9381860d8a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:33:06 +0300

    1691. Maximum Height by Stacking Cuboids

--- 1691. Maximum Height by Stacking Cuboids.swift
+++ 1691. Maximum Height by Stacking Cuboids.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1691. Maximum Height by Stacking Cuboids
     // Given n cuboids where the dimensions of the ith cuboid is cuboids[i] = [widthi, lengthi, heighti] (0-indexed). Choose a subset of cuboids and place them on each other.
     // You can place cuboid i on cuboid j if widthi <= widthj and lengthi <= lengthj and heighti <= heightj. You can rearrange any cuboid's dimensions by rotating it to put it on another cuboid.
