96e48770f33d2f0a378b95489e2ef51f6d294b2f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:28:48 +0300

    1672. Richest Customer Wealth

--- 1672. Richest Customer Wealth.swift
+++ 1672. Richest Customer Wealth.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1672. Richest Customer Wealth
     // You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i-th customer has in the j-th bank. Return the wealth that the richest customer has
     // A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
