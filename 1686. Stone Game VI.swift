9258ea96f1bcf42e6030309a2be5e432c5ac5a35
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:31:53 +0300

    1686. Stone Game VI

--- 1686. Stone Game VI.swift
+++ 1686. Stone Game VI.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1686. Stone Game VI
     // Alice and Bob take turns playing a game, with Alice starting first.
     // There are n stones in a pile. On each player's turn, they can remove a stone from the pile and receive points based on the stone's value. Alice and Bob may value the stones differently.
