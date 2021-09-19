fe0c90f7234b61343692d0a6ac69fd5be9164151
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-19 08:33:43 +0300

    1700. Number of Students Unable to Eat Lunch

--- 1700. Number of Students Unable to Eat Lunch.swift
+++ 1700. Number of Students Unable to Eat Lunch.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1700. Number of Students Unable to Eat Lunch
     // The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively. All students stand in a queue. Each student either prefers square or circular sandwiches.
     // The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches are placed in a stack. At each step:
