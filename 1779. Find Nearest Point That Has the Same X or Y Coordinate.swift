3f477930c65bbf648a51915146c8294909c2bbea
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:46:52 +0300

    1779. Find Nearest Point That Has the Same X or Y Coordinate

--- 1779. Find Nearest Point That Has the Same X or Y Coordinate.swift
+++ 1779. Find Nearest Point That Has the Same X or Y Coordinate.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1779. Find Nearest Point That Has the Same X or Y Coordinate
     // You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y). You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi). A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
     // Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location. If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
