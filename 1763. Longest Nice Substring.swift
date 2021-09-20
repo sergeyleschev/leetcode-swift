c348f8af8ae825e351d8ff1e954431611939023f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:44:35 +0300

    1763. Longest Nice Substring

--- 1763. Longest Nice Substring.swift
+++ 1763. Longest Nice Substring.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1763. Longest Nice Substring
     // A string s is nice if, for every letter of the alphabet that s contains, it appears both in uppercase and lowercase. For example, "abABB" is nice because 'A' and 'a' appear, and 'B' and 'b' appear. However, "abA" is not because 'b' appears, but 'B' does not.
     // Given a string s, return the longest substring of s that is nice. If there are multiple, return the substring of the earliest occurrence. If there are none, return an empty string.
