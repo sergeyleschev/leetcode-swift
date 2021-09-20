80789acd295b504a0d49a8dfe44c3e9055dfa4df
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:45:37 +0300

    1769. Minimum Number of Operations to Move All Balls to Each Box

--- 1769. Minimum Number of Operations to Move All Balls to Each Box.swift
+++ 1769. Minimum Number of Operations to Move All Balls to Each Box.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1769. Minimum Number of Operations to Move All Balls to Each Box
     // You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
     // In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
