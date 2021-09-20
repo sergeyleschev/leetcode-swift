a2d190244a5879dfdbf045c6162242ed36cfa7e0
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:40:52 +0300

    1734. Decode XORed Permutation

--- 1734. Decode XORed Permutation.swift
+++ 1734. Decode XORed Permutation.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1734. Decode XORed Permutation
     // There is an integer array perm that is a permutation of the first n positive integers, where n is always odd.
     // It was encoded into another integer array encoded of length n - 1, such that encoded[i] = perm[i] XOR perm[i + 1]. For example, if perm = [1,3,2], then encoded = [2,1].
