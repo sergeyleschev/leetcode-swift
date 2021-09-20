740561ea24870bc78ae84319640ce1692e228269
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:40:08 +0300

    1732. Find the Highest Altitude

--- 1732. Find the Highest Altitude.swift
+++ 1732. Find the Highest Altitude.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1732. Find the Highest Altitude
     // There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
     // You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
