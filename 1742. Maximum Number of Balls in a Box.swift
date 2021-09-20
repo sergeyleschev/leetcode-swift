4ed383e1d8ea9e12e18e13ed2b3e2f54d22398f5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:41:44 +0300

    1742. Maximum Number of Balls in a Box

--- 1742. Maximum Number of Balls in a Box.swift
+++ 1742. Maximum Number of Balls in a Box.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1742. Maximum Number of Balls in a Box
     // You are working in a ball factory where you have n balls numbered from lowLimit up to highLimit inclusive (i.e., n == highLimit - lowLimit + 1), and an infinite number of boxes numbered from 1 to infinity.
     // Your job at this factory is to put each ball in the box with a number equal to the sum of digits of the ball's number. For example, the ball number 321 will be put in the box number 3 + 2 + 1 = 6 and the ball number 10 will be put in the box number 1 + 0 = 1.
