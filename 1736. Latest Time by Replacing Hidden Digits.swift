e37462c064a97b2d5dc854272217f62a9b9706f7
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:41:02 +0300

    1736. Latest Time by Replacing Hidden Digits

--- 1736. Latest Time by Replacing Hidden Digits.swift
+++ 1736. Latest Time by Replacing Hidden Digits.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1736. Latest Time by Replacing Hidden Digits
     // You are given a string time in the form of hh:mm, where some of the digits in the string are hidden (represented by ?).
     // The valid times are those inclusively between 00:00 and 23:59.
