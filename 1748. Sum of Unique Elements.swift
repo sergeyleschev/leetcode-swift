f73397a970b71ac5dae1dd89747d9c2ea50a2f56
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:42:26 +0300

    1748. Sum of Unique Elements

--- 1748. Sum of Unique Elements.swift
+++ 1748. Sum of Unique Elements.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1748. Sum of Unique Elements
     // You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
 
