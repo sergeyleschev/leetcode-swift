8ab54fa89024e43b9b7e83e4108c12efac38086f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:47:04 +0300

    1780. Check if Number is a Sum of Powers of Three

--- 1780. Check if Number is a Sum of Powers of Three.swift
+++ 1780. Check if Number is a Sum of Powers of Three.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1780. Check if Number is a Sum of Powers of Three
     // Given an integer n, return true if it is possible to represent n as the sum of distinct powers of three. Otherwise, return false.
     // An integer y is a power of three if there exists an integer x such that y == 3x.
