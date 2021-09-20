aa17563d9264426868136d623fc839d2149559bc
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-20 07:46:16 +0300

    1773. Count Items Matching a Rule

--- 1773. Count Items Matching a Rule.swift
+++ 1773. Count Items Matching a Rule.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1773. Count Items Matching a Rule
     // You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
     // The ith item is said to match the rule if one of the following is true:
