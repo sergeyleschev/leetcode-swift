a702b8c8b8da6b493b8725fd3acda4624f608782
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 07:59:18 +0300

    1790. Check if One String Swap Can Make Strings Equal

--- 1790. Check if One String Swap Can Make Strings Equal.swift
+++ 1790. Check if One String Swap Can Make Strings Equal.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1790. Check if One String Swap Can Make Strings Equal
     // You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
 
