f6ea0800e6387b910da4e615dd512fe1884ebb74
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:06:10 +0300

    1838. Frequency of the Most Frequent Element

--- 1838. Frequency of the Most Frequent Element.swift
+++ 1838. Frequency of the Most Frequent Element.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1838. Frequency of the Most Frequent Element
     // The frequency of an element is the number of times it occurs in an array.
     // You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment the element at that index by 1.
