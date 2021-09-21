d444c1cef42d7421d783c71c62251366fed4108f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:02:06 +0300

    1815. Maximum Number of Groups Getting Fresh Donuts

--- 1816. Truncate Sentence.swift
+++ 1816. Truncate Sentence.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1816. Truncate Sentence
     // A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
     // For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
