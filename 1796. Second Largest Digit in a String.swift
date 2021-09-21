9a5bdcc9d0a3466201d5d320e9277640d60847c7
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 07:59:38 +0300

    1796. Second Largest Digit in a String

--- 1796. Second Largest Digit in a String.swift
+++ 1796. Second Largest Digit in a String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1796. Second Largest Digit in a String
     // Given an alphanumeric string s, return the second largest numerical digit that appears in s, or -1 if it does not exist.
     // An alphanumeric string is a string consisting of lowercase English letters and digits.
