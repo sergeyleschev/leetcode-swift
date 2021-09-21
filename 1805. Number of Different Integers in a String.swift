5d52de49dd784512dcb6073afd078fcedaa1b85f
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:00:44 +0300

    1805. Number of Different Integers in a String

--- 1805. Number of Different Integers in a String.swift
+++ 1805. Number of Different Integers in a String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1805. Number of Different Integers in a String
     // You are given a string word that consists of digits and lowercase English letters.
     // You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34". Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
