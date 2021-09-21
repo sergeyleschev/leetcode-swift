334dc6110d84b2e92058869cdab772a172c01c89
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 07:59:28 +0300

    1791. Find Center of Star Graph

--- 1791. Find Center of Star Graph.swift
+++ 1791. Find Center of Star Graph.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1791. Find Center of Star Graph
     // There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
     // You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
