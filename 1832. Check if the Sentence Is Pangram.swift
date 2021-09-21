5ca28f8ecb202a906d8ce9bdcf0e834d9084cabf
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:04:54 +0300

    1832. Check if the Sentence Is Pangram

--- 1832. Check if the Sentence Is Pangram.swift
+++ 1832. Check if the Sentence Is Pangram.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1832. Check if the Sentence Is Pangram
     // A pangram is a sentence where every letter of the English alphabet appears at least once.
     // Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
