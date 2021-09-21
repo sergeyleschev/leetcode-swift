19d8689bd85abee34a9380924a1981ee92c58553
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:04:32 +0300

    1829. Maximum XOR for Each Query

--- 1829. Maximum XOR for Each Query.swift
+++ 1829. Maximum XOR for Each Query.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1829. Maximum XOR for Each Query
     // You are given a sorted array nums of n non-negative integers and an integer maximumBit. You want to perform the following query n times:
     // Find a non-negative integer k < 2maximumBit such that nums[0] XOR nums[1] XOR ... XOR nums[nums.length-1] XOR k is maximized. k is the answer to the ith query.
