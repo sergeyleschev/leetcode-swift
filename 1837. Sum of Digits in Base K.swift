72a961db460bae171b2ad14d5f56cb16e28fb2e5
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:05:50 +0300

    1837. Sum of Digits in Base K

--- 1837. Sum of Digits in Base K.swift
+++ 1837. Sum of Digits in Base K.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1837. Sum of Digits in Base K
     // Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
     // After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
