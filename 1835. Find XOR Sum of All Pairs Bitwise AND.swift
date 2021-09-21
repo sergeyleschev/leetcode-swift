ffa3017d5a23b850bfbea64916b0ea58bdcbe6de
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:05:27 +0300

    1835. Find XOR Sum of All Pairs Bitwise AND

--- 1835. Find XOR Sum of All Pairs Bitwise AND.swift
+++ 1835. Find XOR Sum of All Pairs Bitwise AND.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1835. Find XOR Sum of All Pairs Bitwise AND
     // The XOR sum of a list is the bitwise XOR of all its elements. If the list only contains one element, then its XOR sum will be equal to this element.
     // For example, the XOR sum of [1,2,3,4] is equal to 1 XOR 2 XOR 3 XOR 4 = 4, and the XOR sum of [3] is equal to 3.
