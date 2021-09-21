46ccf0723c4c3a2a902074ddc87bebf0f096bc39
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:01:37 +0300

    1813. Sentence Similarity III

--- 1813. Sentence Similarity III.swift
+++ 1813. Sentence Similarity III.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1813. Sentence Similarity III
     // A sentence is a list of words that are separated by a single space with no leading or trailing spaces. For example, "Hello World", "HELLO", "hello world hello world" are all sentences. Words consist of only uppercase and lowercase English letters.
     // Two sentences sentence1 and sentence2 are similar if it is possible to insert an arbitrary sentence (possibly empty) inside one of these sentences such that the two sentences become equal. For example, sentence1 = "Hello my name is Jane" and sentence2 = "Hello Jane" can be made equal by inserting "my name is" between "Hello" and "Jane" in sentence2.
