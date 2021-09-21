0c8893c35740c8038e66e1a274e3d4914dff73aa
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:00:07 +0300

    1800. Maximum Ascending Subarray Sum

--- 1800. Maximum Ascending Subarray Sum.swift
+++ 1800. Maximum Ascending Subarray Sum.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1800. Maximum Ascending Subarray Sum
     // Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
     // A subarray is defined as a contiguous sequence of numbers in an array.
