a1d525887a00d464c7a13c4e8f8711bf2f583a2a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:05:06 +0300

    1833. Maximum Ice Cream Bars

--- 1833. Maximum Ice Cream Bars.swift
+++ 1833. Maximum Ice Cream Bars.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1833. Maximum Ice Cream Bars
     // It is a sweltering summer day, and a boy wants to buy some ice cream bars.
     // At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
