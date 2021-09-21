c5b23183c5d2fab007f277e265c864f959aaec2a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-21 08:01:46 +0300

    1814. Count Nice Pairs in an Array

--- 1814. Count Nice Pairs in an Array.swift
+++ 1814. Count Nice Pairs in an Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1814. Count Nice Pairs in an Array
     // You are given an array nums that consists of non-negative integers. Let us define rev(x) as the reverse of the non-negative integer x. For example, rev(123) = 321, and rev(120) = 21. A pair of indices (i, j) is nice if it satisfies all of the following conditions:
     // 0 <= i < j < nums.length
