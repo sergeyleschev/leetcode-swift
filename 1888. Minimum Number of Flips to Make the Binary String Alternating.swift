b54b663cba853dad9a906fdb39ff501d2308f7d7
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:09:55 +0300

    1888. Minimum Number of Flips to Make the Binary String Alternating

--- 1888. Minimum Number of Flips to Make the Binary String Alternating.swift
+++ 1888. Minimum Number of Flips to Make the Binary String Alternating.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1888. Minimum Number of Flips to Make the Binary String Alternating
     // You are given a binary string s. You are allowed to perform two types of operations on the string in any sequence:
     // Type-1: Remove the character at the start of the string s and append it to the end of the string.
