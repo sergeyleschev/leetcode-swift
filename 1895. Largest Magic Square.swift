d12bf3863277f660705e8900ac3f6e0be4327b37
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:11:06 +0300

    1895. Largest Magic Square

--- 1895. Largest Magic Square.swift
+++ 1895. Largest Magic Square.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1895. Largest Magic Square
     // A k x k magic square is a k x k grid filled with integers such that every row sum, every column sum, and both diagonal sums are all equal. The integers in the magic square do not have to be distinct. Every 1 x 1 grid is trivially a magic square.
     // Given an m x n integer grid, return the size (i.e., the side length k) of the largest magic square that can be found within this grid.
