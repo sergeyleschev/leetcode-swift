7b979b26910d1a415662a163e9ff2fa6a4ee117d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:09:24 +0300

    1884. Egg Drop With 2 Eggs and N Floors

--- 1884. Egg Drop With 2 Eggs and N Floors.swift
+++ 1884. Egg Drop With 2 Eggs and N Floors.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1884. Egg Drop With 2 Eggs and N Floors
     // You are given two identical eggs and you have access to a building with n floors labeled from 1 to n.
     // You know that there exists a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or below floor f will not break.
