fde4926ed029ad4257f2860e629605714bbefe58
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:09:43 +0300

    1887. Reduction Operations to Make the Array Elements Equal

--- 1887. Reduction Operations to Make the Array Elements Equal.swift
+++ 1887. Reduction Operations to Make the Array Elements Equal.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1887. Reduction Operations to Make the Array Elements Equal
     // Given an integer array nums, your goal is to make all elements in nums equal. To complete one operation, follow these steps:
     // Find the largest value in nums. Let its index be i (0-indexed) and its value be largest. If there are multiple elements with the largest value, pick the smallest i.
