bf6072094dd98ab9dc94c6f7431cb5cf012b75c4
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:04:43 +0300

    1854. Maximum Population Year

--- 1854. Maximum Population Year.swift
+++ 1854. Maximum Population Year.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1854. Maximum Population Year
     // You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
     // The population of some year x is the number of people alive during that year. The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. Note that the person is not counted in the year that they die.
