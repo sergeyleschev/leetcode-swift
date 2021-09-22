2c27dc5dfba61a8e848e79b9cf16db8b1f51eef6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:05:12 +0300

    1857. Largest Color Value in a Directed Graph

--- 1857. Largest Color Value in a Directed Graph.swift
+++ 1857. Largest Color Value in a Directed Graph.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1857. Largest Color Value in a Directed Graph
     // There is a directed graph of n colored nodes and m edges. The nodes are numbered from 0 to n - 1.
     // You are given a string colors where colors[i] is a lowercase English letter representing the color of the ith node in this graph (0-indexed). You are also given a 2D array edges where edges[j] = [aj, bj] indicates that there is a directed edge from node aj to node bj.
