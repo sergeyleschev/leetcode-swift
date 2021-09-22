a284629352baa66bde0ae92160ae062685742fd2
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:08:28 +0300

    1878. Get Biggest Three Rhombus Sums in a Grid

--- 1878. Get Biggest Three Rhombus Sums in a Grid.swift
+++ 1878. Get Biggest Three Rhombus Sums in a Grid.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1878. Get Biggest Three Rhombus Sums in a Grid
     // You are given an m x n integer matrix grid.
     // A rhombus sum is the sum of the elements that form the border of a regular rhombus shape in grid. The rhombus must have the shape of a square rotated 45 degrees with each of the corners centered in a grid cell. Below is an image of four valid rhombus shapes with the corresponding colored cells that should be included in each rhombus sum:
