de3400b5ea3ecaf92e735e78962b773bb41085a7
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:07:43 +0300

    1871. Jump Game VII

--- 1871. Jump Game VII.swift
+++ 1871. Jump Game VII.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1871. Jump Game VII
     // You are given a 0-indexed binary string s and two integers minJump and maxJump. In the beginning, you are standing at index 0, which is equal to '0'. You can move from index i to index j if the following conditions are fulfilled:
     // i + minJump <= j <= min(i + maxJump, s.length - 1), and
