ab352ce34e0fa54d6e3e5fe1650c66fbf0b95a26
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:10:44 +0300

    1894. Find the Student that Will Replace the Chalk

--- 1894. Find the Student that Will Replace the Chalk.swift
+++ 1894. Find the Student that Will Replace the Chalk.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1894. Find the Student that Will Replace the Chalk
     // There are n students in a class numbered from 0 to n - 1. The teacher will give each student a problem starting with the student number 0, then the student number 1, and so on until the teacher reaches the student number n - 1. After that, the teacher will restart the process, starting with the student number 0 again.
     // You are given a 0-indexed integer array chalk and an integer k. There are initially k pieces of chalk. When the student number i is given a problem to solve, they will use chalk[i] pieces of chalk to solve that problem. However, if the current number of chalk pieces is strictly less than chalk[i], then the student number i will be asked to replace the chalk.
