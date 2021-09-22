004b7b1e2e3b1252342d8f5e0b21ac2d206e3af8
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:10:32 +0300

    1893. Check if All the Integers in a Range Are Covered

--- 1893. Check if All the Integers in a Range Are Covered.swift
+++ 1893. Check if All the Integers in a Range Are Covered.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1893. Check if All the Integers in a Range Are Covered
     // You are given a 2D integer array ranges and two integers left and right. Each ranges[i] = [starti, endi] represents an inclusive interval between starti and endi.
     // Return true if each integer in the inclusive range [left, right] is covered by at least one interval in ranges. Return false otherwise.
