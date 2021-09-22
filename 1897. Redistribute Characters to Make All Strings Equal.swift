6b8d237464103b6770febac3f336ea5915f34f4d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:12:39 +0300

    1897. Redistribute Characters to Make All Strings Equal

--- 1897. Redistribute Characters to Make All Strings Equal.swift
+++ 1897. Redistribute Characters to Make All Strings Equal.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 1897. Redistribute Characters to Make All Strings Equal
 	// You are given an array of strings words (0-indexed).
 	// In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
