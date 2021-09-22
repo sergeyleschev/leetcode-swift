6482b63a624cb2a13126c7e6201b6fdca267759a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:08:49 +0300

    1880. Check if Word Equals Summation of Two Words

--- 1880. Check if Word Equals Summation of Two Words.swift
+++ 1880. Check if Word Equals Summation of Two Words.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1880. Check if Word Equals Summation of Two Words
     // The letter value of a letter is its position in the alphabet starting from 0 (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, etc.).
     // The numerical value of some string of lowercase English letters s is the concatenation of the letter values of each letter in s, which is then converted into an integer.
