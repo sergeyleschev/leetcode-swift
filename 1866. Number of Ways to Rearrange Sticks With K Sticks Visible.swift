292ac50669c0488f6af53463443d37172e57d4de
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:07:05 +0300

    1866. Number of Ways to Rearrange Sticks With K Sticks Visible

--- 1866. Number of Ways to Rearrange Sticks With K Sticks Visible.swift
+++ 1866. Number of Ways to Rearrange Sticks With K Sticks Visible.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1866. Number of Ways to Rearrange Sticks With K Sticks Visible
     // There are n uniquely-sized sticks whose lengths are integers from 1 to n. You want to arrange the sticks such that exactly k sticks are visible from the left. A stick is visible from the left if there are no longer sticks to the left of it.
     // For example, if the sticks are arranged [1,3,2,5,4], then the sticks with lengths 1, 3, and 5 are visible from the left.
