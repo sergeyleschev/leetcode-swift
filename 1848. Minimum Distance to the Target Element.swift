f0b50ee03dc2ce78828960a1441c9ab1bc197010
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-22 08:04:09 +0300

    1848. Minimum Distance to the Target Element

--- 1848. Minimum Distance to the Target Element.swift
+++ 1848. Minimum Distance to the Target Element.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1848. Minimum Distance to the Target Element
     // Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
     // Return abs(i - start).
