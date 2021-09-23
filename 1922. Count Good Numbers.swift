ffeae4479c52acf9a4dfe86ac252acbb313b2d6e
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:07:42 +0300

    1922. Count Good Numbers

--- 1922. Count Good Numbers.swift
+++ 1922. Count Good Numbers.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1922. Count Good Numbers
     // A digit string is good if the digits (0-indexed) at even indices are even and the digits at odd indices are prime (2, 3, 5, or 7).
     // For example, "2582" is good because the digits (2 and 8) at even positions are even and the digits (5 and 2) at odd positions are prime. However, "3245" is not good because 3 is at an even index but is not even.
