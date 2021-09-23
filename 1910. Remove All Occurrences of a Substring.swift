7a7501f4954d88b21f91da0a44eb946e6ae37748
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:03:40 +0300

    1910. Remove All Occurrences of a Substring

--- 1910. Remove All Occurrences of a Substring.swift
+++ 1910. Remove All Occurrences of a Substring.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 1910. Remove All Occurrences of a Substring
 	// Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
 	// Find the leftmost occurrence of the substring part and remove it from s.
