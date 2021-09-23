17481e32abd1a3284905b5d12785d3086ac220c6
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:09:12 +0300

    1930. Unique Length-3 Palindromic Subsequences

--- 1930. Unique Length-3 Palindromic Subsequences.swift
+++ 1930. Unique Length-3 Palindromic Subsequences.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 1930. Unique Length-3 Palindromic Subsequences
 	// Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
 	// Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
