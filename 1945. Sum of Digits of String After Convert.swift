162510cd330bf685eda79c96e194840582600c24
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:11:27 +0300

    1945. Sum of Digits of String After Convert

--- 1945. Sum of Digits of String After Convert.swift
+++ 1945. Sum of Digits of String After Convert.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1945. Sum of Digits of String After Convert
     // You are given a string s consisting of lowercase English letters, and an integer k.
     // First, convert s into an integer by replacing each letter with its position in the alphabet (i.e., replace 'a' with 1, 'b' with 2, ..., 'z' with 26). Then, transform the integer by replacing it with the sum of its digits. Repeat the transform operation k times in total.
