56967df4f98b169d660cb836f49ce5f1a8f8d90d
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:08:13 +0300

    1926. Nearest Exit from Entrance in Maze

--- 1926. Nearest Exit from Entrance in Maze.swift
+++ 1926. Nearest Exit from Entrance in Maze.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+	// Solution @ Sergey Leschev, Belarusian State University
+
 	// 1926. Nearest Exit from Entrance in Maze
 	// You are given an m x n matrix maze (0-indexed) with empty cells (represented as '.') and walls (represented as '+'). You are also given the entrance of the maze, where entrance = [entrancerow, entrancecol] denotes the row and column of the cell you are initially standing at.
 	// In one step, you can move one cell up, down, left, or right. You cannot step into a cell with a wall, and you cannot step outside the maze. Your goal is to find the nearest exit from the entrance. An exit is defined as an empty cell that is at the border of the maze. The entrance does not count as an exit.
