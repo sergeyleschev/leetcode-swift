93355cd13dfabaea433bc06ffbf08ea8f587ddab
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-24 00:29:28 +0300

    1961. Check If String Is a Prefix of Array

--- 1961. Check If String Is a Prefix of Array.swift
+++ 1961. Check If String Is a Prefix of Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1961. Check If String Is a Prefix of Array
     // Given a string s and an array of strings words, determine whether s is a prefix string of words.
     // A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
