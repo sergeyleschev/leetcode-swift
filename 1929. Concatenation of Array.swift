cc246b38a9984332da593e0079876d0b66aed596
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:09:03 +0300

    1929. Concatenation of Array

--- 1929. Concatenation of Array.swift
+++ 1929. Concatenation of Array.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1929. Concatenation of Array
     // Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
     // Specifically, ans is the concatenation of two nums arrays.
