0a73888e324f67f284899e42789b1482ad1ca5bc
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:02:48 +0300

    1903. Largest Odd Number in String

--- 1903. Largest Odd Number in String.swift
+++ 1903. Largest Odd Number in String.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1903. Largest Odd Number in String
     // You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
     // A substring is a contiguous sequence of characters within a string.
