179a3c36a052aaad75bcf05e357f507ea3b91976
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:11:06 +0300

    1944. Number of Visible People in a Queue

--- 1944. Number of Visible People in a Queue.swift
+++ 1944. Number of Visible People in a Queue.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1944. Number of Visible People in a Queue
     // There are n people standing in a queue, and they numbered from 0 to n - 1 in left to right order. You are given an array heights of distinct integers where heights[i] represents the height of the ith person.
     // A person can see another person to their right in the queue if everybody in between is shorter than both of them. More formally, the ith person can see the jth person if i < j and min(heights[i], heights[j]) > max(heights[i+1], heights[i+2], ..., heights[j-1]).
