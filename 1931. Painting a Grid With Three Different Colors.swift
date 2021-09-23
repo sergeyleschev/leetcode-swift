ea87c4fe7680e062ff3dfcd8ab4025c0ac6fb431
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:09:22 +0300

    1931. Painting a Grid With Three Different Colors

--- 1931. Painting a Grid With Three Different Colors.swift
+++ 1931. Painting a Grid With Three Different Colors.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1931. Painting a Grid With Three Different Colors
     // You are given two integers m and n. Consider an m x n grid where each cell is initially white. You can paint each cell red, green, or blue. All cells must be painted.
     // Return the number of ways to color the grid with no two adjacent cells having the same color. Since the answer can be very large, return it modulo 10^9 + 7.
