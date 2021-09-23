f0f809913222fdc91644bd7a3c12cb8558eabc69
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:03:06 +0300

    1905. Count Sub Islands

--- 1905. Count Sub Islands.swift
+++ 1905. Count Sub Islands.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1905. Count Sub Islands
     // You are given two m x n binary matrices grid1 and grid2 containing only 0's (representing water) and 1's (representing land). An island is a group of 1's connected 4-directionally (horizontal or vertical). Any cells outside of the grid are considered water cells.
     // An island in grid2 is considered a sub-island if there is an island in grid1 that contains all the cells that make up this island in grid2.
