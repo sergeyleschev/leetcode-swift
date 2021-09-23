ea1df3b54f633f52d56c93cd9ea03f4f03fbdf17
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:07:23 +0300

    1920. Build Array from Permutation

--- 1920. Build Array from Permutation.swift
+++ 1920. Build Array from Permutation.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1920. Build Array from Permutation
     // Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
     // A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
