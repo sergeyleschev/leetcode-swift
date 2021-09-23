1437b030a9cbad73e32ac449424815d225a3b048
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:08:03 +0300

    1925. Count Square Sum Triples

--- 1925. Count Square Sum Triples.swift
+++ 1925. Count Square Sum Triples.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1925. Count Square Sum Triples
     // A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
     // Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
