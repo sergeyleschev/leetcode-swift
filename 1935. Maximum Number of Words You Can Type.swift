797a7733b275b8b7263cfba202149727a9c03e00
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:09:53 +0300

    1935. Maximum Number of Words You Can Type

--- 1935. Maximum Number of Words You Can Type.swift
+++ 1935. Maximum Number of Words You Can Type.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1935. Maximum Number of Words You Can Type
     // There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
     // Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
