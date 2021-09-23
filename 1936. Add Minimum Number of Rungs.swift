162132139ac70b8ed4a2e1e959a7aaebbce1393a
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:10:02 +0300

    1936. Add Minimum Number of Rungs

--- 1936. Add Minimum Number of Rungs.swift
+++ 1936. Add Minimum Number of Rungs.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1936. Add Minimum Number of Rungs
     // You are given a strictly increasing integer array rungs that represents the height of rungs on a ladder. You are currently on the floor at height 0, and you want to reach the last rung.
     // You are also given an integer dist. You can only climb to the next highest rung if the distance between where you are currently at (the floor or on a rung) and the next rung is at most dist. You are able to insert rungs at any positive integer height if a rung is not already there.
