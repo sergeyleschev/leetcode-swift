98c293603cbfd514c96ac56cecd78564d02a90b2
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-24 00:29:39 +0300

    1963. Minimum Number of Swaps to Make the String Balanced

--- 1963. Minimum Number of Swaps to Make the String Balanced.swift
+++ 1963. Minimum Number of Swaps to Make the String Balanced.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1963. Minimum Number of Swaps to Make the String Balanced
     // You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
     // A string is called balanced if and only if:
