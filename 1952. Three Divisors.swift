1f5d048504822ae646b5aced00539b2ecd8ac62b
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:12:20 +0300

    1952. Three Divisors

--- 1952. Three Divisors.swift
+++ 1952. Three Divisors.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1952. Three Divisors
     // Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
     // An integer m is a divisor of n if there exists an integer k such that n = k * m.
