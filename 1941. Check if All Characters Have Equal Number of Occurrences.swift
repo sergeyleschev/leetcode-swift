a7b1c8f2adf1f23fc3c8f0a5329494eb1e1290d4
Sergey Leschev <sergey.leschev@gmail.com>
2021-09-23 08:10:30 +0300

    1941. Check if All Characters Have Equal Number of Occurrences

--- 1941. Check if All Characters Have Equal Number of Occurrences.swift
+++ 1941. Check if All Characters Have Equal Number of Occurrences.swift
@@ -1,5 +1,7 @@
 class Solution {
 
+    // Solution @ Sergey Leschev, Belarusian State University
+
     // 1941. Check if All Characters Have Equal Number of Occurrences
     // Given a string s, return true if s is a good string, or false otherwise.
     // A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
