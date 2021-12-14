6ec947be3c8d9def024a73279c7c45ec9a804789
Sergey Leschev <sergey.leschev@gmail.com>
2021-12-14 05:40:20 +0300

    0006. ZigZag Conversion

--- 0006. ZigZag Conversion.swift
+++ 0006. ZigZag Conversion.swift
@@ -1,6 +1,6 @@
 class Solution {
 
-    // Created by Sergey Leschev, Belarusian State University
+    // Solution @ Sergey Leschev, Belarusian State University
 
     // 6. ZigZag Conversion
     // The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
