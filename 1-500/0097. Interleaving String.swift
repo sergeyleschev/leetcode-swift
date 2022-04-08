bc901204e4a38528e3338619dd6240760a1dc49f
Sergey Leschev <sergey.leschev@gmail.com>
2022-04-08 22:01:59 +0300

    97. Interleaving String

--- 1-500/0097. Interleaving String.swift
+++ 1-500/0097. Interleaving String.swift
@@ -36,15 +36,15 @@ class Solution {
         
         func is_interleave(_ chs1: [Character], _ chs2: [Character], _ chs3: [Character], _ i: Int, _ j: Int, _ k: Int) -> Bool {
             if let v = dp["\(i)-\(j)-\(k)"] { return v }
-            
+            var v1 = false
+            var v2 = false
+
             if i >= chs1.count && j >= chs2.count && k >= chs3.count { return true }
             
-            var v1 = false
             if i < chs1.count && chs1[i] == chs3[k] {
                 v1 = is_interleave(chs1, chs2, chs3, i + 1, j, k + 1)
             }
             
-            var v2 = false
             if j < chs2.count && chs2[j] == chs3[k] {
                 v2 = v1 || is_interleave(chs1, chs2, chs3, i, j + 1, k + 1)
             }
