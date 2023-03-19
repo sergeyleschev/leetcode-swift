02843742c8543784a3c7b4a77e52759011c8fd97
Sergey Leschev <sergey.leschev@gmail.com>
2023-03-19 11:28:14 +0300

    2535. Difference Between Element Sum and Digit Sum of an Array

---
+++ 2001-2500/2535. Difference Between Element Sum and Digit Sum of an Array.swift
@@ -0,0 +1,19 @@
+// Solution @ Sergey Leschev
+
+class Solution {
+    func differenceOfSum(_ nums: [Int]) -> Int {
+        var elementSum = 0
+        var digitSum = 0
+        
+        for num in nums {
+            elementSum += num
+            var currentNum = num
+            while currentNum > 0 {
+                digitSum += currentNum % 10
+                currentNum /= 10
+            }
+        }
+        
+        return abs(elementSum - digitSum)
+    }
+}
