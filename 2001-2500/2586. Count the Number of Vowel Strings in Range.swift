ef79a4f61d8a76f7b9587e098da71010a5d6a6ba
Sergey Leschev <sergey.leschev@gmail.com>
2023-03-19 12:01:38 +0300

    2586. Count the Number of Vowel Strings in Range

---
+++ 2001-2500/2586. Count the Number of Vowel Strings in Range.swift
@@ -0,0 +1,21 @@
+class Solution {
+
+    // Solution @ Sergey Leschev
+
+    func vowelStrings(_ words: [String], _ left: Int, _ right: Int) -> Int {
+        let vowels: Set<Character> = ["a", "e", "i", "o", "u"]
+        var count = 0
+        
+        // Iterate over words in the range [left, right]
+        for i in left...right {
+            let word = words[i]
+            
+            // Check if the first and last characters of the word are vowels
+            if vowels.contains(word.first ?? Character("")) && vowels.contains(word.last ?? Character("")) {
+                count += 1
+            }
+        }
+        
+        return count
+    }
+}
