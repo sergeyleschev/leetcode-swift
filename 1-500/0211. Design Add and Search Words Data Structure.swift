4e7472eaae7b1d04b863b69ab2dd954082462d59
Sergey Leschev <sergey.leschev@gmail.com>
2023-03-19 10:26:40 +0300

    211. Design Add and Search Words Data Structure

--- 1-500/0211. Design Add and Search Words Data Structure.swift
+++ 1-500/0211. Design Add and Search Words Data Structure.swift
@@ -1,118 +1,88 @@
-class WordDictionary {
+// Solution @ Sergey Leschev, Belarusian State University
 
-    // Solution @ Sergey Leschev, Belarusian State University
+// 211. Design Add and Search Words Data Structure
+// Design a data structure that supports adding new words and finding if a string matches any previously added string.
+// Implement the WordDictionary class:
+// WordDictionary() Initializes the object.
+// void addWord(word) Adds word to the data structure, it can be matched later.
+// bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
 
-    // 211. Design Add and Search Words Data Structure
-    // Design a data structure that supports adding new words and finding if a string matches any previously added string.
-    // Implement the WordDictionary class:
-    // WordDictionary() Initializes the object.
-    // void addWord(word) Adds word to the data structure, it can be matched later.
-    // bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+// Example:
+// Input
+// ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+// [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+// Output
+// [null,null,null,null,false,true,true,true]
+// Explanation
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("bad");
+// wordDictionary.addWord("dad");
+// wordDictionary.addWord("mad");
+// wordDictionary.search("pad"); // return False
+// wordDictionary.search("bad"); // return True
+// wordDictionary.search(".ad"); // return True
+// wordDictionary.search("b.."); // return True
 
-    // Example:
-    // Input
-    // ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
-    // [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
-    // Output
-    // [null,null,null,null,false,true,true,true]
-    // Explanation
-    // WordDictionary wordDictionary = new WordDictionary();
-    // wordDictionary.addWord("bad");
-    // wordDictionary.addWord("dad");
-    // wordDictionary.addWord("mad");
-    // wordDictionary.search("pad"); // return False
-    // wordDictionary.search("bad"); // return True
-    // wordDictionary.search(".ad"); // return True
-    // wordDictionary.search("b.."); // return True
+// Constraints:
+// 1 <= word.length <= 500
+// word in addWord consists lower-case English letters.
+// word in search consist of  '.' or lower-case English letters.
+// At most 50000 calls will be made to addWord and search.
 
-    // Constraints:
-    // 1 <= word.length <= 500
-    // word in addWord consists lower-case English letters.
-    // word in search consist of  '.' or lower-case English letters.
-    // At most 50000 calls will be made to addWord and search.
-
-    private var trie: TrieNode
+class TrieNode {
+    var children: [Character: TrieNode]
+    var isWord: Bool
 
     init() {
-        trie = TrieNode()
+        children = [:]
+        isWord = false
     }
+}
 
+class WordDictionary {
+    var root: TrieNode
 
-    // Adds a word into the data structure.
-    // - Parameter word: The word.
-
-    // - Complexity:
-    //   - time: O(n), where n is the length of the word.
-    //   - space: O(n), where n is the length of the word.
+    init() {
+        root = TrieNode()
+    }
 
     func addWord(_ word: String) {
-        var node = trie
-
-        for ch in word {
-            if node.children[ch] == nil {
-                node.children[ch] = TrieNode()
-            }
-
-            if let child = node.children[ch] {
+        var node = root
+        for c in word {
+            if let child = node.children[c] {
+                node = child
+            } else {
+                let child = TrieNode()
+                node.children[c] = child
                 node = child
             }
         }
-        node.word = true
+        node.isWord = true
     }
-
-
-    // Searches for a word in the data structure.
-    // - Parameter word: The word.
-    // - Returns: True if data structure contains the word, otherwise returns false.
-
-    // - Complexity:
-    //   - time: O(n), where n is the length of the word.
-    //   - space: O(n), where n is the length of the word.
 
     func search(_ word: String) -> Bool {
-        return searchInNode(word, trie)
+        return searchHelper(Array(word), 0, root)
     }
 
-
-    private func searchInNode(_ word: String, _ node: TrieNode) -> Bool {
-        var node = node
-        let word = Array(word)
-
-        for i in 0..<word.count {
-            if let child = node.children[word[i]] {
-                node = child
-
-            } else {
-                guard word[i] == "." else { return false }
-
-                for child in node.children.values {
-                    guard searchInNode(String(word.dropFirst(i + 1)), child) else { continue }
-                    return true
-                }
-
-                return false
-            }
+    private func searchHelper(_ word: [Character], _ index: Int, _ node: TrieNode) -> Bool {
+        if index == word.count {
+            return node.isWord
         }
 
-        return node.word
+        let c = word[index]
+
+        if c != "." {
+            if let child = node.children[c] {
+                return searchHelper(word, index+1, child)
+            }
+            return false
+        } else {
+            for child in node.children.values {
+                if searchHelper(word, index+1, child) {
+                    return true
+                }
+            }
+            return false
+        }
     }
 }
-
-
-final class TrieNode {
-    var children: [Character: TrieNode]
-    var word: Bool
-
-    init(children: [Character: TrieNode] = [Character: TrieNode](), word: Bool = false) {
-        self.children = children
-        self.word = word
-    }
-
-}
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * let obj = WordDictionary()
- * obj.addWord(word)
- * let ret_2: Bool = obj.search(word)
- */
