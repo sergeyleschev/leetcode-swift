d13e5b17b43bf218612d62a7d4868683928faf5d
Sergey Leschev <sergey.leschev@gmail.com>
2023-03-19 11:47:37 +0300

    2562. Find the Array Concatenation Value

---
+++ 2001-2500/2562. Find the Array Concatenation Value.swift
@@ -0,0 +1,23 @@
+class Solution {
+
+    // Solution @ Sergey Leschev
+
+    func findTheArrayConcVal(_ nums: [Int]) -> Int {
+        var concatValue = 0
+        var nums = nums
+        
+        while nums.count > 0 {
+            if nums.count == 1 {
+                concatValue += nums[0]
+                nums.removeFirst()
+            } else {
+                let concatNum = Int(String(nums[0]) + String(nums[nums.count - 1]))!
+                concatValue += concatNum
+                nums.removeFirst()
+                nums.removeLast()
+            }
+        }
+        
+        return concatValue
+    }
+}
