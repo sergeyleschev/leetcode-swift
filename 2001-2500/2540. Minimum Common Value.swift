2518596906d25a0013dd7ef6da0bc511f746a329
Sergey Leschev <sergey.leschev@gmail.com>
2023-03-19 11:29:59 +0300

    2540. Minimum Common Value

---
+++ 2001-2500/2540. Minimum Common Value.swift
@@ -0,0 +1,17 @@
+// Solution @ Sergey Leschev
+
+class Solution {
+    func getCommon(_ nums1: [Int], _ nums2: [Int]) -> Int {
+        var i = 0, j = 0
+        while i < nums1.count && j < nums2.count {
+            if nums1[i] == nums2[j] {
+                return nums1[i]
+            } else if nums1[i] < nums2[j] {
+                i += 1
+            } else {
+                j += 1
+            }
+        }
+        return -1
+    }
+}
