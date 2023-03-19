814c0ee2d18e145e3dc47d335219b7ca3abc2ff2
Sergey Leschev <sergey.leschev@gmail.com>
2023-03-19 11:49:40 +0300

    2566. Maximum Difference by Remapping a Digit

---
+++ 2001-2500/2566. Maximum Difference by Remapping a Digit.swift
@@ -0,0 +1,33 @@
+class Solution {
+
+    // Solution @ Sergey Leschev
+
+    func minMaxDifference(_ num: Int) -> Int {
+        var minNum = num
+        var maxNum = num
+        
+        // Convert num to an array of digits
+        var digits = Array(String(num)).map { Int(String($0))! }
+        
+        // Try all possible digit replacements
+        for i in 0...9 {
+            for j in 0...9 {
+                // Replace digit i with digit j
+                let newDigits = digits.map { $0 == i ? j : $0 }
+                
+                // Convert the array of digits back to a number
+                let newNum = newDigits.reduce(0) { $0 * 10 + $1 }
+                
+                // Update minNum and maxNum if necessary
+                if newNum < minNum {
+                    minNum = newNum
+                }
+                if newNum > maxNum {
+                    maxNum = newNum
+                }
+            }
+        }
+        
+        return maxNum - minNum
+    }
+}
