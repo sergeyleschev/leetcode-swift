d4876a3322ed4547f26c91a4b1e4a00810e1d6eb
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-02 23:29:45 +0300

    2529. Maximum Count of Positive Integer and Negative Integer

---
+++ 2001-2500/2529. Maximum Count of Positive Integer and Negative Integer.swift
@@ -0,0 +1,19 @@
+class Solution {
+
+    // Solution @ Sergey Leschev
+
+    func maximumCount(_ nums: [Int]) -> Int {
+        var posCount = 0
+        var negCount = 0
+        
+        for num in nums {
+            if num > 0 {
+                posCount += 1
+            } else if num < 0 {
+                negCount += 1
+            }
+        }
+        
+        return max(posCount, negCount)
+    }
+}
