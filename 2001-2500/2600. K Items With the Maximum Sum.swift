fca015c7ceafd5b76499c785b61a51b91a00dd98
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-02 23:09:35 +0300

    2600. K Items With the Maximum Sum

---
+++ 2001-2500/2600. K Items With the Maximum Sum.swift
@@ -0,0 +1,11 @@
+class Solution {
+
+    // Solution @ Sergey Leschev
+
+    func kItemsWithMaximumSum(_ numOnes: Int, _ numZeros: Int, _ numNegOnes: Int, _ k: Int) -> Int {
+        let ones = min(numOnes, k)
+        let zeros = min(numZeros, k - ones)
+        let negOnes = min(numNegOnes, k - ones - zeros)
+        return ones - negOnes + min(numOnes - ones, k - ones) - min(numNegOnes - negOnes, k - ones - zeros - negOnes)
+    }
+}
