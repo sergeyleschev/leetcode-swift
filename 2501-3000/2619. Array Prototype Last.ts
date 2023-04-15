9d7207c13f6953e01f05bb08902cc6fd8542771a
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-15 18:04:20 +0300

    2619. Array Prototype Last

---
+++ 2501-3000/2619. Array Prototype Last.ts
@@ -0,0 +1,20 @@
+// Solution @ Sergey Leschev
+
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function () {
+    if (this.length === 0) {
+        return -1;
+    } else {
+        return this[this.length - 1];
+    }
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
