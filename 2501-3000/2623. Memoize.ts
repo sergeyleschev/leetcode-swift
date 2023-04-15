6a0d29f3fc819184357d7a8c652dee569aa67cde
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-15 18:04:59 +0300

    2623. Memoize

---
+++ 2501-3000/2623. Memoize.ts
@@ -0,0 +1,27 @@
+// Solution @ Sergey Leschev
+
+type Fn = (...params: any) => any;
+
+function memoize(fn: Fn): Fn {
+    const cache = new Map<string, any>();
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    }
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
