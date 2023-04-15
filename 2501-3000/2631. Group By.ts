c298112e9fa04c77e54b8615480c1f6172d874b2
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-15 18:05:43 +0300

    2631. Group By

---
+++ 2501-3000/2631. Group By.ts
@@ -0,0 +1,22 @@
+// Solution @ Sergey Leschev
+
+declare global {
+    interface Array<T> {
+        groupBy(fn: (item: T) => string): Record<string, T[]>
+    }
+}
+
+Array.prototype.groupBy = function (fn) {
+    return this.reduce((result, item) => {
+        const key = fn(item);
+        if (!result[key]) {
+            result[key] = [];
+        }
+        result[key].push(item);
+        return result;
+    }, {});
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
