ebcfeb36b2982e8ebbecc7eb026982ff06aea23a
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-15 18:05:26 +0300

    2628. JSON Deep Equal

---
+++ 2501-3000/2628. JSON Deep Equal.ts
@@ -0,0 +1,33 @@
+// Solution @ Sergey Leschev
+
+function areDeeplyEqual(o1: any, o2: any): boolean {
+    if (o1 === o2) {
+        return true;
+    }
+
+    if (typeof o1 !== typeof o2) {
+        return false;
+    }
+
+    if (o1 === null || o2 === null || typeof o1 !== 'object') {
+        return false;
+    }
+
+    const keys1 = Object.keys(o1);
+    const keys2 = Object.keys(o2);
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    if (o1.constructor !== o2.constructor) {
+        return false;
+    }
+
+    for (const key of keys1) {
+        if (!o2.hasOwnProperty(key) || !areDeeplyEqual(o1[key], o2[key])) {
+            return false;
+        }
+    }
+
+    return true;
+}  
