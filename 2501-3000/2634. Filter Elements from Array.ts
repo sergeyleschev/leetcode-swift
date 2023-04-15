1bc8141de655c31f51fa8ba126de3c5c2d290988
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-15 18:06:13 +0300

    2634. Filter Elements from Array

---
+++ 2501-3000/2634. Filter Elements from Array.ts
@@ -0,0 +1,11 @@
+// Solution @ Sergey Leschev
+
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+    const result: number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
