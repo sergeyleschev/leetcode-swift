c6235e788f2d1db1c06a82a0b0bd097ac5a77e02
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-15 18:05:17 +0300

    2627. Debounce

---
+++ 2501-3000/2627. Debounce.ts
@@ -0,0 +1,30 @@
+// Solution @ Sergey Leschev
+
+type DebouncedFunction<F extends (...args: any[]) => any> = (
+    ...args: Parameters<F>
+) => void;
+
+function debounce<F extends (...args: any[]) => any>(
+    fn: F,
+    t: number
+): DebouncedFunction<F> {
+    let timer: NodeJS.Timeout | null;
+
+    return function (this: any, ...args: Parameters<F>) {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+
+        timer = setTimeout(() => {
+            fn.apply(this, args);
+        }, t);
+    };
+}
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
