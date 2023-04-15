e0eaae976eb426c525dbe0a7bf167ac6b9e26fbf
Sergey Leschev <sergey.leschev@gmail.com>
2023-04-15 18:05:51 +0300

    2632. Curry

---
+++ 2501-3000/2632. Curry.ts
@@ -0,0 +1,19 @@
+// Solution @ Sergey Leschev
+
+function curry(fn: Function): Function {
+    return function curried(...args: any[]) {
+        if (args.length >= fn.length) {
+            return fn(...args);
+        }
+        return function (...innerArgs: any[]) {
+            return curried(...args, ...innerArgs);
+        };
+    };
+}
+
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
