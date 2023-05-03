2cabde6b3672faf28e17fdd7576e6f8b7b341abb
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-03 08:29:55 +0300

    1661. Average Time of Process per Machine

---
+++ 1501-2000/1661. Average Time of Process per Machine.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT machine_id, ROUND(AVG(end_time - start_time), 3) AS processing_time
+FROM (
+  SELECT machine_id, process_id, MIN(timestamp) AS start_time, MAX(timestamp) AS end_time
+  FROM Activity
+  GROUP BY machine_id, process_id
+) AS subquery
+GROUP BY machine_id;
