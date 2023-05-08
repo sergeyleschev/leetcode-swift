17671865a2eb145f0e672fed0e5ad7c7425a7f16
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 18:25:14 +0300

    1633. Percentage of Users Attended a Contest

---
+++ 1501-2000/1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT r.contest_id, 
+       ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register r
+WHERE r.user_id IN (SELECT user_id FROM Users)
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
