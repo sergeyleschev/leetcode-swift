21b56d12a58125d646dea6be0004b59ae16d86e0
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 18:23:35 +0300

    1327. List the Products Ordered in a Period

---
+++ 1001-1500/1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT p.product_name, SUM(o.unit) AS unit
+FROM Products p 
+JOIN Orders o ON p.product_id = o.product_id 
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29' 
+GROUP BY p.product_id 
+HAVING SUM(o.unit) >= 100;
