319a45914ad5e6a19143054f0e02d73a1f89bf60
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 18:17:27 +0300

    585. Investments in 2016

---
+++ 501-1000/0585. Investments in 2016.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(*) > 1)
+AND (lat, lon) IN (SELECT lat, lon FROM Insurance GROUP BY lat , lon HAVING COUNT(*) = 1);
