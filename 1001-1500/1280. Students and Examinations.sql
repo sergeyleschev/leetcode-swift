fb0f2d4184c7e6f82afe7629d817a2020e91349d
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 17:59:58 +0300

    1280. Students and Examinations

---
+++ 1001-1500/1280. Students and Examinations.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s 
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
