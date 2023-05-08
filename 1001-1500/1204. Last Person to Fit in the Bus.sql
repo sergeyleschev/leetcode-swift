9e090d1880167d9aa19fe3299172b26b6409d7bb
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 17:53:29 +0300

    1204. Last Person to Fit in the Bus

---
+++ 1001-1500/1204. Last Person to Fit in the Bus.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT person_name
+FROM (
+  SELECT *, SUM(weight) OVER (ORDER BY turn) AS total_weight
+  FROM Queue
+) t
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
