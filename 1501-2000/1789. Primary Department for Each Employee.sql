7e8ba5692479b946ac42869acc65d97c445aedad
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 18:27:33 +0300

    1789. Primary Department for Each Employee

---
+++ 1501-2000/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT employee_id, 
+       department_id 
+FROM   Employee 
+WHERE  primary_flag = 'Y' 
+
+UNION 
+
+SELECT employee_id, 
+       department_id 
+FROM   Employee 
+WHERE  employee_id NOT IN (SELECT employee_id 
+                           FROM   Employee 
+                           WHERE  primary_flag = 'Y') 
