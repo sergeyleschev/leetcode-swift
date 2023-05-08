55e401aa3ff2fe7e3b64e5c23a55200635a460e8
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 18:28:55 +0300

    1907. Count Salary Categories

---
+++ 1501-2000/1907. Count Salary Categories.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT 
+  'Low Salary' AS category, COUNT(*) AS accounts_count 
+FROM Accounts 
+WHERE income < 20000
+UNION
+SELECT 
+  'Average Salary' AS category, COUNT(*) AS accounts_count 
+FROM Accounts 
+WHERE income >= 20000 AND income <= 50000
+UNION
+SELECT 
+  'High Salary' AS category, COUNT(*) AS accounts_count 
+FROM Accounts 
+WHERE income > 50000;
