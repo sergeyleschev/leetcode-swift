e6c2a8241ba8c6c5e8657687fd919bb4a338fa28
Sergey Leschev <sergey.leschev@gmail.com>
2023-05-08 18:30:17 +0300

    1934. Confirmation Rate

---
+++ 1501-2000/1934. Confirmation Rate.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+
+SELECT 
+    s.user_id,
+    IFNULL(
+        ROUND(CAST(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) AS FLOAT) / COUNT(c.user_id), 2),
+        0
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
