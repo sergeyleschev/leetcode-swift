cd2abe131c33f8fec30b64f4e9ce9d5bb40e35bc
Sergey Leschev <sergey.leschev@gmail.com>
2023-06-24 10:47:52 +0300

    956. Tallest Billboard

--- 501-1000/0956. Tallest Billboard.swift
+++ 501-1000/0956. Tallest Billboard.swift
@@ -3,6 +3,21 @@ class Solution {
     // Solution @ Sergey Leschev
     // 956. Tallest Billboard
 
+    // Approach
+    // The code uses dynamic programming to solve the problem. It maintains a dictionary dp, where the keys represent the possible height differences between the two billboards, and the values represent the maximum sum of heights achieved for each height difference.
+
+    // The code iterates through each rod in the given input rods. For each rod i, it creates a new dictionary cur to store the updated values for dp. Then, it iterates through the existing entries in dp and updates the values in cur based on three cases:
+    // Adding the current rod i to the same height difference: cur[sum + i] = max(dp[sum]! + i, cur[sum + i, default: 0])
+    // Keeping the same height difference: cur[sum] = max(dp[sum]!, cur[sum, default: 0])
+    // Subtracting the current rod i from the height difference: cur[sum - i] = max(dp[sum]!, cur[sum - i, default: 0])
+    // After iterating through all the rods, the final result is obtained from dp[0], which represents the maximum possible sum of heights for a height difference of 0 (i.e., the two billboards have equal heights).
+
+    // Complexity
+    // - Time complexity: O(n).
+    // The code iterates through each rod in the input rods, so the time complexity is O(n), where n is the number of rods.
+    // - Space complexity: O(n).
+    // The code uses a dictionary dp to store the maximum sums of heights for each height difference. The size of the dictionary grows as the number of rods increases, so the space complexity is also O(n), where n is the number of rods.
+
     func tallestBillboard(_ rods: [Int]) -> Int {
         var dp = [0: 0]
         
