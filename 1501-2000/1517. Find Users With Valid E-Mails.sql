b68f31732326575e4ae5fbd6a46f8bb0e215cd64
Sergey Leschev <sergey.leschev@gmail.com>
2023-06-25 09:53:51 +0300

    1517. Find Users With Valid E-Mails

---
+++ 1501-2000/1517. Find Users With Valid E-Mails.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# Solution @ Sergey Leschev
+# 1517. Find Users With Valid E-Mails
+
+select * from Users where mail regexp '^[A-Z][A-Z0-9_.-]*[@]leetcode[.]com$';
