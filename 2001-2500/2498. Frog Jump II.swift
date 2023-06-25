49394c05b78e5694fa50f1117752e042d7a9f076
Sergey Leschev <sergey.leschev@gmail.com>
2023-06-25 10:34:45 +0300

    2498. Frog Jump II

---
+++ 2001-2500/2498. Frog Jump II.swift
@@ -0,0 +1,16 @@
+class Solution {
+
+    // Solution @ Sergey Leschev
+    // 2498. Frog Jump II
+
+    func maxJump(_ stones: [Int]) -> Int {
+        var res = stones[1] - stones[0] // store max difference
+        for i in stride(from: 3, to: stones.count, by: 2) {
+            res = max(res, stones[i] - stones[i - 2]) // odd path
+        }
+        for i in stride(from: 2, to: stones.count, by: 2) {
+            res = max(res, stones[i] - stones[i - 2]) // even path
+        }
+        return res
+    }
+}
