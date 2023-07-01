831fe54cd3d299c415023f0ba5565643113bed71
Sergey Leschev <sergey.leschev@gmail.com>
2023-07-02 01:01:49 +0300

    1. Two Sum

--- 0001-0500/0001. Two Sum.swift
+++ 0001-0500/0001. Two Sum.swift
@@ -6,12 +6,12 @@ class Solution {
     // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
     // You may assume that each input would have exactly one solution, and you may not use the same element twice.
     // You can return the answer in any order.
-    
+
     // Example 1:
     // Input: nums = [2,7,11,15], target = 9
     // Output: [0,1]
     // Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-    
+
     // Example 2:
     // Input: nums = [3,2,4], target = 6
     // Output: [1,2]
@@ -19,13 +19,13 @@ class Solution {
     // Example 3:
     // Input: nums = [3,3], target = 6
     // Output: [0,1]
-     
+
     // Constraints:
     // 2 <= nums.length <= 10^3
     // -10^9 <= nums[i] <= 10^9
     // -10^9 <= target <= 10^9
     // Only one valid answer exists.
-    
+
     // - Parameters:
     //   - nums: Array of integers
     //   - target: Specific target
@@ -35,7 +35,7 @@ class Solution {
     // - Complexity:
     //   - time: O(n), where n is the length of the nums.
     //   - space: O(n), where n is the length of the nums.
-    
+
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dict = [Int: Int]()
 
