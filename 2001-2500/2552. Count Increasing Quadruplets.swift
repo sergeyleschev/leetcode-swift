c75af7e3d2c52718048e705a13e2dcf0ab120524
Sergey Leschev <sergey.leschev@gmail.com>
2023-07-02 12:40:32 +0300

    2552. Count Increasing Quadruplets

---
+++ 2001-2500/2552. Count Increasing Quadruplets.swift
@@ -0,0 +1,26 @@
+class Solution {
+
+    // Solution by Sergey Leschev
+    // 2552. Count Increasing Quadruplets
+
+    func countQuadruplets(_ nums: [Int]) -> Int {
+        let n = nums.count
+        var cnt = Array(repeating: 0, count: n)
+        var ans = 0
+
+        for j in 0..<n {
+            var prevSmall = 0
+
+            for i in 0..<j {
+                if nums[j] > nums[i] {
+                    prevSmall += 1
+                    ans += cnt[i]
+                } else if nums[j] < nums[i] {
+                    cnt[i] += prevSmall
+                }
+            }
+        }
+
+        return ans
+    }
+}
