3acffaf3f42b5076b93ca2fce314408d7ad2bff0
Sergey Leschev <sergey.leschev@gmail.com>
2023-07-21 23:21:34 +0300

    2629. Function Composition

--- 2501-3000/2629. Function Composition.ts
+++ 2501-3000/2629. Function Composition.ts
@@ -1,12 +1,16 @@
 // Solution by Sergey Leschev
+// 2629. Function Composition
 
-type F = (x: number) => number;
+type F = (x: number) => number
 
 function compose(functions: F[]): F {
-    return functions.reduceRight((prevFn, nextFn) => {
-        return (x) => nextFn(prevFn(x));
-    }, (x) => x);
-};
+    return functions.reduceRight(
+        (prevFn, nextFn) => {
+            return x => nextFn(prevFn(x))
+        },
+        x => x
+    )
+}
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
