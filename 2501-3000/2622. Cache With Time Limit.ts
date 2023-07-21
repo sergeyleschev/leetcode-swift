bda07e5e07d81713d771de5f70ddb2ade4205db9
Sergey Leschev <sergey.leschev@gmail.com>
2023-07-21 23:23:41 +0300

    2622. Cache With Time Limit

--- 2501-3000/2622. Cache With Time Limit.ts
+++ 2501-3000/2622. Cache With Time Limit.ts
@@ -1,50 +1,50 @@
 // Solution by Sergey Leschev
+// 2622. Cache With Time Limit
 
 class TimeLimitedCache {
-    private cache: Map<number, [number, number]>; // [value, expiration time]
+    private cache: Map<number, [number, number]> // [value, expiration time]
 
     constructor() {
-        this.cache = new Map();
+        this.cache = new Map()
     }
 
     set(key: number, value: number, duration: number): boolean {
-        const expirationTime = Date.now() + duration;
-        const existingValue = this.cache.get(key);
+        const expirationTime = Date.now() + duration
+        const existingValue = this.cache.get(key)
         if (existingValue !== undefined && existingValue[1] > Date.now()) {
             // Key already exists and hasn't expired yet, so update value and expiration time
-            existingValue[0] = value;
-            existingValue[1] = expirationTime;
-            return true;
+            existingValue[0] = value
+            existingValue[1] = expirationTime
+            return true
         } else {
             // Key doesn't exist or has expired, so add new value with expiration time
-            this.cache.set(key, [value, expirationTime]);
-            return false;
+            this.cache.set(key, [value, expirationTime])
+            return false
         }
     }
 
     get(key: number): number {
-        const valueAndExpiration = this.cache.get(key);
+        const valueAndExpiration = this.cache.get(key)
         if (valueAndExpiration !== undefined && valueAndExpiration[1] > Date.now()) {
             // Key exists and hasn't expired yet, so return value
-            return valueAndExpiration[0];
+            return valueAndExpiration[0]
         } else {
             // Key doesn't exist or has expired, so return -1
-            return -1;
+            return -1
         }
     }
 
     count(): number {
         // Count number of unexpired keys
-        let count = 0;
+        let count = 0
         for (const [_, valueAndExpiration] of this.cache) {
             if (valueAndExpiration[1] > Date.now()) {
-                count++;
+                count++
             }
         }
-        return count;
+        return count
     }
 }
-
 
 /**
  * Your TimeLimitedCache object will be instantiated and called as such:
