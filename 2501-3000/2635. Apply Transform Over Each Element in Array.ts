aaa326a6835106e75e2817f114bee056fad34f8d
Sergey Leschev <sergey.leschev@gmail.com>
2023-07-21 23:19:56 +0300

    2635. Apply Transform Over Each Element in Array

--- 2501-3000/2635. Apply Transform Over Each Element in Array.ts
+++ 2501-3000/2635. Apply Transform Over Each Element in Array.ts
@@ -1,9 +1,10 @@
 // Solution by Sergey Leschev
+// 2635. Apply Transform Over Each Element in Array
 
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    const result: number[] = [];
+    const result: number[] = []
     for (let i = 0; i < arr.length; i++) {
-        result.push(fn(arr[i], i));
+        result.push(fn(arr[i], i))
     }
-    return result;
+    return result
 }
