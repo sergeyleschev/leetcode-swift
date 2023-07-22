b4cc44fa1dca78b2e3f35758e8c121170d45ffdb
Sergey Leschev <sergey.leschev@gmail.com>
2023-07-22 21:42:28 +0300

    2767. Partition String Into Minimum Beautiful Substrings

---
+++ 2501-3000/2767. Partition String Into Minimum Beautiful Substrings.swift
@@ -0,0 +1,29 @@
+class Solution {
+
+    // Solution by Sergey Leschev
+    // 2767. Partition String Into Minimum Beautiful Substrings
+    // Complexity
+    // Time: O(n^2)
+    // Space: O(n)
+
+    func minimumBeautifulSubstrings(_ s: String) -> Int {
+        let n = s.count
+        var dp = [Int](repeating: n + 1, count: n + 1)
+        dp[0] = 0
+
+        for (i, char) in s.enumerated() {
+            if char == "0" { continue }
+            var cur = 0
+            var index = i
+            while index < n {
+                cur = cur * 2 + Int(String(s[s.index(s.startIndex, offsetBy: index)]))!
+                if 15625 % cur == 0 {
+                    dp[index + 1] = min(dp[index + 1], dp[i] + 1)
+                }
+                index += 1
+            }
+        }
+
+        return dp[n] > n ? -1 : dp[n]
+    }
+}
