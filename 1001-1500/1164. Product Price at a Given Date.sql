64662d77aadbf3217d0e36931c70c7426c97ca61
Sergey Leschev <sergey.leschev@gmail.com>
2023-07-22 22:04:39 +0300

    1164. Product Price at a Given Date

---
+++ 1001-1500/1164. Product Price at a Given Date.sql
@@ -0,0 +1,39 @@
+-- Solution by Sergey Leschev
+-- 1164. Product Price at a Given Date
+
+SELECT
+    product_id,
+    COALESCE(new_price, 10) AS price
+FROM
+    (
+        SELECT
+            product_id,
+            new_price,
+            change_date,
+            ROW_NUMBER() OVER (
+                PARTITION BY product_id
+                ORDER BY
+                    change_date DESC
+            ) AS rn
+        FROM
+            Products
+        WHERE
+            change_date <= '2019-08-16'
+    ) AS subquery
+WHERE
+    rn = 1
+UNION
+SELECT
+    DISTINCT product_id,
+    10 AS price
+FROM
+    Products
+WHERE
+    product_id NOT IN (
+        SELECT
+            DISTINCT product_id
+        FROM
+            Products
+        WHERE
+            change_date <= '2019-08-16'
+    );
