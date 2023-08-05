8849387187e99b419cd4827ce56e3e13f623535b
Sergey Leschev <sergey.leschev@gmail.com>
2023-08-05 06:37:40 +0300

    2769. Find the Maximum Achievable Number

---
+++ 2501-3000/2769. Find the Maximum Achievable Number.swift
@@ -0,0 +1,9 @@
+class Solution {
+
+    // Solution by Sergey Leschev
+    // 2769. Find the Maximum Achievable Number
+
+    func theMaximumAchievableX(_ num: Int, _ t: Int) -> Int {
+        num + 2 * t
+    }
+}
