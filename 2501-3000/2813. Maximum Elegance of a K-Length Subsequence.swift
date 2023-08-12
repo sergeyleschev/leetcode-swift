1aa505cc20e1fc8237e28263fa809fdbcdf6a224
Sergey Leschev <sergey.leschev@gmail.com>
2023-08-12 09:19:14 +0300

    2813. Maximum Elegance of a K-Length Subsequence

--- 2501-3000/2813. Maximum Elegance of a K-Length Subsequence.swift
+++ 2501-3000/2813. Maximum Elegance of a K-Length Subsequence.swift
@@ -3,10 +3,10 @@ class Solution {
     // Solution by Sergey Leschev
     // 2813. Maximum Elegance of a K-Length Subsequence
 
-    // #Intuition
+    // # Intuition
     // The approach involves sorting the "items" array in descending order based on the "profiti". By selecting the first "k" items, we ensure that we attain the highest possible "total_profit".
 
-    // #Approach
+    // # Approach
     // Upon the selection of the initial "k" items, attention turns to the remaining "n - k" items. The viability of adding these items depends on whether they belong to an unexplored category (not yet in the "seen" set).
     // Given the restriction of maintaining a subsequence size of "k", a pivotal decision arises. To optimize the elegance metric, the algorithm strategically replaces an existing item with the lowest profit when that item shares its category with another.
     // This iterative refinement process continually adjusts the subsequence while upholding the imperative of category distinctiveness.
