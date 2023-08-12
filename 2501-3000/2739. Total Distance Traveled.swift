e7654f973d557a0d0fbdade618e6d9f47004dc96
Sergey Leschev <sergey.leschev@gmail.com>
2023-08-12 09:54:40 +0300

    2739. Total Distance Traveled

--- 2501-3000/2739. Total Distance Traveled.swift
+++ 2501-3000/2739. Total Distance Traveled.swift
@@ -3,6 +3,9 @@ class Solution {
     // Solution by Sergey Leschev
     // 2739. Total Distance Traveled
 
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+
     func distanceTraveled(_ mainTank: Int, _ additionalTank: Int) -> Int {
         (mainTank + min((mainTank - 1) / 4, additionalTank)) * 10
     }
