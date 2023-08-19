4fadbaa699ebf58fdddde6edf91578c5a3123f61
Sergey Leschev <sergey.leschev@gmail.com>
2023-08-19 18:20:35 +0300

    2546. Apply Bitwise Operations to Make Strings Equal

---
+++ 2501-3000/2546. Apply Bitwise Operations to Make Strings Equal.swift
@@ -0,0 +1,9 @@
+class Solution {
+
+    // Solution by Sergey Leschev
+    // 2546. Apply Bitwise Operations to Make Strings Equal
+
+    func makeStringsEqual(_ s: String, _ target: String) -> Bool {
+        s.contains("1") == target.contains("1")
+    }
+}
